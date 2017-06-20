141d0a25a21a6e1c2eb3e88f2e4e5344550fc678
Sarah Fischmann <sfischmann@edx.org>
2017-06-20 17:16:52 -0400

    setup ability to write tests and wrote the first test

--- __tests__/checkbox-test.jsx
+++ __tests__/checkbox-test.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import {shallow, mount} from 'enzyme';
+import { shallow } from 'enzyme';
 import CheckBox from '../src/CheckBox';
 
-test('aria-label changes after click', () => {
-  const wrapper = shallow(
-    <CheckBox name="checkbox" descibedBy="checkbox"
-      label="check me out!" checked="false" />
-    );
+describe('<CheckBox />', () => {
+  it('aria-label changes after click', () => {
+    const wrapper = shallow(
+      <CheckBox
+        name="checkbox"
+        descibedBy="checkbox"
+        label="check me out!"
+        checked="false"
+      />,
+      );
 
     expect(wrapper.find('[aria-checked=false]').exists()).toEqual(true);
     wrapper.find('input').simulate('click');
     expect(wrapper.find('[aria-checked=false]').exists()).toEqual(false);
     expect(wrapper.find('[aria-checked=true]').exists()).toEqual(true);
+  });
 });
