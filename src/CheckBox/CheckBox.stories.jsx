e15b0282544bd5c8875305ff8240338bcd9f14a1
Sarah Fischmann <sfischmann@edx.org>
2017-06-29 16:39:42 -0400

    adding details to checkbox component

--- src/CheckBox/CheckBox.stories.jsx
+++ src/CheckBox/CheckBox.stories.jsx
@@ -10,49 +10,49 @@ storiesOf('CheckBox', module)
     <div>
       <h1>CheckBox</h1>
       <p>This is the basic checkbox that is default unchecked</p>
-    <CheckBox
-      name="checkbox"
-      describedBy="checkbox"
-      label="check me out!"
-      checked="false"
-    />
+      <CheckBox
+        name="checkbox"
+        describedBy="checkbox"
+        label="check me out!"
+        checked="false"
+      />
     </div>
   ))
   .add('disabled', () => (
     <div>
       <h1>Disabled CheckBox</h1>
       <p>This checkbox is disabled so it cannot be checked</p>
-    <CheckBox
-      name="checkbox"
-      describedBy="checkbox"
-      label="you cannot check me out"
-      checked="false"
-      disabled
-    />
-  </div>
+      <CheckBox
+        name="checkbox"
+        describedBy="checkbox"
+        label="you cannot check me out"
+        checked="false"
+        disabled
+      />
+    </div>
   ))
   .add('default checked', () => (
     <div>
       <h1>Selected CheckBox</h1>
       <p>The default state of this checkbox is checked</p>
-    <CheckBox
-      name="checkbox"
-      describedBy="checkbox"
-      label="(un)check me out"
-      checked="true"
-    />
+      <CheckBox
+        name="checkbox"
+        describedBy="checkbox"
+        label="(un)check me out"
+        checked="true"
+      />
     </div>
   ))
   .add('call a function', () => (
     <div>
       <h1>CheckBox with callback</h1>
       <p>This checkbox calls a function when it is either enabled or disabled</p>
-    <CheckBox
-      name="checkbox"
-      describedBy="checkbox"
-      label="check out the console"
-      checked="false"
-      onChange={() => console.log('the checkbox changed state')}
-    />
+      <CheckBox
+        name="checkbox"
+        describedBy="checkbox"
+        label="check out the console"
+        checked="false"
+        onChange={() => console.log('the checkbox changed state')}
+      />
     </div>
   ));
