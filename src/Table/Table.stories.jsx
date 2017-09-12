ab8bb7d67cba829f6404d3e3dcc95c1b5fd488e3
Ari Rizzitano <arizzitano@edx.org>
2017-09-12 17:21:19 -0400

    tests

--- src/Table/Table.stories.jsx
+++ src/Table/Table.stories.jsx
@@ -3,39 +3,70 @@ import { storiesOf } from '@storybook/react';
 
 import Table from './index';
 
+const catData = [
+  {
+    name: 'Lil Bub',
+    color: 'brown tabby',
+    famous_for: 'weird tongue',
+  },
+  {
+    name: 'Grumpy Cat',
+    color: 'siamese',
+    famous_for: 'serving moods',
+  },
+  {
+    name: 'Smoothie',
+    color: 'orange tabby',
+    famous_for: 'modeling',
+  },
+  {
+    name: 'Maru',
+    color: 'brown tabby',
+    famous_for: 'being a lovable oaf',
+  },
+  {
+    name: 'Keyboard Cat',
+    color: 'orange tabby',
+    famous_for: 'piano virtuoso',
+  },
+];
+
+const catHeadings = [
+  {
+    label: 'Name',
+    key: 'name',
+  },
+  {
+    label: 'Famous For',
+    key: 'famous_for',
+  },
+  {
+    label: 'Coat Color',
+    key: 'color',
+  },
+];
+
 storiesOf('Table', module)
-  .add('basic usage', () => (
+  .add('unstyled', () => (
     <Table
-      data={[
-        {
-          name: 'Lil Bub',
-          color: 'brown tabby',
-          famous_for: 'weird tongue',
-        },
-        {
-          name: 'Grumpy Cat',
-          color: 'siamese',
-          famous_for: 'serving moods',
-        },
-        {
-          name: 'Smoothie',
-          color: 'orange tabby',
-          famous_for: 'modeling',
-        },
-      ]}
-      headings={[
-        {
-          display: 'Name',
-          key: 'name',
-        },
-        {
-          display: 'Famous For',
-          key: 'famous_for',
-        },
-        {
-          display: 'Coat Color',
-          key: 'color',
-        },
-      ]}
+      data={catData}
+      headings={catHeadings}
+      caption="Famous Internet Cats"
+    />
+  ))
+  .add('table-striped', () => (
+    <Table
+      data={catData}
+      headings={catHeadings}
+      caption="Famous Internet Cats"
+      className={['table-striped']}
+    />
+  ))
+  .add('default heading', () => (
+    <Table
+      data={catData}
+      headings={catHeadings}
+      caption="Famous Internet Cats"
+      headingClassName={['thead-default']}
     />
   ));
--- src/Table/index.jsx
+++ src/Table/index.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
 import styles from './Table.scss';
 
 class Table extends React.Component {
+  getCaption() {
+    return this.props.caption && (
+      <caption>{this.props.caption}</caption>
+    );
+  }
+
   getHeadings() {
     return (
-      <thead className={styles['thead-default']}>
+      <thead className={classNames(
+        ...this.props.headingClassName.map(className => styles[className]),
+      )}
+      >
         <tr>
           {this.props.headings.map(heading => (
-            <th key={heading.key}>{heading.display}</th>
+            <th
+              key={heading.key}
+              scope="col"
+            >
+              {heading.label}
+            </th>
           ))}
         </tr>
       </thead>
@@ -32,8 +47,13 @@ class Table extends React.Component {
 
   render() {
     return (
-      <table className={styles.table}>
-        {this.getHeadings(this.props.headings)}
+      <table className={classNames(
+        styles.table,
+        ...this.props.className.map(className => styles[className]),
+      )}
+      >
+        {this.getCaption()}
+        {this.getHeadings()}
         {this.getBody()}
       </table>
     );
@@ -41,8 +61,26 @@ class Table extends React.Component {
 }
 
 Table.propTypes = {
-  headings: PropTypes.arrayOf(PropTypes.object).isRequired,
+  caption: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.element,
+  ]),
+  className: PropTypes.arrayOf(PropTypes.string),
   data: PropTypes.arrayOf(PropTypes.object).isRequired,
+  headings: PropTypes.arrayOf(PropTypes.shape({
+    key: PropTypes.string.isRequired,
+    label: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.element,
+    ]).isRequired,
+  })).isRequired,
+  headingClassName: PropTypes.arrayOf(PropTypes.string),
+};
+
+Table.defaultProps = {
+  caption: null,
+  className: [],
+  headingClassName: [],
 };
 
 export default Table;
