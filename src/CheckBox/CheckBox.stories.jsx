1008dad7a40f9707a1fac63a7365f6f5379ecebe
Kevin Groat <apps@kevingroat.io>
2017-10-15 01:36:40 -0400

    Added missing semicolon

--- src/CheckBox/CheckBox.stories.jsx
+++ src/CheckBox/CheckBox.stories.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions'
+import { action } from '@storybook/addon-actions';
 
 import CheckBox from './index';
 
