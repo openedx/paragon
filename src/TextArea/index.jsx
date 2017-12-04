4bd0082552d75fca679e6227a317f08499a8baf3
Farhanah Sheets <fsheets@edx.org>
2017-12-04 10:04:12 -0500

    feat(TextArea): New TextArea Component
    New bootstrap form-control TextArea component - leverages Paragon asInput
    
    Note* based on: https://github.com/edx/paragon/tree/master/src/InputText
    
    Ticket: EDUCATOR-1824

---
+++ src/TextArea/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import asInput, { inputProps } from '../asInput';
+
+function Text(props) {
+  return (
+    <textarea
+      id={props.id}
+      className={classNames(props.className)}
+      type="text"
+      name={props.name}
+      value={props.value}
+      placeholder={props.placeholder}
+      aria-describedby={props.describedBy}
+      onChange={props.onChange}
+      onBlur={props.onBlur}
+      aria-invalid={!props.isValid}
+      disabled={props.disabled}
+      required={props.required}
+    />
+  );
+}
+
+Text.propTypes = inputProps;
+
+const TextArea = asInput(Text);
+
+export default TextArea;
