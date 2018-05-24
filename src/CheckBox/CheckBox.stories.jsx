3ecaf695c5f695db2d16bfd153f610e215c3bb29
jaebradley <jae.b.bradley@gmail.com>
2018-05-24 13:11:42 -0400

    docs(readme-storybook-addons): update storybook README addons

--- src/CheckBox/CheckBox.stories.jsx
+++ src/CheckBox/CheckBox.stories.jsx
@@ -5,6 +5,7 @@ import { storiesOf } from '@storybook/react';
 import centered from '@storybook/addon-centered';
 import { checkA11y } from '@storybook/addon-a11y';
 import { withInfo } from '@storybook/addon-info';
+import { withReadme } from 'storybook-readme';
 
 import CheckBox from './index';
 import Button from '../Button';
@@ -46,9 +47,10 @@ class CheckBoxWrapper extends React.Component {
 }
 
 storiesOf('CheckBox', module)
-  .addDecorator((story, context) => withInfo({}, README)(story)(context))
+  .addDecorator((story, context) => withInfo()(story)(context))
   .addDecorator(centered)
   .addDecorator(checkA11y)
+  .addDecorator(withReadme(README))
   .add('basic usage', () => (
     <CheckBox
       name="checkbox"
--- src/Icon/Icon.stories.jsx
+++ src/Icon/Icon.stories.jsx
@@ -3,15 +3,17 @@ import { storiesOf } from '@storybook/react';
 import centered from '@storybook/addon-centered';
 import { checkA11y } from '@storybook/addon-a11y';
 import { withInfo } from '@storybook/addon-info';
+import { withReadme } from 'storybook-readme';
 
 import FontAwesomeStyles from 'font-awesome/css/font-awesome.min.css';
 import Icon from './index';
 import README from './README.md';
 
 storiesOf('Icon', module)
-  .addDecorator((story, context) => withInfo({}, README)(story)(context))
+  .addDecorator((story, context) => withInfo()(story)(context))
   .addDecorator(centered)
   .addDecorator(checkA11y)
+  .addDecorator(withReadme(README))
   .add('basic usage', () => (
     <Icon
       className={[
