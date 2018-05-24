759576718183aa73603df14e4ee95caa6d97d9e4
jaebradley <jae.b.bradley@gmail.com>
2018-05-24 12:50:26 -0400

    docs(tabs): update tabs documentation

--- src/Tabs/Tabs.stories.jsx
+++ src/Tabs/Tabs.stories.jsx
@@ -1,10 +1,19 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import centered from '@storybook/addon-centered';
+import { checkA11y } from '@storybook/addon-a11y';
+import { withInfo } from '@storybook/addon-info';
+import { withReadme } from 'storybook-readme';
 
 import Tabs from './index';
+import README from './README.md';
 
 storiesOf('Tabs', module)
+  .addDecorator((story, context) => withInfo()(story)(context))
+  .addDecorator(centered)
+  .addDecorator(checkA11y)
+  .addDecorator(withReadme(README))
   .add('basic usage', () => (
     <Tabs
       labels={[
