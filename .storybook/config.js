c92c2ea6d702e61d0b0f07b8cf72da8d403ceedf
Adam Butterworth <abutterworth@edx.org>
2019-03-15 17:11:28 -0400

    fix: misc config changes

--- .storybook/config.js
+++ .storybook/config.js
@@ -15,75 +15,10 @@ addParameters({
   options: {
     brandTitle: '💎 PARAGON',
     brandUrl: 'https://github.com/edx/paragon',
-    /**
-     * show story component as full screen
-     * @type {Boolean}
-     */
-    isFullScreen: false,
-    /**
-     * display panel that shows a list of stories
-     * @type {Boolean}
-     */
-    showNav: true,
-    /**
-     * display panel that shows addon configurations
-     * @type {Boolean}
-     */
-    showPanel: true,
-    /**
-     * where to show the addon panel
-     * @type {String}
-     */
-    panelPosition: 'bottom',
-    /**
-     * sorts stories
-     * @type {Boolean}
-     */
-    sortStoriesByKind: false,
-    /**
-     * regex for finding the hierarchy separator
-     * @example:
-     *   null - turn off hierarchy
-     *   /\// - split by `/`
-     *   /\./ - split by `.`
-     *   /\/|\./ - split by `/` or `.`
-     * @type {Regex}
-     */
-    hierarchySeparator: /\/|\./,
-    /**
-     * regex for finding the hierarchy root separator
-     * @example:
-     *   null - turn off multiple hierarchy roots
-     *   /\|/ - split by `|`
-     * @type {Regex}
-     */
-    hierarchyRootSeparator: /\|/,
-    /**
-     * sidebar tree animations
-     * @type {Boolean}
-     */
-    sidebarAnimations: true,
-    /**
-     * enable/disable shortcuts
-     * @type {Boolean}
-     */
-    enableShortcuts: true,
-    /**
-     * theme storybook, see link below
-     */
-    theme: 'undefined',
   },
   info: {
-    // Make a default for all stories in this book,
-    inline: false, // where the components are inlined
+    inline: false,
     source: true,
-    styles: {
-      header: {
-        h1: {
-          // color: 'red', // and the headers of the sections are red.
-        },
-      },
-    },
   },
 });
 
