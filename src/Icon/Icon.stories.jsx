7f499a85af50762639ba12592157888840a4f439
Adam Butterworth <abutterworth@edx.org>
2019-03-18 09:45:08 -0400

    fix: remove css module import for font awesome in Icon story

--- src/Icon/Icon.stories.jsx
+++ src/Icon/Icon.stories.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import FontAwesomeStyles from 'font-awesome/css/font-awesome.min.css';
 import Icon from './index';
 import README from './README.md';
 
@@ -9,21 +8,13 @@ storiesOf('Icon', module)
   .addParameters({ info: { text: README } })
   .add('basic usage', () => (
     <Icon
-      className={[
-        FontAwesomeStyles.fa,
-        FontAwesomeStyles['fa-birthday-cake'],
-        FontAwesomeStyles['fa-4x'],
-      ]}
+      className="fa fa-birthday-cake fa-4x"
     />
   ))
   .add('with screenreader text', () => (
     <Icon
       id="SampleIcon"
-      className={[
-        FontAwesomeStyles.fa,
-        FontAwesomeStyles['fa-smile-o'],
-        FontAwesomeStyles['fa-4x'],
-      ]}
+      className="fa fa-smile-o fa-4x"
       screenReaderText="Happy Icon"
     />
   ));
