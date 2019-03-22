cb6e0c89a2866a93854ab0af41bd08e7e63af848
Adam Butterworth <abutterworth@edx.org>
2019-03-21 18:43:00 -0700

    fix: add withDeprecatedProps HOC and deprecate className array in button

---
+++ src/withDeprecatedProps.jsx
@@ -0,0 +1,83 @@
+/* eslint no-console: 0 */
+import React from 'react';
+
+
+export const DEPR_TYPES = {
+  MOVED: 'MOVED',
+  FORMAT: 'FORMAT',
+};
+
+function getDisplayName(WrappedComponent) {
+  return WrappedComponent.displayName || WrappedComponent.name || 'Component';
+}
+
+
+function withDeprecatedProps(WrappedComponent, deprecatedProps) {
+  class WithDeprecatedProps extends React.Component {
+    constructor(props) {
+      super(props);
+      this.transformProps = this.transformProps.bind(this);
+    }
+
+    warn(message) {
+      if (process.env.NODE_ENV !== 'development') return;
+      console.warn(`[Deprecated] ${message}`);
+    }
+
+    transformProps(acc, propName) {
+      if (deprecatedProps[propName] === undefined) {
+        acc[propName] = this.props[propName];
+        return acc;
+      }
+
+      const {
+        deprType,
+        newName,
+        expect,
+        transform,
+        message,
+      } = deprecatedProps[propName];
+
+      switch (deprType) {
+        case DEPR_TYPES.MOVED:
+          this.warn(`${getDisplayName(WrappedComponent)}: The prop '${propName}' has been moved to '${newName}'.`);
+          acc[newName] = this.props[propName];
+          break;
+        case DEPR_TYPES.FORMAT:
+          if (!expect(this.props[propName])) {
+            this.warn(`${getDisplayName(WrappedComponent)}: The prop '${propName}' expects a new format. ${message}`);
+            acc[propName] = transform(this.props[propName]);
+          } else {
+            acc[propName] = this.props[propName];
+          }
+          break;
+        default:
+          acc[propName] = this.props[propName];
+          break;
+      }
+
+      return acc;
+    }
+    render() {
+      const transformedProps = Object
+        .keys(this.props)
+        .reduce(this.transformProps, {});
+
+      // eslint-disable-next-line react/prop-types
+      const children = this.props.children || transformedProps.children;
+      delete transformedProps.children;
+
+      return (
+        <WrappedComponent {...transformedProps}>
+          {children}
+        </WrappedComponent>
+      );
+    }
+  }
+
+  WithDeprecatedProps.displayName = `WithDeprecatedProps(${getDisplayName(WrappedComponent)})`;
+
+  return WithDeprecatedProps;
+}
+
+export default withDeprecatedProps;
