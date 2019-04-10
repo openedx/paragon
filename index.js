9b8d0b124cc6982101e3a9a3080c61298c4d2ccd
Adam Butterworth <abutterworth@edx.org>
2019-04-10 17:29:14 -0400

    fix: use webpack config to pass through NODE_ENV

--- index.js
+++
@@ -1,9 +0,0 @@
-// Set an environment variable on the window during
-// development mode of the consuming application.
-// This enables the production, built version of
-// Paragon to display warnings during development.
-if (process.env.NODE_ENV === 'development') {
-  window.PARAGON_ENV = 'development';
-}
-
-module.exports = require('./dist/paragon.js');
