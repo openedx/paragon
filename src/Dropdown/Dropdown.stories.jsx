43d26b879a4d21c811597762a88d68ee7d2244b9
julianajlk <julianajlk@gmail.com>
2019-06-28 15:36:11 -0400

    feat: dropdown (#452)
    * feat: refactor Dropdown to be more flexible
    
    * refactor: dropdown with button subcomponent using Provider
    
    * fix: iconElement conditional redendering
    
    * refactor: added subcomponents to separate files
    
    * refactor: remove some unneeded props and style updates
    
    * test: update config remove old props
    
    * docs: add props tables for sub components

--- src/Dropdown/Dropdown.stories.jsx
+++ src/Dropdown/Dropdown.stories.jsx
@@ -2,57 +2,60 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import Dropdown from './index';
+
 import Icon from '../Icon';
 import README from './README.md';
 
 storiesOf('Navigation|Dropdown', module)
   .addParameters({ info: { text: README } })
   .add('basic usage', () => (
-    <Dropdown
-      title="Search Engines"
-      menuItems={[
-        {
-          label: 'Google',
-          href: 'https://google.com',
-        },
-        {
-          label: 'DuckDuckGo',
-          href: 'https://duckduckgo.com',
-        },
-        {
-          label: 'Yahoo',
-          href: 'https://yahoo.com',
-        },
-      ]}
-    />
-  ))
-  .add('menu items as elements', () => (
-    <Dropdown
-      title="Search Engines"
-      menuItems={[
-        <a href="http://www.google.com">Google</a>,
-        <a href="http://www.duckduckgo.com">DuckDuckGo</a>,
-        <a href="http://www.yahoo.com">Yahoo</a>,
-      ]}
-    />
+    <Dropdown>
+      <Dropdown.Button>
+        Search Engines
+      </Dropdown.Button>
+      <Dropdown.Menu>
+        <Dropdown.Item href="https://google.com">Google</Dropdown.Item>
+        <Dropdown.Item href="https://duckduckgo.com">DuckDuckGo</Dropdown.Item>
+        <Dropdown.Item href="https://yahoo.com">Yahoo</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
   ))
   .add('with icon element', () => (
-    <Dropdown
-      title="Search Engines"
-      iconElement={<Icon className="fa fa-user px-3" />}
-      menuItems={[
-        {
-          label: 'Google',
-          href: 'https://google.com',
-        },
-        {
-          label: 'DuckDuckGo',
-          href: 'https://duckduckgo.com',
-        },
-        {
-          label: 'Yahoo',
-          href: 'https://yahoo.com',
-        },
-      ]}
-    />
+    <Dropdown>
+      <Dropdown.Button>
+        <Icon className="fa fa-user pr-3" alt="" />
+        Search Engines
+      </Dropdown.Button>
+      <Dropdown.Menu>
+        <Dropdown.Item href="https://google.com">Google</Dropdown.Item>
+        <Dropdown.Item href="https://duckduckgo.com">DuckDuckGo</Dropdown.Item>
+        <Dropdown.Item href="https://yahoo.com">Yahoo</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  ))
+  .add('with caret/chevron', () => (
+    <React.Fragment>
+      <Dropdown className="mb-3">
+        <Dropdown.Button>
+          Search Engines
+          <Icon className="fa fa-caret-down pl-3" alt="" />
+        </Dropdown.Button>
+        <Dropdown.Menu>
+          <Dropdown.Item href="https://google.com">Google</Dropdown.Item>
+          <Dropdown.Item href="https://duckduckgo.com">DuckDuckGo</Dropdown.Item>
+          <Dropdown.Item href="https://yahoo.com">Yahoo</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      <Dropdown>
+        <Dropdown.Button>
+          Search Engines
+          <Icon className="fa fa-angle-down pl-3" alt="" />
+        </Dropdown.Button>
+        <Dropdown.Menu>
+          <Dropdown.Item href="https://google.com">Google</Dropdown.Item>
+          <Dropdown.Item href="https://duckduckgo.com">DuckDuckGo</Dropdown.Item>
+          <Dropdown.Item href="https://yahoo.com">Yahoo</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </React.Fragment>
   ));
