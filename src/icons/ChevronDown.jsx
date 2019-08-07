93b0bd24a4becad9e938b9f78150b74cb13e4b70
julianajlk <julianajlk@gmail.com>
2019-08-07 17:32:19 -0400

    fix: update ChevronDown to not be a class Component (wip)

--- src/icons/ChevronDown.jsx
+++ src/icons/ChevronDown.jsx
@@ -1,31 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class ChevronDown extends React.PureComponent {
-  render() {
-    const {
-      viewBox,
-      height,
-      ...other
-    } = this.props;
-    return (
-      <div>
-        <svg
-          aria-hidden="true"
-          role="img"
-          xmlns="http://www.w3.org/2000/svg"
-          height={height}
-          viewBox={viewBox}
-          {...other}
-        >
-          <path
-            d="M1683 808l-742 741q-19 19-45 19t-45-19l-742-741q-19-19-19-45.5t19-45.5l166-165q19-19 45-19t45 19l531 531 531-531q19-19 45-19t45 19l166 165q19 19 19 45.5t-19 45.5z"
-          />
-        </svg>
-      </div>
-    );
-  }
-}
+const ChevronDown = ({ viewBox, height, ...other }) => (
+  <div>
+    <svg
+      aria-hidden="true"
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      height={height}
+      viewBox={viewBox}
+      {...other}
+    >
+      <path
+        d="M1683 808l-742 741q-19 19-45 19t-45-19l-742-741q-19-19-19-45.5t19-45.5l166-165q19-19 45-19t45 19l531 531 531-531q19-19 45-19t45 19l166 165q19 19 19 45.5t-19 45.5z"
+      />
+    </svg>
+  </div>
+);
 
 ChevronDown.propTypes = {
   viewBox: PropTypes.string.isRequired,
