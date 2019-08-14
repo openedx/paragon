e7ccb7dd7fef06b651f78e9ef6514f141a25f65a
julianajlk <julianajlk@gmail.com>
2019-08-14 11:56:57 -0400

    fix: forwardRef

--- src/Collapsible/CollapsibleBody.jsx
+++ src/Collapsible/CollapsibleBody.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useCallback } from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { CollapsibleContext } from './CollapsibleAdvanced';
 import TransitionReplace from '../TransitionReplace';
--- src/Collapsible/index.jsx
+++ src/Collapsible/index.jsx
@@ -1,26 +1,44 @@
 import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
+import * as Icons from '../Icons';
 import CollapsibleAdvanced from './CollapsibleAdvanced';
 import CollapsibleBody from './CollapsibleBody';
 import CollapsibleTrigger from './CollapsibleTrigger';
 import CollapsibleVisible from './CollapsibleVisible';
 
-function Collapsible({
-  children, className, title, ...other
-}) {
+const Collapsible = React.forwardRef((props, ref) => {
+  const {
+    children, className, title, ...other
+  } = props;
+
   return (
-    <CollapsibleAdvanced {...other} className={classNames('collapsible-card', className)}>
-      <CollapsibleTrigger className="collapsible-card-header">{title}</CollapsibleTrigger>
-      <CollapsibleBody className="collapsible-card-body">{children}</CollapsibleBody>
-    </CollapsibleAdvanced>
+    <Collapsible.Advanced
+      {...other}
+      className={classNames('collapsible-card', className)}
+      forwardedRef={ref}
+    >
+      <Collapsible.Trigger className="collapsible-card-header">
+        { typeof title === 'string' ? <p><strong>{title}</strong></p> : title }
+        <span className="collapsible-card-header-icon" aria-hidden>
+          <Collapsible.Visible whenClosed>
+            <Icons.ChevronDown width="2rem" height="2rem" />
+          </Collapsible.Visible>
+
+          <Collapsible.Visible whenOpen>
+            <Icons.ChevronUp width="2rem" height="2rem" />
+          </Collapsible.Visible>
+        </span>
+      </Collapsible.Trigger>
+      <Collapsible.Body className="collapsible-card-body">{children}</Collapsible.Body>
+    </Collapsible.Advanced>
   );
-}
+});
 
 Collapsible.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
-  title: PropTypes.string.isRequired,
+  title: PropTypes.node.isRequired,
 };
 Collapsible.defaultProps = {
   className: 'collapsible',
