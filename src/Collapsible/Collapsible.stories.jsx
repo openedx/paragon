143fcb18fa2dfb08587402994c0fc700a4f9b407
Adam Butterworth <abutterworth@edx.org>
2019-08-20 15:51:29 -0400

    test: update snapshots

--- src/Collapsible/Collapsible.stories.jsx
+++ src/Collapsible/Collapsible.stories.jsx
@@ -2,36 +2,18 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import Collapsible from './index';
-import Icon from '../Icon';
 
 storiesOf('Collapsible', module)
   .add('usage', () => (
     <Collapsible
-      className="collapsible-card"
       onToggle={isOpen => console.log('Collapsible toggled and open is: ', isOpen)}
       onOpen={() => console.log('Collapsible opened.')}
       onClose={() => console.log('Collapsible closed.')}
     >
-      <Collapsible.Trigger className="collapsible-card-header d-flex align-items-center">
-        <h4 className="flex-grow-1">A heading</h4>
+      <p>Your stuff goes here.</p>
 
-        <span className="collapsible-card-header-icon" aria-hidden>
-          <Collapsible.Visible whenClosed>
-            <Icon className="fa fa-chevron-down" />
-          </Collapsible.Visible>
-
-          <Collapsible.Visible whenOpen>
-            <Icon className="fa fa-chevron-up" />
-          </Collapsible.Visible>
-        </span>
+      <Collapsible.Trigger closeOnly tag="a" className="btn btn-outline-primary">
+        Close
       </Collapsible.Trigger>
-
-      <Collapsible.Body className="collapsible-card-body">
-        <p>Your stuff goes here.</p>
-
-        <Collapsible.Trigger closeOnly tag="a" className="btn btn-outline-primary">
-          Close
-        </Collapsible.Trigger>
-      </Collapsible.Body>
     </Collapsible>
   ));
