5df143cb3d7a5f49548643d5f4bd06e888949078
Thomas Tracy <contact@tjtracy.com>
2019-08-23 16:55:44 -0400

    feat: delete storybook
    feat: delete storybook
    
    fix: fix scripts, add post install
    
    fix: fix scripts, add post install

--- src/setupTest.js
+++ src/setupTest.js
@@ -1,11 +1,4 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import registerRequireContextHook from 'babel-plugin-require-context-hook/register';
-
-// Polyfill Webpack's require.context function for our test environment.
-// It is currently used in .storybook/config.js. See this for more info:
-// https://www.npmjs.com/package/@storybook/addon-storyshots#configure-jest-to-work-with-webpacks-requirecontext
-registerRequireContextHook();
-
 
 Enzyme.configure({ adapter: new Adapter() });
