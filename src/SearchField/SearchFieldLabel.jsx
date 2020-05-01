ea9386b521e62c19d0a7cbdc9bda5363aee72107
Adam Stankiewicz <astankiewicz@edx.org>
2020-05-01 10:08:52 -0400

    feat(searchfield): refactor SearchField component (#540)
    * feat(searchfield): refactor SearchField component
    
    BREAKING CHANGE: refactoring the SearchField component to add support for
    additional flexibility (e.g., optional clear button, no label displayed, etc.).

---
+++ src/SearchField/SearchFieldLabel.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable max-len */
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import { SearchFieldContext } from './SearchFieldAdvanced';
+
+const SearchFieldLabel = ({ children }) => {
+  const { screenReaderText, inputId } = useContext(SearchFieldContext);
+
+  return (
+    <label
+      htmlFor={inputId.current}
+      className={classNames('m-0', { 'has-label-text': !!children })}
+    >
+      {children || <span className="sr-only">{screenReaderText.label}</span>}
+    </label>
+  );
+};
+
+SearchFieldLabel.propTypes = {
+  /**
+   * specifies the label to use for the input field (e.g., for i18n translations). Note: if `children` is not provided,
+   * a screenreader-only label will be used in its placed based on the `screenReaderText.label` prop for `SearchField.Advanced`.
+   */
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+};
+
+SearchFieldLabel.defaultProps = {
+  children: undefined,
+};
+
+export default SearchFieldLabel;
