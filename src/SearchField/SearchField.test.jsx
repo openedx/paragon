74934977b9653275838e9c96ab019fb084954451
Adam Stankiewicz <astankiewicz@edx.org>
2020-05-01 10:08:52 -0400

    feat(searchfield): refactor SearchField component (#540)
    * feat(searchfield): refactor SearchField component
    
    BREAKING CHANGE: refactoring the SearchField component to add support for
    additional flexibility (e.g., optional clear button, no label displayed, etc.).

--- src/SearchField/SearchField.test.jsx
+++ src/SearchField/SearchField.test.jsx
@@ -1,105 +1,102 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 import SearchField from './index';
+import SearchFieldLabel from './SearchFieldLabel';
+import SearchFieldInput from './SearchFieldInput';
+import SearchFieldClearButton from './SearchFieldClearButton';
+import SearchFieldSubmitButton from './SearchFieldSubmitButton';
 
 const baseProps = {
   onSubmit: () => {},
 };
 
-describe('<SearchField />', () => {
-  it('renders', () => {
-    const props = {
-      ...baseProps,
-    };
-    const wrapper = mount(<SearchField {...props} />);
-    expect(wrapper.exists()).toEqual(true);
+describe('<SearchField /> with basic usage', () => {
+  it('should match the snapshot', () => {
+    const wrapper = shallow(<SearchField {...baseProps} />);
+    expect(wrapper.html()).toMatchSnapshot();
   });
 
-  it('uses passed in value', () => {
-    const value = 'foofoo';
-    const props = {
+  it('should pass correct props to `SearchField.Advanced`', () => {
+    const wrapper = shallow(<SearchField {...baseProps} />);
+    expect(wrapper.find(SearchField.Advanced).props()).toEqual({
+      children: [
+        <SearchFieldLabel />,
+        <SearchFieldInput />,
+        <SearchFieldClearButton />,
+        <SearchFieldSubmitButton />,
+      ],
+      className: undefined,
+      icons: {
+        clear: <FontAwesomeIcon icon={faTimes} />,
+        submit: <FontAwesomeIcon icon={faSearch} />,
+      },
+      onFocus: expect.any(Function),
+      onBlur: expect.any(Function),
+      onChange: expect.any(Function),
+      onSubmit: expect.any(Function),
+      onClear: expect.any(Function),
+      value: '',
+      screenReaderText: {
+        label: 'search',
+        clearButton: 'clear search',
+        submitButton: 'submit search',
+      },
+    });
+  });
+
+  it('should pass correct props to `SearchField.Label`', () => {
+    const label = 'foobar';
+    let props = { ...baseProps, label };
+    let wrapper = mount(<SearchField {...props} />);
+
+    expect(wrapper.find(SearchField.Label).prop('children')).toEqual(label);
+    expect(wrapper.find('label').prop('children')).toEqual(label);
+
+    props = {
       ...baseProps,
-      value,
+      screenReaderText: { label, submitButton: 'submit foobar' },
     };
+
+    wrapper = mount(<SearchField {...props} />);
+    expect(wrapper.find('label').children().equals(<span className="sr-only">{label}</span>)).toBeTruthy();
+  });
+
+  it('should pass correct props to `SearchField.Input`', () => {
+    const wrapper = mount(<SearchField {...baseProps} placeholder="foobar" />);
+    expect(wrapper.find(SearchField.Input).prop('placeholder')).toEqual('foobar');
+    expect(wrapper.find('input').prop('placeholder')).toEqual('foobar');
+  });
+
+  it('should use passed in initial `value` prop', () => {
+    const value = 'foobar';
+    const props = { ...baseProps, value };
     const wrapper = mount(<SearchField {...props} />);
-    expect(wrapper.state('value')).toEqual(value);
+
+    expect(wrapper.find(SearchField.Advanced).prop('value')).toEqual(value);
     expect(wrapper.find('input').prop('value')).toEqual(value);
   });
 
-  it('overrides state value when props value changes', () => {
-    const initValue = 'foofoo';
-    const newValue = 'barbar';
-    const props = {
-      ...baseProps,
-      value: initValue,
-    };
-    const wrapper = mount(<SearchField {...props} />);
-    expect(wrapper.state('value')).toEqual(initValue);
-    wrapper.setProps({
-      value: newValue,
-    });
-    expect(wrapper.state('value')).toEqual(newValue);
-  });
-
-  it('does not override state value when props value changes with existing value', () => {
-    const value = 'foofoo';
-    const props = {
-      ...baseProps,
-      value,
-    };
-    const wrapper = mount(<SearchField {...props} />);
-    expect(wrapper.state('value')).toEqual(value);
-    wrapper.setProps({
-      value,
-    });
-    expect(wrapper.state('value')).toEqual(value);
-  });
-
-  it('uses passed in placeholder', () => {
-    const placeholder = 'foofoo';
-    const props = {
-      ...baseProps,
-      placeholder,
-    };
-    const wrapper = mount(<SearchField {...props} />);
-    expect(wrapper.find('input').prop('placeholder')).toEqual(placeholder);
-  });
-
-  it('uses passed in inputLabel text', () => {
-    const inputLabel = 'Buscar:';
-    const props = {
-      ...baseProps,
-      inputLabel,
-    };
-    const wrapper = mount(<SearchField {...props} />);
-    expect(wrapper.find('label').text()).toEqual(inputLabel);
-  });
-
-  it('uses passed in screenReaderText', () => {
+  it('should use passed in `screenReaderText` prop', () => {
     const screenReaderText = {
-      clearButton: 'Borrar búsqueda',
-      searchButton: 'Enviar búsqueda',
+      label: 'buscar',
+      submitButton: 'enviar búsqueda',
+      clearButton: 'borrar búsqueda',
     };
-    const props = {
-      ...baseProps,
-      screenReaderText,
-    };
+    const props = { ...baseProps, screenReaderText };
     const wrapper = mount(<SearchField {...props} />);
     wrapper.find('input').simulate('change', { target: { value: 'foobar' } });
-    const searchActionButtons = wrapper.find('button');
-    expect(searchActionButtons.first().find('Icon').prop('screenReaderText')).toEqual(screenReaderText.clearButton);
-    expect(searchActionButtons.last().find('Icon').prop('screenReaderText')).toEqual(screenReaderText.searchButton);
+    expect(wrapper.find('button[type="reset"] .sr-only').prop('children')).toEqual(screenReaderText.clearButton);
+    expect(wrapper.find('button[type="submit"] .sr-only').prop('children')).toEqual(screenReaderText.submitButton);
   });
 
-  describe('fires', () => {
+  describe('should fire', () => {
     it('focus handler', () => {
       const spy = jest.fn();
-      const props = {
-        ...baseProps,
-        onFocus: spy,
-      };
+      const props = { ...baseProps, onFocus: spy };
       const wrapper = mount(<SearchField {...props} />);
       wrapper.find('input').simulate('focus');
       expect(spy).toHaveBeenCalledTimes(1);
@@ -107,10 +104,7 @@ describe('<SearchField />', () => {
 
     it('blur handler', () => {
       const spy = jest.fn();
-      const props = {
-        ...baseProps,
-        onBlur: spy,
-      };
+      const props = { ...baseProps, onBlur: spy };
       const wrapper = mount(<SearchField {...props} />);
       wrapper.find('input').simulate('blur');
       expect(spy).toHaveBeenCalledTimes(1);
@@ -118,88 +112,48 @@ describe('<SearchField />', () => {
 
     it('change handler', () => {
       const spy = jest.fn();
-      const props = {
-        ...baseProps,
-        onChange: spy,
-      };
+      const props = { ...baseProps, onChange: spy };
       const wrapper = mount(<SearchField {...props} />);
-      wrapper.find('input').simulate('change');
+      wrapper.find('input').simulate('change', { target: { value: 'foobar' } });
       expect(spy).toHaveBeenCalledTimes(1);
     });
 
     it('clear handler', () => {
       const spy = jest.fn();
-      const props = {
-        ...baseProps,
-        onClear: spy,
-      };
+      const props = { ...baseProps, onClear: spy };
       const wrapper = mount(<SearchField {...props} />);
       wrapper.find('input').simulate('change', { target: { value: 'foobar' } });
-      wrapper.find('.input-group-append button').first().simulate('click');
+      wrapper.find('button[type="reset"]').simulate('reset');
       expect(spy).toHaveBeenCalledTimes(1);
     });
 
-    it('submit handler on click', () => {
+    it('submit handler on submit button click', () => {
       const spy = jest.fn();
-      const props = {
-        ...baseProps,
-        onSubmit: spy,
-      };
+      const props = { ...baseProps, onSubmit: spy };
       const wrapper = mount(<SearchField {...props} />);
       wrapper.find('input').simulate('change', { target: { value: 'foobar' } });
-      wrapper.find('.input-group-append button').last().simulate('click');
+      wrapper.find('button[type="submit"]').simulate('submit');
       expect(spy).toHaveBeenCalledTimes(1);
-    });
-
-    it('submit handler on enter keypress', () => {
-      const spy = jest.fn();
-      const props = {
-        ...baseProps,
-        onSubmit: spy,
-      };
-      const wrapper = mount(<SearchField {...props} />);
-      wrapper.find('input').simulate('change', { target: { value: 'foobar' } });
-      wrapper.find('input').simulate('keypress', { key: 'Enter' });
-      expect(spy).toHaveBeenCalledTimes(1);
-    });
-  });
-
-  describe('does not fire', () => {
-    it('submit handler on non-enter keypress', () => {
-      const spy = jest.fn();
-      const props = {
-        ...baseProps,
-        onSubmit: spy,
-      };
-      const wrapper = mount(<SearchField {...props} />);
-      wrapper.find('input').simulate('change', { target: { value: 'foobar' } });
-      wrapper.find('input').simulate('keypress', { key: 's' });
-      expect(spy).toHaveBeenCalledTimes(0);
+      expect(spy).toHaveBeenCalledWith('foobar');
     });
   });
 
   describe('clear button', () => {
-    it('renders', () => {
-      const props = {
-        ...baseProps,
-      };
+    it('should be visible with input value', () => {
+      const props = { ...baseProps };
       const wrapper = mount(<SearchField {...props} />);
+      expect(wrapper.find('button[type="reset"]').exists()).toBeFalsy();
       wrapper.find('input').simulate('change', { target: { value: 'foobar' } });
-      expect(wrapper.find('.input-group-append button').length).toEqual(2);
+      expect(wrapper.find('button[type="reset"]').exists()).toBeTruthy();
     });
 
-    it('clears input', () => {
-      const props = {
-        ...baseProps,
-        hasClearButton: true,
-      };
+    it('should clear input value when clicked', () => {
+      const props = { ...baseProps };
       const wrapper = mount(<SearchField {...props} />);
       wrapper.find('input').simulate('change', { target: { value: 'foobar' } });
       expect(wrapper.find('input').prop('value')).toEqual('foobar');
-      expect(wrapper.find('.input-group-append button').length).toEqual(2);
-      wrapper.find('.input-group-append button').first().simulate('click');
+      wrapper.find('button[type="reset"]').simulate('reset');
       expect(wrapper.find('input').prop('value')).toEqual('');
-      expect(wrapper.find('.input-group-append button').length).toEqual(1);
     });
   });
 });
---
+++ src/SearchField/SearchFieldInput.jsx
@@ -0,0 +1,40 @@
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+
+import Input from '../Input';
+import { SearchFieldContext } from './SearchFieldAdvanced';
+
+const SearchFieldInput = (props) => {
+  const {
+    inputId, value, handleChange, handleFocus, handleBlur, refs,
+  } = useContext(SearchFieldContext);
+
+  return (
+    <Input
+      {...props}
+      ref={refs.input}
+      type="text"
+      role="searchbox"
+      id={inputId.current}
+      name="searchfield-input"
+      value={value}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+      onChange={handleChange}
+    />
+  );
+};
+
+SearchFieldInput.propTypes = {
+  /** specifies a custom class name. */
+  className: PropTypes.string,
+  /** specifies the placeholder text for the input. */
+  placeholder: PropTypes.string,
+};
+
+SearchFieldInput.defaultProps = {
+  className: undefined,
+  placeholder: undefined,
+};
+
+export default SearchFieldInput;
