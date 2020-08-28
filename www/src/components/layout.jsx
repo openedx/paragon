b874dcefcb9fbd26eee8b34e9f6f6efe3085b674
Adam Butterworth <abutterworth@edx.org>
2020-08-28 11:11:59 -0400

    docs: add netlify open source plan requirements to documentation (#564)

--- www/src/components/layout.jsx
+++ www/src/components/layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
+import { Nav } from '~paragon-react';
 
 import Navigation from './navigation';
 
@@ -24,6 +25,24 @@ const Layout = ({ children }) => (
         <div className="pgn-doc__main-content">
           {children}
         </div>
+        <footer className="pgn-doc_footer border-top border-light d-flex align-items-center">
+          <Nav className="flex-grow-1">
+            <Nav.Item>
+              <Nav.Link href="https://github.com/edx/paragon">Contribute on Github</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="https://github.com/edx/.github/blob/master/CODE_OF_CONDUCT.md">Code of Conduct</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="https://open.edx.org/">Open edX</Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <p className="m-0 text-muted">
+            <a href="https://www.netlify.com">
+              <img src="https://www.netlify.com/img/global/badges/netlify-light.svg" alt="Deploys by Netlify" />
+            </a>
+          </p>
+        </footer>
       </div>
     )}
   />
