e38a4078674c3ebbd8fa8fab38386354a77da4b1
Adam Butterworth <abutterworth@edx.org>
2020-12-08 19:54:40 -0500

    docs: fix color docs (#594)
    * docs: fix color docs
    
    * docs: minor page refactor

--- www/src/pages/foundations/colors.jsx
+++ www/src/pages/foundations/colors.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import classNames from 'classnames';
+import Color from 'color';
 import SEO from '../../components/seo';
+import MeasuredItem from '../../components/MeasuredItem';
 
 const utilityClasses = {
   bg: (color, level) => (level ? `bg-${color}-${level}` : `bg-${color}`),
@@ -13,20 +15,15 @@ const colors = [
   { themeName: 'gray', color: 'gray', unusedLevels: [] },
   { themeName: 'primary', color: 'blue', unusedLevels: [] },
   { themeName: 'brand', color: 'blue', unusedLevels: [] },
+  { themeName: 'light', color: 'light', unusedLevels: [] },
+  { themeName: 'dark', color: 'dark', unusedLevels: [] },
   { themeName: 'success', color: 'green', unusedLevels: [] },
   { themeName: 'info', color: 'teal', unusedLevels: [] },
   { themeName: 'danger', color: 'red', unusedLevels: [] },
-  { themeName: 'warning', color: 'yellow', unusedLevels: [500, 700, 900] },
+  { themeName: 'warning', color: 'yellow', unusedLevels: [] },
 ];
 
-const levels = [
-  { level: 100, use: 'Backgrounds' },
-  { level: 200, use: 'Borders & Lines' },
-  { level: 300, use: 'Icons & Semantic State' },
-  { level: 500, use: 'Lighter Text & Element Fills' },
-  { level: 700, use: 'Regular Text & Hover State' },
-  { level: 900, use: 'Darker Text & Active State' },
-];
+const levels = [100, 200, 300, 400, 500, 600, 700, 800, 900];
 
 const selectorColors = {};
 
@@ -41,6 +38,39 @@ function parseColors(cssSelectors) {
   });
 }
 
+const Swatch = ({ name, colorClassName, isUnused }) => (
+  <div className="d-flex align-items-center mb-2">
+    <MeasuredItem
+      properties={['background-color']}
+      renderAfter={(measurements) => {
+        return (
+          <div style={{ lineHeight: 1 }} className="small">
+            <code className="mb-0 d-block text-lowercase text-dark-700">{name}</code>
+            <code style={{ fontSize: '65%' }} className="text-muted">{Color(measurements['background-color']).hex()}</code>
+          </div>
+        );
+      }}>
+      <div className={classNames('pgn-doc__swatch', colorClassName, { 'unused-level': isUnused })} />
+    </MeasuredItem>
+  </div>
+);
+
+const renderColorRamp = (themeName, unusedLevels) => (
+  <div style={{ flexBasis: '24%', marginRight: '1%', marginBottom: '2rem' }}>
+    <h5>
+      {themeName}
+    </h5>
+    {levels.map(level => (
+      <Swatch
+        name={`$${themeName}-${level}`}
+        colorClassName={utilityClasses.bg(themeName, level)}
+        isUnused={unusedLevels.includes(level)}
+      />
+    ))}
+  </div>
+);
+
+
 // eslint-disable-next-line react/prop-types
 export default function ({ data }) {
   parseColors(data.allCssUtilityClasses.nodes);
@@ -52,50 +82,19 @@ export default function ({ data }) {
 
       <h1>Colors</h1>
 
-      <h3>Palette</h3>
-
-      <p>
-        Below is an exhaustive set of UI colors. Colors for brands,
-        illustrations, or graphics are not included.
-      </p>
-
-      {levels.map(({ level, use }) => (
-        <div>
-          <h6 className="text-muted text-uppercase mb-3 font-weight-normal">
-            <strong className="mr-2">{level}</strong> {use}
+      <div className="d-flex flex-wrap">
+        {colors.slice(0,3).map(({ themeName, unusedLevels }) => renderColorRamp(themeName, unusedLevels))}
+        <div style={{ flexBasis: '19%', marginRight: '1%', marginBottom: '2rem' }}>
+          <h6>
+            accents
           </h6>
-          <div className="d-flex">
-            {colors.map(({ themeName, unusedLevels }) => (
-              <div className="mr-3 mb-4 w-100">
-                <div
-                  className={classNames(
-                    'pgn-doc__swatch',
-                    utilityClasses.bg(themeName, level),
-                    {
-                      'unused-level': unusedLevels.includes(level),
-                    },
-                  )}
-                />
-                {unusedLevels.includes(level) ? null : (
-                  <div style={{ lineHeight: 1 }} className="mt-1">
-                    <code className="mb-0 d-flex justify-content-between text-lowercase text-gray-700">
-                      <span>{themeName}</span>
-                      <span>{level}</span>
-                    </code>
-                    <code
-                      style={{ fontSize: '65%' }}
-                      className="text-black-50 text-lowercase"
-                    >
-                      {selectorColors[utilityClasses.bg(themeName, level)]}
-                    </code>
-                  </div>
-                )}
-              </div>
-            ))}
-          </div>
-        </div>
-      ))}
 
+          <Swatch name="$accent-a" colorClassName="bg-accent-a" />
+          <Swatch name="$accent-b" colorClassName="bg-accent-b" />
+        </div>
+
+        {colors.slice(3).map(({ themeName, unusedLevels }) => renderColorRamp(themeName, unusedLevels))}
+      </div>
 
       <h3>SCSS Color Usage</h3>
       <p>
