b0439302ccd7960132f0e2d9ae0b68c856c8045e
Lael Birch <lbirch@edx.org>
2020-12-17 16:51:11 -0500

    Add SmartStatus indicator

---
+++ src/TableNew/SmartStatus.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { requiredWhen } from './utils/propTypesUtils';
+import SelectionState from './SelectionState';
+
+const SmartStatus = ({
+  isSelectable,
+  numberOfSelectedRows,
+  toggleAllRowsSelected,
+  filterNames,
+  pageSize,
+  itemCount,
+}) => {
+  if (isSelectable && numberOfSelectedRows > 0) {
+    return (
+      <SelectionState
+        numberOfSelectedRows={numberOfSelectedRows}
+        toggleAllRowsSelected={toggleAllRowsSelected}
+        itemCount={itemCount}
+      />
+    );
+  }
+  if (filterNames.length > 0) {
+    return <div>Filtered by {filterNames.join(', ')}</div>;
+  }
+  return <div>Showing {pageSize || itemCount} of {itemCount}</div>;
+};
+
+SmartStatus.defaultProps = {
+  numberOfSelectedRows: 0,
+  toggleAllRowsSelected: () => {},
+  filterNames: [],
+  pageSize: null,
+};
+
+SmartStatus.propTypes = {
+  isSelectable: PropTypes.bool.isRequired,
+  numberOfSelectedRows: requiredWhen(PropTypes.number, 'isSelectable'),
+  toggleAllRowsSelected: requiredWhen(PropTypes.func, 'isSelectable'),
+  filterNames: PropTypes.arrayOf(PropTypes.string),
+  pageSize: PropTypes.number,
+  itemCount: PropTypes.number.isRequired,
+};
+
+export default SmartStatus;
