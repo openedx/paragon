f94c98406b764bfdc8b8d5735a1f32db8e232d43
Lael Birch <lbirch@edx.org>
2020-12-17 18:41:19 -0500

    Add dropdown filters component

---
+++ src/TableNew/DropdownFilters.jsx
@@ -0,0 +1,48 @@
+import React, { useMemo } from 'react';
+import PropTypes from 'prop-types';
+import { DropdownButton, useWindowSize } from '..';
+
+/** The first filter will be as an input, additional filters will be available in a dropdown.  */
+const DropdownFilters = ({
+  columns,
+}) => {
+  const { width } = useWindowSize();
+  const [breakoutFilter, otherFilters] = useMemo(() => {
+    const availableFilters = columns.filter((column) => column.canFilter);
+    // 576 is the bootstrap xs breakpoint
+    if (width < 576) {
+      return [null, availableFilters];
+    }
+
+    return [availableFilters.shift(), availableFilters];
+  }, [columns, width]);
+
+  return (
+    <div className="pgn__table-filters">
+      {breakoutFilter && <div className="pgn__table-filters-breakout-filter">{breakoutFilter.render('Filter')}</div>}
+      {otherFilters.length > 0 && (
+      <DropdownButton variant="outline-primary" id="table-filters-dropdown" title="Filters">
+        {otherFilters.map(column => (
+          <div
+            key={column.Header}
+            className="pgn__table-filters-dropdown-item"
+          >
+            {column.render('Filter')}
+          </div>
+        ))}
+      </DropdownButton>
+      )}
+    </div>
+
+  );
+};
+
+DropdownFilters.propTypes = {
+  columns: PropTypes.arrayOf(PropTypes.shape({
+    Header: PropTypes.string.isRequired,
+    canFilter: PropTypes.bool,
+    render: PropTypes.func.isRequired,
+  })).isRequired,
+};
+
+export default DropdownFilters;
