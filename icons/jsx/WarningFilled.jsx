bec2481781ef960842416f986acaa11f6c14fe4f
Adam Butterworth <abutterworth@edx.org>
2021-01-26 09:23:55 -0500

    feat: add several new icons (#607)
    New icons: add_alert, assignment, spinner, remove, delete, email, menu

---
+++ icons/jsx/WarningFilled.jsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+function SvgWarningFilled(props) {
+  return (
+    <svg
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M12 2L1 21h22L12 2z" fill="#F0CC00" />
+      <path d="M13 16h-2v2h2v-2zM13 10h-2v4h2v-4z" fill="#111" />
+    </svg>
+  );
+}
+
+export default SvgWarningFilled;
