e2dbc6d3de0a40c5bc92fa6ea8bec02bb933e48d
Adam Stankiewicz <agstanki@gmail.com>
2021-01-30 17:16:59 -0500

    clean up section action styles

--- src/Card/index.jsx
+++ src/Card/index.jsx
@@ -6,11 +6,6 @@ import CardSection from './CardSection';
 const Card = CardDeprecated;
 Card.Divider = CardDivider;
 Card.Section = CardSection;
-Card.SectionContent = CardSection.Content;
-Card.SectionTitle = CardSection.Title;
-Card.SectionActions = CardSection.Actions;
-Card.SectionAction = CardSection.Action;
-Card.SectionText = CardSection.Text;
 
 export { default as CardDeck } from 'react-bootstrap/CardDeck';
 export { default as CardColumns } from 'react-bootstrap/CardColumns';
