b1b532c1d340d81b6784cf4e4bd8c70cd0ec4d21
Adam Stankiewicz <agstanki@gmail.com>
2021-01-31 20:51:56 -0500

    export card footer text

--- src/Card/CardFooter/index.jsx
+++ src/Card/CardFooter/index.jsx
@@ -1,3 +1,5 @@
+import Card from 'react-bootstrap/Card';
+
 import CardFooter from './CardFooter';
 import CardFooterContent from './CardFooterContent';
 import CardFooterActions from './CardFooterActions';
@@ -5,6 +7,7 @@ import CardFooterAction from './CardFooterAction';
 
 CardFooter.Content = CardFooterContent;
 CardFooter.Actions = CardFooterActions;
+CardFooter.Text = Card.Text;
 CardFooter.Action = CardFooterAction;
 
 export default CardFooter;
