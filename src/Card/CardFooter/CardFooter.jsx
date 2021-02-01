68f340b9d66e35deccbfb60fd096d3b8530ebcb6
Adam Stankiewicz <agstanki@gmail.com>
2021-01-31 20:11:15 -0500

    add custom breakpoints prop for card footer

--- src/Card/CardFooter/CardFooter.jsx
+++ src/Card/CardFooter/CardFooter.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState, useMemo } from 'react';
+import React, { useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { useResizeDetector } from 'react-resize-detector/build/withPolyfill';
@@ -7,7 +7,7 @@ import { useReversedChildren } from '../hooks';
 
 export const CardFooterContext = React.createContext();
 
-const CARD_FOOTER_BREAKPOINTS = {
+export const CARD_FOOTER_BREAKPOINTS = {
   STACKED: 460,
 };
 
@@ -15,27 +15,25 @@ const CardFooter = ({
   className,
   children,
   stacked: controlledStacked,
+  breakpoints,
   ...attrs
 }) => {
   const [stacked, setStacked] = useState(controlledStacked);
 
-  const handleResize = useCallback(
-    (width) => {
-      if (controlledStacked) {
-        // ``stacked`` was passed in as a prop so this component is being used as a
-        // controlled component; rely on this prop instead of setting ``stacked`` state below.
-        return;
-      }
+  const handleResize = (width) => {
+    if (controlledStacked) {
+      // ``stacked`` was passed in as a prop so this component is being used as a
+      // controlled component; rely on this prop instead of setting ``stacked`` state below.
+      return;
+    }
 
-      // TODO: what to do about hardcoded width breakpoints...?
-      if (width < CARD_FOOTER_BREAKPOINTS.STACKED) {
-        setStacked(true);
-      } else {
-        setStacked(false);
-      }
-    },
-    [controlledStacked],
-  );
+    // TODO: what to do about hardcoded width breakpoints...?
+    if (width < (breakpoints?.stacked || CARD_FOOTER_BREAKPOINTS.STACKED)) {
+      setStacked(true);
+    } else {
+      setStacked(false);
+    }
+  };
 
   const { ref } = useResizeDetector({
     handleHeight: false,
@@ -45,6 +43,9 @@ const CardFooter = ({
   });
 
   const reversedChildren = useReversedChildren(children);
+
+  // ``contextValue`` must be memoized to ensure the object reference stays consistent on re-renders,
+  // so that children of CardFooterContext do not constantly re-render, too.
   const contextValue = useMemo(
     () => ({
       stacked,
@@ -71,11 +72,18 @@ CardFooter.propTypes = {
   className: PropTypes.string,
   children: PropTypes.node.isRequired,
   stacked: PropTypes.bool,
+  breakpoints: PropTypes.shape({
+    // px width at which the component will switch to stacked view
+    stacked: PropTypes.number.isRequired,
+  }),
 };
 
 CardFooter.defaultProps = {
   className: undefined,
   stacked: undefined, // the internals of ``CardFooter`` will decide the initial default state.
+  breakpoints: {
+    stacked: CARD_FOOTER_BREAKPOINTS.STACKED,
+  },
 };
 
 export default CardFooter;
