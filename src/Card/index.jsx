646eb262779e23c55ac5a71d0aa050ef9136305c
Adam Stankiewicz <astankiewicz@edx.org>
2021-02-03 17:38:55 -0500

    feat: CardGrid to organize collection of cards in responsive grid (#614)

--- src/Card/index.jsx
+++ src/Card/index.jsx
@@ -3,3 +3,5 @@ export { default as CardColumns } from 'react-bootstrap/CardColumns';
 export { default as CardDeck } from 'react-bootstrap/CardDeck';
 export { default as CardImg } from 'react-bootstrap/CardImg';
 export { default as CardGroup } from 'react-bootstrap/CardGroup';
+
+export { default as CardGrid } from './CardGrid';
