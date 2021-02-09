ebe253424233e399b5d22d22716572853521ccde
Ben Warzeski <bwarzeski@edx.org>
2021-02-09 18:24:04 -0500

    feat: new sheet component (#625)
    * feat: new sheet component
    
    * fix: clean up and re-run tests
    
    * fix: add configured z-index variables for sheet and scrim
    
    * feat: use focus-on-react for focus locking
    
    * fix: clean up css re-pr comments
    
    * fix: clean up focus-on-react usage
    
    * fix: move aria keys out of js interp

---
+++ src/Sheet/Sheet.test.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Sheet, { POSITIONS, VARIANTS } from './index';
+
+/* eslint-disable react/prop-types */
+jest.mock('./SheetContainer', () => (props) => {
+  const { children, ...otherProps } = props;
+  return (
+    <sheet-container {...otherProps}>
+      {children}
+    </sheet-container>
+  );
+});
+
+jest.mock('react-focus-on', () => ({
+  FocusOn: (props) => {
+    const { children, ...otherProps } = props;
+    return (
+      <focus-on {...otherProps}>{children}</focus-on>
+    );
+  },
+}));
+
+const testContent = (<div className="sheet-content">Hi</div>);
+
+const renderJSON = (jsxContent) => renderer.create(jsxContent).toJSON();
+
+describe('<Sheet />', () => {
+  describe('snapshots', () => {
+    test('default args snapshot: bottom, show, light', () => {
+      const el = renderJSON(<Sheet>{testContent}</Sheet>);
+      expect(el).toMatchSnapshot();
+    });
+
+    test('blocking, left snapshot', () => {
+      expect(
+        renderJSON(<Sheet blocking position={POSITIONS.left} />),
+      ).toMatchSnapshot();
+    });
+
+    test('dark, right snapshot', () => {
+      expect(
+        renderJSON(<Sheet position={POSITIONS.right} variant={VARIANTS.dark} />),
+      ).toMatchSnapshot();
+    });
+  });
+
+  it('returns empty render iff show is false', () => {
+    expect(renderJSON(<Sheet show={false} />)).toEqual(null);
+    expect(renderJSON(<Sheet />)).not.toEqual(null);
+  });
+});
---
+++ src/Sheet/index.jsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { FocusOn } from 'react-focus-on';
+
+import SheetContainer from './SheetContainer';
+
+export const POSITIONS = {
+  left: 'left',
+  right: 'right',
+  top: 'top',
+  bottom: 'bottom',
+};
+
+export const VARIANTS = {
+  light: 'light',
+  dark: 'dark',
+};
+
+class Sheet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.renderSheet = this.renderSheet.bind(this);
+  }
+
+  renderSheet() {
+    const { children, position, variant } = this.props;
+    return (
+      <div
+        className={classNames(
+          'pgn__sheet-component',
+          `pgn__sheet__${variant}`,
+          position,
+        )}
+        role="alert"
+        aria-live="polite"
+        aria-atomic="true"
+      >
+        <div className="pgn__sheet-content">
+          { children }
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    const {
+      blocking,
+      show,
+    } = this.props;
+    if (!show) {
+      return null;
+    }
+    return (
+      <SheetContainer>
+        <div
+          className={classNames(
+            'pgn__sheet-skrim',
+            { hidden: !blocking },
+          )}
+          role="presentation"
+        />
+        <FocusOn enabled={this.props.blocking}>
+          {this.renderSheet()}
+        </FocusOn>
+      </SheetContainer>
+    );
+  }
+}
+
+Sheet.propTypes = {
+  /** specifies whether the sheet provides a click-blocking scrim */
+  blocking: PropTypes.bool,
+  /** an element rendered inside the sheet */
+  children: PropTypes.node,
+  /** a string designating the sheet's position on the window */
+  position: PropTypes.oneOf([
+    POSITIONS.left,
+    POSITIONS.right,
+    POSITIONS.top,
+    POSITIONS.bottom,
+  ]),
+  /** Boolean used to control whether the Sheet shows. */
+  show: PropTypes.bool,
+  /** a string designating which version of the sheet to show (light vs dark) */
+  variant: PropTypes.oneOf([VARIANTS.light, VARIANTS.dark]),
+};
+
+Sheet.defaultProps = {
+  blocking: false,
+  children: undefined,
+  position: POSITIONS.bottom,
+  show: true,
+  variant: VARIANTS.light,
+};
+
+export default Sheet;
