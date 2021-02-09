15d891c6a4f5ca3b6d9a8969d95d8a01fbf71a10
Adam Butterworth <abutterworth@edx.org>
2021-02-09 15:04:06 -0500

    fix: move Container component to own directory

--- src/Container/Container.test.jsx
+++ src/Container/Container.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import { Container } from './index';
+import Container from './index';
 
 const getClassNames = container => container.props().className.split(' ');
 
