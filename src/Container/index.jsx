2a70e6d49259a1c530490139619735fb0d8f6813
Adam Butterworth <abutterworth@edx.org>
2021-02-09 16:10:36 -0500

    feat: add container sizes (#624)
    Introduce the Container 'size' prop. Add max-width css utilities .mw-sm, .mw-md, etc. Add theming variables for max widths.

---
+++ src/Container/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import classNames from 'classnames';
+import RBContainer from 'react-bootstrap/Container';
+import PropTypes from 'prop-types';
+
+const SIZE_CLASS_NAMES = {
+  xs: 'container-mw-xs',
+  sm: 'container-mw-sm',
+  md: 'container-mw-md',
+  lg: 'container-mw-lg',
+  xl: 'container-mw-xl',
+};
+
+const Container = ({ size, children, ...props }) => (
+  <RBContainer
+    {...props}
+    className={classNames(
+      props.className,
+      SIZE_CLASS_NAMES[size],
+    )}
+  >
+    {children}
+  </RBContainer>
+);
+
+Container.propTypes = {
+  ...RBContainer.propTypes,
+  /** Override the base element */
+  as: PropTypes.elementType,
+  children: PropTypes.node,
+  /** Fill all available space at any breakpoint */
+  fluid: PropTypes.bool,
+  /** Set the maximum width for the container */
+  size: PropTypes.oneOf(Object.keys(SIZE_CLASS_NAMES)),
+};
+
+Container.defaultProps = {
+  as: 'div',
+  children: undefined,
+  fluid: true,
+  size: undefined,
+};
+
+export default Container;
