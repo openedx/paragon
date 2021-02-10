f18bb56957ba0850fb63d7b27b9049344e606e14
Lael Birch <lbirch@edx.org>
2021-02-10 09:27:52 -0500

    DropdownFilter and MultiSelectDropdownFilter (#622)
    * fix: make number of breakout filters configrable
    
    * feat: add dropdown filter for DataTable
    
    * refactor: move LabelledCheckbox to it's own file
    
    * feat: add multiselect filter for DataTable
    
    * docs: update filter docs
    
    * fix: modify props
    
    for ease of switching between filter types

--- src/DataTable/filters/tests/MultiSelectDropdownFilter.test.jsx
+++ src/DataTable/filters/tests/MultiSelectDropdownFilter.test.jsx
@@ -1,71 +1,78 @@
 import React from 'react';
 import { mount } from 'enzyme';
 
-import CheckboxFilter from '../CheckboxFilter';
-import Badge from '../../Badge';
+import MultiSelectDropdownFilter from '../MultiSelectDropdownFilter';
+import Badge from '../../../Badge';
 
 const setFilterMock = jest.fn();
-const roan = { name: 'roan', number: 3 };
-const palomino = { name: 'palomino' };
+const roan = { name: 'roan', number: 3, value: 10 };
+const palomino = { name: 'palomino', value: 2 };
 const props = {
   column: {
     filterValue: [],
     setFilter: setFilterMock,
     Header: 'Horse colors',
-    checkboxFilters: [
+    filterChoices: [
       roan,
       palomino,
-      { name: 'dappled grey', number: 4 },
+      { name: 'dappled grey', number: 4, value: 7 },
     ],
     getHeaderProps: () => ({ key: 'foo' }),
   },
 };
 
-describe('<CheckboxFilter />', () => {
+describe('<MultiSelectDropdownFilter />', () => {
   beforeEach(() => {
     jest.resetAllMocks();
   });
   it('renders a list of checkboxes', () => {
-    const wrapper = mount(<CheckboxFilter {...props} />);
+    const wrapper = mount(<MultiSelectDropdownFilter {...props} />);
+    wrapper.find('button').simulate('click');
     const checkbox = wrapper.find({ type: 'checkbox' }).find('input');
     expect(checkbox.length).toEqual(3);
   });
   it('renders a title', () => {
-    const wrapper = mount(<CheckboxFilter {...props} />);
+    const wrapper = mount(<MultiSelectDropdownFilter {...props} />);
     expect(wrapper.text()).toContain(props.column.Header);
   });
   it('sets a filter - no initial filters', () => {
-    const wrapper = mount(<CheckboxFilter {...props} />);
+    const wrapper = mount(<MultiSelectDropdownFilter {...props} />);
+    wrapper.find('button').simulate('click');
     const checkbox = wrapper.find({ type: 'checkbox' }).find('input').at(1);
     checkbox.simulate('change');
-    expect(setFilterMock).toHaveBeenCalledWith([palomino.name]);
+    expect(setFilterMock).toHaveBeenCalledWith([palomino.value]);
   });
   it('sets a filter - initial filters', () => {
-    const wrapper = mount(<CheckboxFilter column={{ ...props.column, filterValue: [roan.name] }} />);
+    const wrapper = mount(<MultiSelectDropdownFilter column={{ ...props.column, filterValue: [roan.value] }} />);
+    wrapper.find('button').simulate('click');
     const checkbox = wrapper.find({ type: 'checkbox' }).find('input').at(1);
     checkbox.simulate('change');
-    expect(setFilterMock).toHaveBeenCalledWith([roan.name, palomino.name]);
+    expect(setFilterMock).toHaveBeenCalledWith([roan.value, palomino.value]);
   });
   it('removes a filter', () => {
-    const wrapper = mount(<CheckboxFilter column={{ ...props.column, filterValue: [palomino.name] }} />);
+    const wrapper = mount(<MultiSelectDropdownFilter column={{ ...props.column, filterValue: [palomino.value] }} />);
+    wrapper.find('button').simulate('click');
     const checkbox = wrapper.find({ type: 'checkbox' }).find('input').at(1);
     checkbox.simulate('change');
     expect(setFilterMock).toHaveBeenCalledWith([]);
   });
   it('renders checkbox label with filter name', () => {
-    const wrapper = mount(<CheckboxFilter column={{ ...props.column, filterValue: ['roan'] }} />);
+    const wrapper = mount(<MultiSelectDropdownFilter column={{ ...props.column, filterValue: [roan.value] }} />);
+    wrapper.find('button').simulate('click');
     const label = wrapper.find('.form-check-label').at(0);
     expect(label.text()).toContain(roan.name);
   });
   it('renders checkbox label with number', () => {
-    const wrapper = mount(<CheckboxFilter column={{ ...props.column, filterValue: ['roan'] }} />);
+    const wrapper = mount(<MultiSelectDropdownFilter column={{ ...props.column, filterValue: [roan.value] }} />);
+    wrapper.find('button').simulate('click');
     const label = wrapper.find('.pgn__checkbox-filter').at(0);
     const badge = label.find(Badge);
     expect(badge).toHaveLength(1);
     expect(badge.text()).toEqual(String(roan.number));
   });
   it('renders checkbox label with number', () => {
-    const wrapper = mount(<CheckboxFilter column={{ ...props.column, filterValue: ['roan'] }} />);
+    const wrapper = mount(<MultiSelectDropdownFilter column={{ ...props.column, filterValue: [roan.value] }} />);
+    wrapper.find('button').simulate('click');
     const label = wrapper.find('.pgn__checkbox-filter').at(1);
     const badge = label.find(Badge);
     expect(badge).toHaveLength(0);
