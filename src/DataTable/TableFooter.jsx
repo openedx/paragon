e018bc2e32a54c57fa3e459ebe54cde93e511b39
Lael Birch <lbirch@edx.org>
2021-02-18 09:38:26 -0500

    feat: make TableFooter more composeable (#634)
    * feat: make TableFooter more composeable
    
    Users can pass whatever components they want to be displayed in the footer
    Defaults to the current RowStatus component on the left and TablePagination on the right
    
    * docs: update docs for new approach

--- src/DataTable/TableFooter.jsx
+++ src/DataTable/TableFooter.jsx
@@ -1,15 +1,33 @@
 /* eslint-disable react/require-default-props */
 import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
 import RowStatus from './RowStatus';
 import TablePagination from './TablePagination';
 
-const TableFooter = () => (
-  <div className="pgn__data-table-footer">
-    <RowStatus
-      className="pgn__data-table-footer-row-status"
-    />
-    <TablePagination />
+const TableFooter = ({ className, children }) => (
+  <div className={classNames(className, 'pgn__data-table-footer')}>
+    {children}
   </div>
 );
+
+TableFooter.defaultProps = {
+  children: (
+    <>
+      <TablePagination />
+      <RowStatus />
+    </>
+  ),
+  className: null,
+};
+
+TableFooter.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.node])),
+  ]),
+};
 
 export default TableFooter;
