e6a86a99badee700aa332e32651d3ce33859c3a1
Lael Birch <lbirch@edx.org>
2021-02-18 09:38:26 -0500

    feat: make TableFooter more composeable (#634)
    * feat: make TableFooter more composeable
    
    Users can pass whatever components they want to be displayed in the footer
    Defaults to the current RowStatus component on the left and TablePagination on the right
    
    * docs: update docs for new approach

---
+++ src/DataTable/tests/TableFooter.test.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import TableFooter from '../TableFooter';
+import RowStatus from '../RowStatus';
+import TablePagination from '../TablePagination';
+import DataTableContext from '../DataTableContext';
+
+const footerInstance = {
+  previousPage: () => {},
+  nextPage: () => {},
+  canPreviousPage: true,
+  canNextPage: true,
+  state: { pageIndex: 1 },
+  pageCount: 3,
+  itemCount: 30,
+};
+
+// eslint-disable-next-line react/prop-types
+const TableFooterWrapper = ({ value = footerInstance, props = {}, children }) => (
+  <DataTableContext.Provider value={value}>
+    <TableFooter {...props}>{children}</TableFooter>
+  </DataTableContext.Provider>
+);
+
+describe('<TableFooter />', () => {
+  it('Renders the default footer', () => {
+    const wrapper = mount(<TableFooterWrapper />);
+    expect(wrapper.find(RowStatus)).toHaveLength(1);
+    expect(wrapper.find(TablePagination)).toHaveLength(1);
+  });
+  it('accepts a class name', () => {
+    const fakeClass = 'fancy-class';
+    const wrapper = mount(<TableFooterWrapper props={{ className: fakeClass }} />);
+    expect(wrapper.find(TableFooter).props().className).toContain(fakeClass);
+  });
+  it('renders a children', () => {
+    const leftText = "I'm on the left";
+    const wrapper = mount(<TableFooterWrapper><div>{leftText}</div></TableFooterWrapper>);
+    expect(wrapper.text()).toContain(leftText);
+  });
+});
