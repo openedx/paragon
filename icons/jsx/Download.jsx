a1241561665701ede1a75c6c54cdc31bf0c92560
Adam Butterworth <abutterworth@edx.org>
2021-03-04 13:38:20 -0500

    feat(icons): add 16 icons (#654)

---
+++ icons/jsx/Download.jsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+function SvgDownload(props) {
+  return (
+    <svg
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z" fill="currentColor" />
+    </svg>
+  );
+}
+
+export default SvgDownload;
