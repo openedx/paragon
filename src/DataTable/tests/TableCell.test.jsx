78270a01301380bf990c5f613dfc77f8a76e2bd1
Lael Birch <lbirch@edx.org>
2021-03-23 17:54:25 -0400

    fix: add headerClassName and cellClassName (#668)
    allow users to customize cell styles easily

--- src/DataTable/tests/TableCell.test.jsx
+++ src/DataTable/tests/TableCell.test.jsx
@@ -6,10 +6,16 @@ import TableCell from '../TableCell';
 const props = {
   getCellProps: () => ({ className: 'red' }),
   render: () => 'Cell data',
+  column: {},
 };
 
 describe('<TableCell />', () => {
-  const wrapper = mount(<table><tbody><tr><TableCell {...props} /></tr></tbody></table>);
+  let wrapper;
+  beforeEach(() => {
+    // Rendering a cell outside of a table causes a warning, so it is wrapped the appropriate html
+    wrapper = mount(<table><tbody><tr><TableCell {...props} /></tr></tbody></table>);
+  });
+
   it('renders a table cell', () => {
     const cell = wrapper.find('td');
     expect(cell.length).toEqual(1);
@@ -21,5 +27,19 @@ describe('<TableCell />', () => {
   it('renders cell content', () => {
     const cell = wrapper.find('td');
     expect(cell.text()).toEqual('Cell data');
+  });
+  it('adds class names to the cell span', () => {
+    const addedClass = 'align-me';
+    wrapper = mount(
+      <table>
+        <tbody>
+          <tr>
+            <TableCell {...{ ...props, column: { cellClassName: addedClass } }} />
+          </tr>
+        </tbody>
+      </table>,
+    );
+    const innerCell = wrapper.find('td span');
+    expect(innerCell.props().className).toContain(addedClass);
   });
 });
