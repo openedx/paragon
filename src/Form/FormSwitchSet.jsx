d16b3c7fccef07adbcc411865b1ebf583d0eae0d
Adam Butterworth <abutterworth@edx.org>
2021-04-06 09:53:46 -0400

    Delete FormSwitchSet.jsx

--- src/Form/FormSwitchSet.jsx
+++
@@ -1,6 +0,0 @@
-import FormCheckboxSet from './FormCheckboxSet';
-
-// SwitchSet is an alias for CheckboxSet since under the hood
-// Checkbox and Switch are the same.
-
-export default FormCheckboxSet;
