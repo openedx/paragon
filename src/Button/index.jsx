813b6dea01c4d2a794cf44ccb5dfd89e2b222688
Adam Butterworth <abutterworth@edx.org>
2021-04-26 15:29:13 -0400

    feat: add emotion

--- src/Button/index.jsx
+++ src/Button/index.jsx
@@ -1,5 +1,24 @@
-import Button from 'react-bootstrap/Button';
+// this comment tells babel to convert jsx to calls to a function called jsx instead of React.createElement
+/** @jsx jsx */
+import React from 'react';
+import { css, jsx } from '@emotion/react';
+import RBButton from 'react-bootstrap/Button';
 import ButtonDeprecated from './deprecated';
+
+const Button = React.forwardRef((props, ref) => (
+  <RBButton
+    ref={ref}
+    css={css`
+      border: 2px solid palevioletred;
+      &:hover {
+        border-color: darkred;
+      }
+    `}
+    {...props}
+  >
+    {props.children}
+  </RBButton>
+));
 
 Button.Deprecated = ButtonDeprecated;
 
