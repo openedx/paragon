9cd6650575ed907efdaf93361712dbc00df29b4d
José Ignacio Palma <jignaciopm13@gmail.com>
2021-04-28 13:42:49 -0400

    [BD-35] feat: add iconbefore and iconafter props to button component (#687)

--- src/Button/index.jsx
+++ src/Button/index.jsx
@@ -1,8 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import Button from 'react-bootstrap/Button';
 import ButtonDeprecated from './deprecated';
 
-Button.Deprecated = ButtonDeprecated;
+import Icon from '../Icon';
 
-export default Button;
+const WrappedButton = React.forwardRef(({
+  children,
+  iconAfter,
+  iconBefore,
+  ...props
+}, ref) => (
+  <Button
+    {...props}
+    className={classNames(props.className)}
+    ref={ref}
+  >
+    <span className="btn-icons-container">
+      {iconBefore && <Icon className={classNames('btn-icon-before', props.size && `pgn__icon__${props.size}`)} src={iconBefore} />}
+      {children}
+      {iconAfter && <Icon className={classNames('btn-icon-after', props.size && `pgn__icon__${props.size}`)} src={iconAfter} />}
+    </span>
+  </Button>
+));
+
+WrappedButton.propTypes = {
+  ...Button.propTypes,
+  /** Docstring for the children prop */
+  children: PropTypes.string,
+  /** Docstring for className... A class name to append to the button */
+  className: PropTypes.string,
+  /** An icon component to render.
+   * Example import of a Paragon icon component: `import { Check } from '@edx/paragon/dist/icon';` */
+  iconBefore: PropTypes.node,
+  /** An icon component to render.
+   * Example import of a Paragon icon component: `import { Check } from '@edx/paragon/dist/icon';` */
+  iconAfter: PropTypes.node,
+};
+
+WrappedButton.defaultProps = {
+  ...Button.defaultProps,
+  children: undefined,
+  className: undefined,
+  iconBefore: undefined,
+  iconAfter: undefined,
+};
+
+WrappedButton.Deprecated = ButtonDeprecated;
+
+export default WrappedButton;
 export { default as ButtonGroup } from 'react-bootstrap/ButtonGroup';
 export { default as ButtonToolbar } from 'react-bootstrap/ButtonToolbar';
