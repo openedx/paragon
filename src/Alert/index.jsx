1ca10d677b98def4373198172ce28b5961fc2ac1
Adeel Ehsan <adeel.ehsan@arbisoft.com>
2021-06-09 01:08:53 +0500

    feat: Alert text aligned with heading (#738)
    VAN-499

--- src/Alert/index.jsx
+++ src/Alert/index.jsx
@@ -1,1 +1,40 @@
-export { default } from 'react-bootstrap/Alert';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Alert from 'react-bootstrap/Alert';
+import { Icon } from '..';
+
+const WrappedAlert = React.forwardRef(({
+  children,
+  icon,
+  ...props
+}, ref) => (
+  <Alert
+    {...props}
+    className={`alert-content ${props.className}`}
+    ref={ref}
+  >
+    {icon && <Icon src={icon} className="alert-icon" />}
+    <div>
+      {children}
+    </div>
+  </Alert>
+));
+
+WrappedAlert.propTypes = {
+  ...Alert.propTypes,
+  /** Docstring for the children prop */
+  children: PropTypes.node,
+  /** Docstring for the icon prop... Icon that will be shown in the alert */
+  icon: PropTypes.func,
+};
+
+WrappedAlert.defaultProps = {
+  ...Alert.defaultProps,
+  children: undefined,
+  icon: undefined,
+};
+
+WrappedAlert.Link = Alert.Link;
+WrappedAlert.Heading = Alert.Heading;
+
+export default WrappedAlert;
