d3d15a26bdf790f0af12056801bf3620f3529b2e
=Awais Jibran <awaisdar001@gmail.com>
2021-07-01 14:48:58 +0500

    feat: add new icons

---
+++ icons/jsx/Flag.jsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+function SvgFlag(props) {
+  return (
+    <svg
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M14.4 6L14 4H5v17h2v-7h5.6l.4 2h7V6h-5.6z" fill="currentColor" />
+    </svg>
+  );
+}
+
+export default SvgFlag;
