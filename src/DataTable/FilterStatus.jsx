60374029b752abb56d37bde60aa60154d8480693
Kira Miller <31229189+kiram15@users.noreply.github.com>
2021-07-21 09:45:32 -0400

    feat: making "Filtered by __" text optional (#789)
    * feat: making "Filtered by __" text optional
    
    * refactor: requested PR changes

--- src/DataTable/FilterStatus.jsx
+++ src/DataTable/FilterStatus.jsx
@@ -4,7 +4,7 @@ import { Button } from '..';
 import DataTableContext from './DataTableContext';
 
 const FilterStatus = ({
-  className, variant, size, clearFiltersText, buttonClassName,
+  className, variant, size, clearFiltersText, buttonClassName, showFilteredFields,
 }) => {
   const { state, setAllFilters } = useContext(DataTableContext);
   if (!setAllFilters) {
@@ -12,10 +12,11 @@ const FilterStatus = ({
   }
 
   const filterNames = state.filters ? state.filters.map((filter) => filter.id) : [];
+  const filterTexts = <p>Filtered by {filterNames.join(', ')}</p>;
 
   return (
     <div className={className}>
-      Filtered by {filterNames.join(', ')}.
+      {showFilteredFields && filterTexts}
       <Button
         className={buttonClassName}
         variant={variant}
@@ -34,6 +35,7 @@ FilterStatus.defaultProps = {
   variant: 'link',
   size: 'inline',
   clearFiltersText: 'Clear Filters',
+  showFilteredFields: true,
 };
 
 FilterStatus.propTypes = {
@@ -42,6 +44,7 @@ FilterStatus.propTypes = {
   variant: PropTypes.string,
   size: PropTypes.string,
   clearFiltersText: PropTypes.string,
+  showFilteredFields: PropTypes.bool,
 };
 
 export default FilterStatus;
