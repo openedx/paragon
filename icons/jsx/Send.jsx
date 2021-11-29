5a9209cc21b932dee952b977133389e457d24373
Bianca Severino <bseverino@edx.org>
2021-11-29 13:53:04 -0500

    feat: add Send and Tune icons (#902)

---
+++ icons/jsx/Send.jsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+function SvgSend(props) {
+  return (
+    <svg
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2 .01 7z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
+
+export default SvgSend;
