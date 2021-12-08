600fb51fd748cc016ffa6d9bc51e84c7c398d142
Bianca Severino <bseverino@edx.org>
2021-12-08 10:21:21 -0500

    fix: update ListView icon (#928)

--- icons/jsx/ListView.jsx
+++ icons/jsx/ListView.jsx
@@ -11,7 +11,7 @@ function SvgListView(props) {
       {...props}
     >
       <path
-        d="M3 14h4v-4H3v4zm0 5h4v-4H3v4zM3 9h4V5H3v4zm5 5h13v-4H8v4zm0 5h13v-4H8v4zM8 5v4h13V5H8z"
+        d="M3 13h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm4 4h14v-2H7v2zm0 4h14v-2H7v2zM7 7v2h14V7H7zm-4 6h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm4 4h14v-2H7v2zm0 4h14v-2H7v2zM7 7v2h14V7H7z"
         fill="currentColor"
       />
     </svg>
