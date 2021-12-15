9743f388f757907a87ea72dae70b3f418d42634d
binodpant <bpant@edx.org>
2021-12-15 13:28:05 -0500

    feat: update selector names

--- src/IconButtonToggle/index.jsx
+++ src/IconButtonToggle/index.jsx
@@ -2,27 +2,37 @@ import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { Container } from '..';
 
+/**
+ *
+ * @param {object} args arguments
+ * @param {boolean} args.activeValue the current value of the active/selected iconButton.
+ *                                   if not provided, none of the iconButtons will initially be active
+ * @param {Function} args.onChange callback to call when toggle value changes.
+ *                                 Receives value of the selected toggle button.
+ * @param {Array<IconButton>} args.children children components expected to be IconButton
+ * @returns {React.Component} A React component
+ */
 const IconButtonToggle = ({ activeValue, onChange, children }) => {
   const iconButtons = useMemo(
     () => React.Children.map(children, iconButton => (
-      <>
-        {React.cloneElement(iconButton, {
-          onClick: () => { onChange(iconButton.props.value); },
-          isActive: iconButton.props.value === activeValue,
-        })}
-      </>
+      React.cloneElement(iconButton, {
+        onClick: () => { onChange(iconButton.props.value); },
+        isActive: iconButton.props.value === activeValue,
+        'data-testid': `icon-btn-val-${iconButton.props.value}`,
+      })
     )),
     [children, activeValue],
   );
-  return <Container>{iconButtons}</Container>;
+  return <Container className="icon-button-toggle-container">{iconButtons}</Container>;
 };
 
 IconButtonToggle.defaultProps = {
   onChange: () => {},
+  activeValue: undefined,
 };
 
 IconButtonToggle.propTypes = {
-  activeValue: PropTypes.any.isRequired, // eslint-disable-line react/forbid-prop-types
+  activeValue: PropTypes.any, // eslint-disable-line react/forbid-prop-types
   onChange: PropTypes.func,
   children: PropTypes.node.isRequired,
 };
