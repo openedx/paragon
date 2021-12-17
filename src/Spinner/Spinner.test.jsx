66538ff330eb9142b51e4bb215f07eed0585d414
Adam Stankiewicz <agstanki@gmail.com>
2021-12-17 18:18:19 -0500

    fix: native a11y support for Spinner and improve docs (#976)
    * fix: native a11y support for Spinner and improve docs
    
    * fix: add a11y support for Spinner

---
+++ src/Spinner/Spinner.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Spinner from './index';
+
+describe('Spinner', () => {
+  it('should render a spinner', () => {
+    const wrapper = shallow(<Spinner animation="border" />);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+  it('should render a spinner with screen reader text', () => {
+    const wrapper = shallow(<Spinner animation="border" screenReaderText="loading" />);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
