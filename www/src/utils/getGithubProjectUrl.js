b984ed489eceeb0c1625ee09246a7a68038e5ab8
Adam Stankiewicz <agstanki@gmail.com>
2021-12-21 13:04:43 -0500

    docs: support repository field as string or object in package.json (#985)

--- www/src/utils/getGithubProjectUrl.js
+++ www/src/utils/getGithubProjectUrl.js
@@ -1,5 +1,11 @@
-const getGithubProjectUrl = (repositoryUrl) => {
-  if (!repositoryUrl) {
+const getGithubProjectUrl = (repository) => {
+  let repositoryUrl;
+  if (repository === Object(repository) && repository.url) {
+    repositoryUrl = repository.url;
+  } else if (typeof repository === 'string') {
+    repositoryUrl = repository;
+  } else {
+    // unsupported repository field
     return;
   }
   const parts = repositoryUrl.split('/');
