769e49616fa51f21b11f2ae71455b11571191bc2
Adam Stankiewicz <agstanki@gmail.com>
2022-01-11 10:58:33 -0500

    fix: reduce size to inline on IconButton in ExpandRow

--- src/DataTable/ExpandRow.jsx
+++ src/DataTable/ExpandRow.jsx
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 import { Icon, IconButton } from '..';
 import { ExpandLess, ExpandMore } from '../../icons';
 
+const EXPAND_COLLAPSE_ICON_SIZE = 'inline';
+
 const ExpandRow = ({ row }) => (
   <span {...row.getToggleRowExpandedProps()}>
     {row.isExpanded
-      ? <IconButton src={ExpandLess} iconAs={Icon} alt="Collapse row" />
-      : <IconButton src={ExpandMore} iconAs={Icon} alt="Expand row" />}
+      ? <IconButton src={ExpandLess} iconAs={Icon} alt="Collapse row" size={EXPAND_COLLAPSE_ICON_SIZE} />
+      : <IconButton src={ExpandMore} iconAs={Icon} alt="Expand row" size={EXPAND_COLLAPSE_ICON_SIZE} />}
   </span>
 );
 
