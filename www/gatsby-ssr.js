dfe953d77cf6ad6dc0d0c9f778ec9661db3fbb36
Adam Stankiewicz <agstanki@gmail.com>
2022-02-01 14:34:19 -0500

    docs: recalculate MeasureItem measurements on theme change

--- www/gatsby-ssr.js
+++ www/gatsby-ssr.js
@@ -1,5 +1,18 @@
 const React = require('react');
 const { SettingsContextProvider } = require('./src/context/SettingsContext');
 
-// wrap whole app in theme context
+// wrap whole app in settings context
 exports.wrapRootElement = ({ element }) => <SettingsContextProvider>{element}</SettingsContextProvider>;
+
+// add default openedx theme to <head>
+const headComponents = [
+  <link
+    key="openedx-theme"
+    href="/static/openedx-theme.css"
+    rel="stylesheet"
+    type="text/css"
+  />
+];
+exports.onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents(headComponents);
+}
