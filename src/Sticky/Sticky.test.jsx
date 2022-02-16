e9faa7013cad08aa877e461eda1ccbb54242518a
vzadorozhnii <vladyslav.zadorozhnii@raccoongang.com>
2022-02-16 11:44:11 +0200

    feat: implement Sticky component

---
+++ src/Sticky/Sticky.test.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import renderer from 'react-test-renderer';
+
+import Sticky from './index';
+
+const observe = jest.fn();
+const unobserve = jest.fn();
+
+window.IntersectionObserver = jest.fn(() => ({
+  observe,
+  unobserve,
+}));
+
+describe('<Sticky />', () => {
+  describe('correct rendering', () => {
+    it('renders without props', () => {
+      const tree = renderer.create((
+        <Sticky>Sticky</Sticky>
+      )).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+    it('renders with top positioning', () => {
+      const wrapper = mount(<Sticky>content</Sticky>);
+      const sticky = wrapper.find('.pgn__sticky');
+      expect(sticky.hasClass('pgn__sticky-top')).toEqual(true);
+      wrapper.setProps({ position: 'top' });
+      expect(sticky.hasClass('pgn__sticky-top')).toEqual(true);
+    });
+    it('renders with bottom positioning', () => {
+      const wrapper = mount(<Sticky position="bottom">content</Sticky>);
+      const sticky = wrapper.find('.pgn__sticky');
+      expect(sticky.hasClass('pgn__sticky-bottom')).toEqual(true);
+    });
+    it('renders with offset', () => {
+      const offset = 3.5;
+      const wrapper = mount(<Sticky offset={offset}>content</Sticky>);
+      const sticky = wrapper.find('.pgn__sticky');
+      expect(sticky.hasClass(`pgn__sticky-offset--${offset}`)).toEqual(true);
+    });
+    it('observer is initialized during render and detached during unmount', () => {
+      const wrapper = mount(<Sticky>content</Sticky>);
+      expect(observe).toHaveBeenCalled();
+      wrapper.unmount();
+      expect(unobserve).toHaveBeenCalled();
+    });
+  });
+});
---
+++ src/Sticky/index.jsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useRef, useState } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+const POSITION_VARIANTS = [
+  'top',
+  'bottom',
+];
+
+const Sticky = ({
+  position,
+  children,
+  offset,
+  className,
+}) => {
+  const [isSticky, setIsSticky] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    // getComputedStyle is used to get real top/bottom
+    // values on the page for proper shadows display
+    const elementStyles = window.getComputedStyle(ref.current);
+    const elementOffset = elementStyles[position || 'top'];
+    // Margin calculations according to the offset.
+    // 1 pixel above/bellow + offset pixels that determines
+    // when callback function is called
+    const elementWithOffset = 1 + (parseInt(elementOffset, 10) || 0);
+    const observer = new IntersectionObserver(
+      ([entry]) => setIsSticky(entry.intersectionRatio < 1),
+      {
+        threshold: [1],
+        rootMargin: position === 'bottom'
+          ? `0px 0px -${elementWithOffset}px 0px`
+          : `-${elementWithOffset}px 0px 0px 0px`,
+      },
+    );
+    observer.observe(ref.current);
+
+    return () => {
+      observer.unobserve(ref.current);
+    };
+  }, []);
+
+  return (
+    <div
+      className={classNames(
+        'pgn__sticky',
+        `pgn__sticky-${position || 'top'}`,
+        offset ? `pgn__sticky-offset--${offset}` : '',
+        { 'pgn__sticky-shadow': isSticky },
+        className,
+      )}
+      ref={ref}
+    >
+      {children}
+    </div>
+  );
+};
+
+Sticky.propTypes = {
+  /** Specifies content of the component. */
+  children: PropTypes.node.isRequired,
+  /** Specifies position of the element. */
+  position: PropTypes.oneOf(POSITION_VARIANTS),
+  /**
+   * Specifies offset from top/bottom depending on the `position` property.
+   *
+   * Valid values are based on `the spacing classes`:
+   * `0, 0.5, ... 6`
+   */
+  offset: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  /** Specifies an additional `className` to add to the base element. */
+  className: PropTypes.string,
+};
+
+Sticky.defaultProps = {
+  position: 'top',
+  offset: undefined,
+  className: undefined,
+};
+
+export default Sticky;
