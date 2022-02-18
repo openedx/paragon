04e343ab46e6fe69273584b84146b09bcd933902
Michael Vlasov <vlasovmichael88@gmail.com>
2022-02-18 14:11:07 +0200

    feat: [BD-46] implement new pagination component (#1065)
    * feat: [BD-46] implement new pagination component
    
    * feat: [BD-46] implement new pagination component / review
    
    * feat: [BD-46] implement new pagination component / review
    
    * feat: [BD-46] implement new pagination component / review - IconButton
    
    * feat: [BD-46] implement new pagination component / review - unit test, arrows fixes
    
    Co-authored-by: vlasovmichael <mykhailo.vlasov@raccoongang.com>

---
+++ src/DataTable/TablePaginationMinimal.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react';
+import { Pagination } from '..';
+import DataTableContext from './DataTableContext';
+
+const TablePaginationMinimal = () => {
+  const {
+    nextPage, pageCount, gotoPage, state,
+  } = useContext(DataTableContext);
+
+  // Use nextPage as a proxy for whether or not the table is paginated
+  if (!nextPage) {
+    return null;
+  }
+
+  const pageIndex = state?.pageIndex;
+
+  return (
+    <Pagination
+      variant="minimal"
+      currentPage={pageIndex + 1}
+      pageCount={pageCount}
+      paginationLabel="table pagination"
+      onPageSelect={(pageNum) => gotoPage(pageNum - 1)}
+    />
+  );
+};
+
+export default TablePaginationMinimal;
--- src/Pagination/Pagination.test.jsx
+++ src/Pagination/Pagination.test.jsx
@@ -3,8 +3,10 @@ import { mount } from 'enzyme';
 import { Context as ResponsiveContext } from 'react-responsive';
 import breakpoints from '../utils/breakpoints';
 import Pagination from './index';
+import Dropdown from '../Dropdown';
 
 const baseProps = {
+  state: { pageIndex: 1 },
   paginationLabel: 'pagination navigation',
   pageCount: 5,
   onPageSelect: () => {},
@@ -280,6 +282,25 @@ describe('<Pagination />', () => {
 
       expect(wrapper.find('.btn').at(1).prop('aria-label'))
         .toEqual(`${buttonLabels.page} 1, ${buttonLabels.currentPage}, ${buttonLabels.pageOfCount} 5`);
+    });
+
+    it('for the reduced variant shows dropdown button with the page count as label', () => {
+      wrapper = mount(<Pagination variant="reduced" {...props} />);
+
+      const dropdown = wrapper.find(Dropdown);
+      expect(dropdown.text()).toContain(`${baseProps.state.pageIndex} of ${baseProps.pageCount}`);
+
+      const dropdownButton = wrapper.find('button.dropdown-toggle');
+      dropdownButton.simulate('click');
+      const dropdownChoices = wrapper.find(Dropdown.Item);
+      expect(dropdownChoices.length).toEqual(baseProps.pageCount);
+    });
+
+    it('renders only previous and next buttons in minimal variant', () => {
+      wrapper = mount(<Pagination variant="minimal" />);
+      const items = wrapper.find('li.page-item');
+
+      expect(items.length).toEqual(2);
     });
   });
 });
