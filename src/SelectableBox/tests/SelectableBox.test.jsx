2f4d9827af7481127d4b7d022159024a90e9b0e8
monteri <36768631+monteri@users.noreply.github.com>
2022-02-18 15:33:26 +0200

    feat: [BD-46] implement SelectableBox component (#1030)
    * feat: implement SelectableBox component
    
    * feat: pull request comments
    
    * feat: update snapshot
    
    * feat: fix proptypes
    
    * feat: PR comments changes
    
    * feat: test coverage issue
    
    * feat: test enhancement
    
    * feat: restyling with grid
    
    Co-authored-by: vzadorozhnii <vladyslav.zadorozhnii@raccoongang.com>

---
+++ src/SelectableBox/tests/SelectableBox.test.jsx
@@ -0,0 +1,122 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import renderer from 'react-test-renderer';
+
+import SelectableBox from '../index';
+import { Form } from '../../index';
+
+const checkboxType = 'checkbox';
+const checkboxText = 'SelectableCheckbox';
+
+const radioType = 'radio';
+const radioText = 'SelectableRadio';
+
+const SelectableCheckbox = (props) => (
+  <SelectableBox type={checkboxType} {...props}>{checkboxText}</SelectableBox>
+);
+
+const SelectableRadio = (props) => (
+  <SelectableBox type={radioType} {...props}>{radioText}</SelectableBox>
+);
+
+describe('<SelectableBox />', () => {
+  describe('correct rendering', () => {
+    it('renders without props', () => {
+      const tree = renderer.create((
+        <SelectableBox>SelectableBox</SelectableBox>
+      )).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+    it('correct render when type prop is changed', () => {
+      const boxWrapper = mount(<SelectableBox />);
+      expect(boxWrapper.find(Form.Radio).length).toBeGreaterThan(0);
+      boxWrapper.setProps({ type: 'anytype' });
+      expect(boxWrapper.find(Form.Radio).length).toBeGreaterThan(0);
+      boxWrapper.setProps({ type: 'radio' });
+      expect(boxWrapper.find(Form.Radio).length).toBeGreaterThan(0);
+      boxWrapper.setProps({ type: 'checkbox' });
+      expect(boxWrapper.find(Form.Checkbox).length).toBeGreaterThan(0);
+    });
+    it('renders with radio input type if neither checkbox nor radio is passed', () => {
+      const wrapper = mount(<SelectableBox type="wrongType" />);
+      const selectableBox = wrapper.find('input');
+      expect(selectableBox.prop('type')).toEqual(radioType);
+    });
+    it('renders with checkbox input type', () => {
+      const wrapper = mount(<SelectableCheckbox />);
+      const selectableBox = wrapper.find('input');
+      expect(selectableBox.prop('type')).toEqual(checkboxType);
+    });
+    it('renders with radio input type', () => {
+      const wrapper = mount(<SelectableRadio />);
+      const selectableBox = wrapper.find('input');
+      expect(selectableBox.prop('type')).toEqual(radioType);
+    });
+    it('renders with correct children', () => {
+      const wrapper = mount(<SelectableRadio />);
+      const selectableBox = wrapper.find('.pgn__selectable_box');
+      expect(selectableBox.text()).toContain(radioText);
+    });
+    it('renders with correct class', () => {
+      const className = 'myClass';
+      const wrapper = mount(<SelectableRadio className={className} />);
+      const selectableBox = wrapper.find('.pgn__selectable_box');
+      expect(selectableBox.hasClass(className)).toEqual(true);
+    });
+    it('renders as active when checked is passed', () => {
+      const wrapper = mount(<SelectableRadio checked />);
+      const selectableBox = wrapper.find('.pgn__selectable_box');
+      expect(selectableBox.hasClass('pgn__selectable_box-active')).toEqual(true);
+      expect(selectableBox.find('input').prop('checked')).toEqual(true);
+    });
+    it('renders as invalid when isInvalid is passed', () => {
+      const wrapper = mount(<SelectableRadio isInvalid />);
+      const selectableBox = wrapper.find('.pgn__selectable_box');
+      expect(selectableBox.hasClass('pgn__selectable_box-invalid')).toEqual(true);
+    });
+    it('renders with on click event when onClick is passed', () => {
+      const wrapper = mount(<SelectableCheckbox onClick={undefined} />);
+      wrapper.find('.pgn__selectable_box').simulate('click');
+      const onClickSpy = jest.fn();
+      wrapper.setProps({ onClick: onClickSpy });
+      wrapper.find('.pgn__selectable_box').simulate('click');
+      expect(onClickSpy).toHaveBeenCalledTimes(1);
+    });
+    it('renders with on key press event when onClick is passed', () => {
+      const onClickSpy = jest.fn();
+      const wrapper = mount(<SelectableCheckbox onClick={onClickSpy} />);
+      const selectableBox = wrapper.find('.pgn__selectable_box');
+      selectableBox.simulate('keypress', { key: 'Enter' });
+      expect(onClickSpy).toHaveBeenCalledTimes(1);
+    });
+    it('renders with hidden input when inputHidden is passed', () => {
+      const wrapper = mount(<SelectableCheckbox />);
+      expect(wrapper.find('.pgn__selectable_box input').prop('hidden')).toEqual(true);
+      wrapper.setProps({ inputHidden: false });
+      expect(wrapper.find('.pgn__selectable_box input').prop('hidden')).toEqual(false);
+    });
+  });
+  describe('correct interactions', () => {
+    it('correct checkbox state change when checked is changed', () => {
+      const wrapper = mount(<SelectableCheckbox />);
+      wrapper.setProps({ checked: true });
+      expect(wrapper.find('.pgn__selectable_box').hasClass('pgn__selectable_box-active')).toEqual(true);
+      wrapper.setProps({ checked: false });
+      expect(wrapper.find('.pgn__selectable_box').hasClass('pgn__selectable_box-active')).toEqual(false);
+    });
+    it('correct radio state change when checked is changed', () => {
+      const wrapper = mount(<SelectableRadio />);
+      wrapper.setProps({ checked: true });
+      expect(wrapper.find('.pgn__selectable_box').hasClass('pgn__selectable_box-active')).toEqual(true);
+      wrapper.setProps({ checked: false });
+      expect(wrapper.find('.pgn__selectable_box').hasClass('pgn__selectable_box-active')).toEqual(true);
+    });
+    it('ref is passed to onClick function', () => {
+      let inputRef;
+      const onClick = (ref) => { inputRef = ref; };
+      const wrapper = mount(<SelectableRadio onClick={onClick} />);
+      wrapper.find('.pgn__selectable_box').simulate('click');
+      expect(inputRef).not.toBeFalsy();
+    });
+  });
+});
---
+++ src/SelectableBox/tests/SelectableBoxSet.test.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import renderer from 'react-test-renderer';
+
+import { Form } from '../../index';
+import SelectableBox from '../index';
+
+const checkboxType = 'checkbox';
+const checkboxText = (text) => `SelectableCheckbox${text}`;
+
+const radioType = 'radio';
+const radioText = (text) => `SelectableRadio${text}`;
+
+const SelectableCheckboxSet = (props) => (
+  <SelectableBox.Set name={radioType} type={checkboxType} {...props}>
+    <SelectableBox value={1} type={checkboxType}>{checkboxText(1)}</SelectableBox>
+    <SelectableBox value={2} type={checkboxType}>{checkboxText(2)}</SelectableBox>
+    <SelectableBox value={3} type={checkboxType}>{checkboxText(3)}</SelectableBox>
+  </SelectableBox.Set>
+);
+
+const SelectableRadioSet = (props) => (
+  <SelectableBox.Set name={radioType} type={radioType} {...props}>
+    <SelectableBox value={1} type={radioType}>{radioText(1)}</SelectableBox>
+    <SelectableBox value={2} type={radioType}>{radioText(2)}</SelectableBox>
+    <SelectableBox value={3} type={radioType}>{radioText(3)}</SelectableBox>
+  </SelectableBox.Set>
+);
+
+describe('<SelectableBox.Set />', () => {
+  describe('correct rendering', () => {
+    it('renders without props', () => {
+      const tree = renderer.create((
+        <SelectableBox.Set name="testName">SelectableBox</SelectableBox.Set>
+      )).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+    it('correct render when type prop is changed', () => {
+      const setWrapper = mount(<SelectableBox.Set name="set" />);
+      expect(setWrapper.find(Form.RadioSet).length).toBeGreaterThan(0);
+      setWrapper.setProps({ type: 'anytype' });
+      expect(setWrapper.find(Form.RadioSet).length).toBeGreaterThan(0);
+      setWrapper.setProps({ type: 'radio' });
+      expect(setWrapper.find(Form.RadioSet).length).toBeGreaterThan(0);
+      setWrapper.setProps({ type: 'checkbox' });
+      expect(setWrapper.find(Form.CheckboxSet).length).toBeGreaterThan(0);
+    });
+    it('renders with children', () => {
+      const wrapper = mount(<SelectableBox.Set name="testName">{checkboxText(1)}</SelectableBox.Set>);
+      expect(wrapper.text()).toContain(checkboxText(1));
+    });
+    it('renders with on change event', () => {
+      const onChangeSpy = jest.fn();
+      const wrapper = mount(<SelectableCheckboxSet onChange={onChangeSpy} />);
+      wrapper.props().onChange();
+      expect(onChangeSpy).toHaveBeenCalledTimes(1);
+    });
+    it('renders with checkbox type', () => {
+      const wrapper = mount(<SelectableCheckboxSet />);
+      const selectableBoxSet = wrapper.find(Form.CheckboxSet);
+      expect(selectableBoxSet.length).toEqual(1);
+    });
+    it('renders with radio type if neither checkbox nor radio is passed', () => {
+      const wrapper = mount(<SelectableCheckboxSet type="text" />);
+      const selectableBoxSet = wrapper.find(Form.RadioSet);
+      expect(selectableBoxSet.length).toEqual(1);
+    });
+    it('renders with radio type', () => {
+      const wrapper = mount(<SelectableRadioSet type={radioType} />);
+      const selectableBoxSet = wrapper.find(Form.RadioSet);
+      expect(selectableBoxSet.length).toEqual(1);
+    });
+    it('renders with correct number of columns', () => {
+      const columns = 10;
+      const wrapper = mount(<SelectableRadioSet columns={columns} />);
+      const selectableBoxSet = wrapper.find(Form.RadioSet);
+      expect(selectableBoxSet.hasClass(`pgn__selectable_box-set--${columns}`)).toBe(true);
+    });
+  });
+});
