e84ada250ccd42a367e3ef4c467d82e0a4afec33
monteri <36768631+monteri@users.noreply.github.com>
2022-02-18 15:26:36 +0200

    feat: implement Stack component (#1098)
    Co-authored-by: vzadorozhnii <vladyslav.zadorozhnii@raccoongang.com>

---
+++ src/Stack/index.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+const DIRECTION_VARIANTS = [
+  'horizontal',
+  'vertical',
+];
+
+const Stack = ({
+  direction,
+  gap,
+  children,
+  className,
+}) => (
+  <div
+    className={classNames(
+      direction === 'horizontal' ? 'pgn__hstack' : 'pgn__vstack',
+      gap ? `pgn__stack-gap--${gap}` : '',
+      className,
+    )}
+  >
+    {children}
+  </div>
+);
+
+Stack.propTypes = {
+  /** Specifies the content of the `Stack`. */
+  children: PropTypes.node.isRequired,
+  /** Specifies direction of the children blocks (column/row). */
+  direction: PropTypes.oneOf(DIRECTION_VARIANTS),
+  /**
+   * Specifies inner space between children blocks.
+   *
+   * Valid values are based on `the spacing classes`:
+   * `0, 0.5, ... 6`.
+   */
+  gap: PropTypes.number,
+  /** Specifies an additional `className` to add to the base element. */
+  className: PropTypes.string,
+};
+
+Stack.defaultProps = {
+  direction: 'vertical',
+  gap: 0,
+  className: undefined,
+};
+
+export default Stack;
