903e14051435384d6052759fe5216b5b3d874cc2
monteri <36768631+monteri@users.noreply.github.com>
2022-03-01 00:27:18 +0200

    docs: [BD-46] navigation for page headings (#1085)
    * feat: navigation for page headings
    
    * feat: PR comments changes
    
    Co-authored-by: vzadorozhnii <vladyslav.zadorozhnii@raccoongang.com>

--- www/gatsby-browser.js
+++ www/gatsby-browser.js
@@ -3,3 +3,15 @@ const { SettingsContextProvider } = require('./src/context/SettingsContext');
 
 // wrap whole app in settings context
 exports.wrapRootElement = ({ element }) => <SettingsContextProvider>{element}</SettingsContextProvider>;
+
+exports.onRouteUpdate = ({ location }) => {
+  if (location.hash) {
+    setTimeout(() => {
+      const id = location.href.split('#')[1];
+      const element = document.getElementById(id);
+      if (element) {
+        window.scrollTo({ top: element.offsetTop })
+      }
+    }, 0);
+  }
+}
