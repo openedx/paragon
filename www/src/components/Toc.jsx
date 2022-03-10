314a5d5228f2e9444ab8e360ddc40871d5976e50
vzadorozhnii <vladyslav.zadorozhnii@raccoongang.com>
2022-03-10 15:26:59 +0200

    feat: remove recursive toc container

--- www/src/components/Toc.jsx
+++ www/src/components/Toc.jsx
@@ -5,24 +5,28 @@ import classNames from 'classnames';
 const Toc = ({ data, className }) => {
   const generateTree = (headings) => (headings?.items?.length
     ? (
-      <div className={classNames(className, 'pgn-doc__toc')}>
-        <p className="pgn-doc__toc-header">Contents</p>
-        <ul className="pgn-doc__toc-list">
-          {headings.items.map(heading => (
-            <li key={heading.url}>
-              <a href={heading.url}>{heading.title}</a>
-              {!!heading.items && (
+      <ul className="pgn-doc__toc-list">
+        {headings.items.map(heading => (
+          <li key={heading.url}>
+            <a href={heading.url}>{heading.title}</a>
+            {!!heading.items && (
               <ul className="pgn-doc__toc-list">
                 {generateTree(heading)}
               </ul>
-              )}
-            </li>
-          ))}
-        </ul>
-      </div>
+            )}
+          </li>
+        ))}
+      </ul>
     ) : null);
 
-  return generateTree(data);
+  const tocTree = generateTree(data);
+
+  return tocTree ? (
+    <div className={classNames(className, 'pgn-doc__toc')}>
+      <p className="pgn-doc__toc-header">Contents</p>
+      {tocTree}
+    </div>
+  ) : null;
 };
 
 const itemsShape = {
