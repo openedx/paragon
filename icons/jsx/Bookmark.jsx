6338846fd07e4300dabd7797531b21ed7d4e2cfb
Kshitij Sobti <kshitij@sobti.in>
2022-03-11 19:12:21 +0530

    feat: add horizontal variant of more icon, and "issue" and "bookmark" icons (#1142)

---
+++ icons/jsx/Bookmark.jsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+function SvgBookmark(props) {
+  return (
+    <svg
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M5 21V3h14v18l-7-3-7 3z" fill="currentColor" />
+    </svg>
+  );
+}
+
+export default SvgBookmark;
