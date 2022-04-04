1adeeab4b13c1e243054813a93550e62d6b73c4c
Adam Stankiewicz <agstanki@gmail.com>
2022-04-04 01:19:07 -0400

    fix: add container to 404 page

--- www/src/pages/404.jsx
+++ www/src/pages/404.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Container } from '~paragon-react';
 
 import Layout from '../components/PageLayout';
 import SEO from '../components/SEO';
 
 const NotFoundPage = () => (
   <Layout>
-    {/* eslint-disable-next-line react/jsx-pascal-case */}
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Container size="md" className="py-5">
+      {/* eslint-disable-next-line react/jsx-pascal-case */}
+      <SEO title="404: Not found" />
+      <h1>404: Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Container>
   </Layout>
 );
 
