3932b1bfa2448a32029829904519b04ff0b96b4f
monteri <36768631+monteri@users.noreply.github.com>
2022-05-06 13:15:06 +0300

    feat: [BD-46] create consumer app (#1257)
    * feat: create consumer app
    
    * feat: changes according to PR comments
    
    * feat: rebase and install-all
    
    Co-authored-by: vzadorozhnii <vladyslav.zadorozhnii@raccoongang.com>

---
+++ example/src/MyComponent.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { Button, Form, Icon } from '@edx/paragon'; // eslint-disable-line
+import { FavoriteBorder } from '@edx/paragon/icons'; // eslint-disable-line
+
+const MyComponent = () => {
+  const [value, setValue] = useState('');
+  const handleChange = (e) => setValue(e.target.value);
+  // eslint-disable-next-line no-alert
+  const handleClick = () => alert('Form is submitted!');
+
+  return (
+    <div className="p-5">
+      <h1>My Form</h1>
+      <Form>
+        <Form.Group>
+          <Form.Control
+            value={value}
+            onChange={handleChange}
+            leadingElement={<Icon src={FavoriteBorder} />}
+            floatingLabel="My component"
+          />
+        </Form.Group>
+        <Button onClick={handleClick}>Submit</Button>
+      </Form>
+    </div>
+  );
+};
+
+export default MyComponent;
