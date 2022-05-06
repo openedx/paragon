1e0cfd9531d075782fa6aaf8a48964bfd69fc678
Peter Kulko <93188219+PKulkoRaccoonGang@users.noreply.github.com>
2022-05-06 12:51:07 +0300

    feat: [BD-46] add support for React 17 (#1248)

--- src/setupTest.js
+++ src/setupTest.js
@@ -1,6 +1,6 @@
 import 'regenerator-runtime/runtime';
 
 import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 Enzyme.configure({ adapter: new Adapter() });
