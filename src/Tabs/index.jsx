75311f69d37f7016b08956f7c63a9194b9c63269
monteri <36768631+monteri@users.noreply.github.com>
2022-05-27 13:41:35 +0300

    feat: [BD-46] exports useIndexOfLastVisibleChild, responsive tabs (#1287)
    * Adds new React hook `useIndexOfLastVisibleChild` to provide a mechanism to determine the last element in a container, accounting for when the screen width changes.
    * Utilizes `useIndexOfLastVisibleChild` in the `Tabs` component to ensure the tabs don't wrap; that is, they now remain on a single line and show a "More" dropdown when there is overflow.

--- src/Tabs/index.jsx
+++ src/Tabs/index.jsx
@@ -1,21 +1,83 @@
-import React from 'react';
+import React, { useEffect, useMemo, useRef } from 'react';
 import BaseTabs from 'react-bootstrap/Tabs';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import TabsDeprecated from './deprecated';
 import Bubble from '../Bubble';
+import Dropdown from '../Dropdown';
+import useIndexOfLastVisibleChild from '../hooks/useIndexOfLastVisibleChild';
+import Tab from './Tab';
+
+export const MORE_TAB_TEXT = 'More...';
 
 const Tabs = ({
   children,
   className,
+  moreTabText = MORE_TAB_TEXT,
+  defaultActiveKey,
+  activeKey,
   ...props
-}) => (
-  <BaseTabs {...props} className={classNames(className, 'pgn__tabs')}>
-    {React.Children.map(children, (child) => {
+}) => {
+  const containerElementRef = useRef(null);
+  const overflowElementRef = useRef(null);
+  const indexOfLastVisibleChild = useIndexOfLastVisibleChild(
+    containerElementRef.current?.children[0],
+    overflowElementRef.current?.parentNode,
+  );
+
+  useEffect(() => {
+    if (containerElementRef.current) {
+      const observer = new MutationObserver((mutations => {
+        mutations.forEach(mutation => {
+          // React-Bootstrap attribute 'data-rb-event-key' is responsible for the tab identification
+          const eventKey = mutation.target.getAttribute('data-rb-event-key');
+          // React-Bootstrap attribute 'aria-selected' is responsible for selected/unselected state
+          const isActive = mutation.target.getAttribute('aria-selected') === 'true';
+          // datakey attribute is added manually to the dropdown
+          // elements so that they correspond to the native tabs' eventKey
+          const element = containerElementRef.current.querySelector(`[datakey='${eventKey}']`);
+          const moreTab = containerElementRef.current.querySelector('.pgn__tab_more');
+          if (isActive) {
+            element?.classList.add('active');
+            // Here we add active class to the 'More Tab' if element exists in the dropdown
+            if (element) {
+              moreTab.classList.add('active');
+            } else {
+              moreTab.classList.remove('active');
+            }
+          } else {
+            element?.classList.remove('active');
+          }
+        });
+      }));
+      observer.observe(containerElementRef.current, {
+        attributes: true, subtree: true, attributeFilter: ['aria-selected'],
+      });
+      return () => observer.disconnect();
+    }
+    return undefined;
+  }, []);
+
+  useEffect(() => {
+    if (overflowElementRef.current?.parentNode) {
+      overflowElementRef.current.parentNode.tabIndex = -1;
+    }
+  }, [overflowElementRef.current?.parentNode]);
+
+  const handleDropdownTabClick = (eventKey) => {
+    const hiddenTab = containerElementRef.current.querySelector(`[data-rb-event-key='${eventKey}']`);
+    hiddenTab.click();
+  };
+
+  const tabsChildren = useMemo(() => {
+    const indexOfOverflowStart = indexOfLastVisibleChild + 1;
+    const childrenList = React.Children.map(children, (child, index) => {
       if (!React.isValidElement(child)) {
         return child;
       }
-      const { title, notification, ...rest } = child.props;
+      const {
+        title, notification, tabClassName, ...rest
+      } = child.props;
       let newTitle;
       if (notification) {
         newTitle = (
@@ -27,27 +89,97 @@ const Tabs = ({
       } else {
         newTitle = title;
       }
-      const modifiedTab = React.cloneElement(child, { ...rest, title: newTitle });
+      const tabClass = index > indexOfLastVisibleChild ? 'pgn__tab_invisible' : '';
+      const modifiedTab = React.cloneElement(child, {
+        ...rest,
+        title: newTitle,
+        tabClassName: classNames(tabClass, tabClassName),
+      });
       return modifiedTab;
-    })}
-  </BaseTabs>
-);
+    });
+    let moreTabHasNotification = false;
+    const overflowChildren = childrenList.slice(indexOfOverflowStart)
+      .map(overflowChild => {
+        if (!moreTabHasNotification && overflowChild.props.notification) {
+          moreTabHasNotification = true;
+        }
+        return (
+          <Dropdown.Item
+            key={`${overflowChild.props.eventKey}overflow`}
+            onClick={() => handleDropdownTabClick(overflowChild.props.eventKey)}
+            disabled={overflowChild.props.disabled}
+            datakey={overflowChild.props.eventKey}
+            className={classNames({
+              active: overflowChild.props.eventKey === defaultActiveKey || overflowChild.props.eventKey === activeKey,
+            }, 'pgn__tabs__dropdown-item')}
+          >
+            {overflowChild.props.title}
+          </Dropdown.Item>
+        );
+      });
+
+    childrenList.splice(indexOfOverflowStart, 0, (
+      <Tab
+        key="moreTabKey"
+        tabClassName={classNames(!overflowChildren.length && 'pgn__tab_invisible', 'pgn__tab_more')}
+        title={(
+          <Dropdown ref={overflowElementRef}>
+            <Dropdown.Toggle
+              variant="link"
+              className="nav-link"
+              id="pgn__tab-toggle"
+            >
+              {moreTabText}
+              {moreTabHasNotification && (
+                <Bubble variant="error" className="pgn__tab-notification" />
+              )}
+            </Dropdown.Toggle>
+            <Dropdown.Menu className="dropdown-menu-right">{overflowChildren}</Dropdown.Menu>
+          </Dropdown>
+        )}
+      />
+    ));
+    return childrenList;
+  }, [activeKey, children, defaultActiveKey, indexOfLastVisibleChild, moreTabText]);
+
+  return (
+    <div ref={containerElementRef}>
+      <BaseTabs
+        defaultActiveKey={defaultActiveKey}
+        activeKey={activeKey}
+        {...props}
+        className={classNames(className, 'pgn__tabs')}
+      >
+        {tabsChildren}
+      </BaseTabs>
+    </div>
+  );
+};
 
 Tabs.propTypes = {
   /** Specifies elements that is processed to create tabs. */
   children: PropTypes.node.isRequired,
   /** Specifies class name to append to the base element. */
   className: PropTypes.string,
+  /** Specifies text for the 'More' tab. */
+  moreTabText: PropTypes.string,
+  /** Specifies default active tab (uncontrolled usage). */
+  defaultActiveKey: PropTypes.string,
+  /** Specifies active tab (controlled usage). */
+  activeKey: PropTypes.string,
 };
 
 Tabs.defaultProps = {
   className: undefined,
+  moreTabText: MORE_TAB_TEXT,
+  defaultActiveKey: undefined,
+  activeKey: undefined,
 };
 
 Tabs.Deprecated = TabsDeprecated;
 
 export default Tabs;
-export { default as Tab } from './Tab';
+export { Tab };
 export { default as TabContainer } from 'react-bootstrap/TabContainer';
 export { default as TabContent } from 'react-bootstrap/TabContent';
 export { default as TabPane } from 'react-bootstrap/TabPane';
---
+++ src/hooks/useIndexOfLastVisibleChild.jsx
@@ -0,0 +1,73 @@
+import { useEffect, useLayoutEffect, useState } from 'react';
+
+import useWindowSize from './useWindowSize';
+
+/**
+ * This hook will find the index of the last child of a containing element
+ * that fits within its bounding rectangle. This is done by summing the widths
+ * of the children until they exceed the width of the container.
+ *
+ * @param {Element} containerElementRef - container element
+ * @param {Element} overflowElementRef - overflow element
+ *
+ * The hook returns an array containing:
+ * [indexOfLastVisibleChild, containerElementRef, overflowElementRef]
+ *
+ * indexOfLastVisibleChild - the index of the last visible child
+ * containerElementRef - a ref to be added to the containing html node
+ * overflowElementRef - a ref to be added to an html node inside the container
+ *    that is likely to be used to contain a "More" type dropdown or other
+ *    mechanism to reveal hidden children. The width of this element is always
+ *    included when determining which children will fit or not. Usage of this ref
+ *    is optional.
+ */
+const useIndexOfLastVisibleChild = (containerElementRef, overflowElementRef) => {
+  const [containerWidth, setContainerWidth] = useState(0);
+  const [indexOfLastVisibleChild, setIndexOfLastVisibleChild] = useState(-1);
+  const windowSize = useWindowSize();
+
+  useEffect(() => {
+    if (containerElementRef) {
+      const observer = new ResizeObserver(entries => {
+        for (let i = 0; i < entries.length; i++) {
+          setContainerWidth(entries[i].contentRect.width);
+        }
+      });
+      observer.observe(containerElementRef);
+      return () => observer.disconnect();
+    }
+    return undefined;
+  }, [containerElementRef]);
+
+  useLayoutEffect(() => {
+    if (!containerElementRef) {
+      return;
+    }
+    // Get array of child nodes from NodeList form
+    const childNodesArr = Array.prototype.slice.call(containerElementRef.children);
+    const { nextIndexOfLastVisibleChild } = childNodesArr
+      // filter out the overflow element
+      .filter(childNode => childNode !== overflowElementRef)
+      // sum the widths to find the last visible element's index
+      .reduce((acc, childNode, index) => {
+        acc.sumWidth += childNode.getBoundingClientRect().width;
+        if (acc.sumWidth <= containerWidth) {
+          acc.nextIndexOfLastVisibleChild = index;
+        }
+        return acc;
+      }, {
+        // Include the overflow element's width to begin with. Doing this means
+        // sometimes we'll show a dropdown with one item in it when it would fit,
+        // but allowing this case dramatically simplifies the calculations we need
+        // to do above.
+        sumWidth: overflowElementRef ? overflowElementRef.getBoundingClientRect().width : 0,
+        nextIndexOfLastVisibleChild: -1,
+      });
+
+    setIndexOfLastVisibleChild(nextIndexOfLastVisibleChild);
+  }, [windowSize, containerWidth, containerElementRef, overflowElementRef]);
+
+  return indexOfLastVisibleChild;
+};
+
+export default useIndexOfLastVisibleChild;
