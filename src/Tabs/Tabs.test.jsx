017d309d35b7acb92e88730d0182cfc075c1e0dc
monteri <36768631+monteri@users.noreply.github.com>
2022-05-27 13:41:35 +0300

    feat: [BD-46] exports useIndexOfLastVisibleChild, responsive tabs (#1287)
    * Adds new React hook `useIndexOfLastVisibleChild` to provide a mechanism to determine the last element in a container, accounting for when the screen width changes.
    * Utilizes `useIndexOfLastVisibleChild` in the `Tabs` component to ensure the tabs don't wrap; that is, they now remain on a single line and show a "More" dropdown when there is overflow.

--- src/Tabs/Tabs.test.jsx
+++ src/Tabs/Tabs.test.jsx
@@ -2,28 +2,91 @@ import React from 'react';
 import { mount } from 'enzyme';
 import renderer from 'react-test-renderer';
 
-import Tabs from './index';
+import Tabs, { MORE_TAB_TEXT } from './index';
 import Tab from './Tab';
 
+jest.mock('../hooks/useIndexOfLastVisibleChild', () => ({
+  __esModule: true,
+  default: () => 0,
+}));
+
+let getAttributeValue = 'true';
+
+const disconnect = jest.fn();
+const observeMutation = jest.fn((callback) => callback([{
+  target: {
+    getAttribute: () => getAttributeValue,
+  },
+}]));
+const observeResize = jest.fn();
+const unobserve = jest.fn();
+
+window.MutationObserver = jest.fn((callback) => ({
+  disconnect,
+  observe: () => observeMutation(callback),
+  unobserve,
+}));
+
+window.ResizeObserver = jest.fn(() => ({
+  disconnect,
+  observe: observeResize,
+  unobserve,
+}));
+
+const TabsTestComponent = (props) => (
+  <Tabs {...props} defaultActiveKey="tab_1">
+    <Tab title="Tab 1" notification={4} eventKey="tab_1" />
+    <Tab title="Tab 2" eventKey="tab_2" />
+    <Tab title="Tab 3" eventKey="tab_3" />
+  </Tabs>
+);
+
 describe('<Tabs />', () => {
+  afterEach(() => {
+    document.getElementsByTagName('html')[0].innerHTML = '';
+    observeMutation.mockClear();
+  });
+
   describe('correct rendering', () => {
     it('renders successfully', () => {
-      const tree = renderer.create((
-        <Tabs>
-          <Tab title="Tab 1" />
-          <Tab title="Tab 2" />
-        </Tabs>
-      )).toJSON();
+      const tree = renderer.create(<TabsTestComponent />).toJSON();
       expect(tree).toMatchSnapshot();
     });
     it('renders notification property', () => {
+      const wrapper = mount(<TabsTestComponent />);
+      expect(wrapper.find('.pgn__tab-notification').length).toBeGreaterThan(0);
+    });
+    it('MutationObserver is initialized', () => {
+      mount(<TabsTestComponent />);
+      expect(observeMutation).toHaveBeenCalledTimes(1);
+      getAttributeValue = 'false';
+      mount(<TabsTestComponent />);
+      expect(observeMutation).toHaveBeenCalledTimes(2);
+    });
+    it('dropdown menu is displayed', () => {
+      const wrapper = mount(<TabsTestComponent />);
+      expect(wrapper.find('.pgn__tab_more').at(0).hasClass('pgn__tab_invisible')).toBe(false);
+    });
+    it('moreTabText is displayed', () => {
+      const text = 'Mehr...';
+      const wrapper = mount(<TabsTestComponent />);
+      expect(wrapper.find('#pgn__tab-toggle').at(0).text()).toEqual(MORE_TAB_TEXT);
+      wrapper.setProps({ moreTabText: text });
+      expect(wrapper.find('#pgn__tab-toggle').at(0).text()).toEqual(text);
+    });
+    it('click on the dropdown item activates tab', () => {
+      const wrapper = mount(<TabsTestComponent />);
+      wrapper.find('#pgn__tab-toggle').at(0).simulate('click');
+      wrapper.find('.dropdown-item').at(0).simulate('click');
+      expect(wrapper.find('[data-rb-event-key="tab_2"]').at(0).hasClass('active')).toEqual(true);
+    });
+    it('invalid child does not render', () => {
       const wrapper = mount((
         <Tabs>
-          <Tab title="Tab 1" notification={4} />
-          <Tab title="Tab 2" />
+          {[false, undefined]}
         </Tabs>
       ));
-      expect(wrapper.find('.pgn__tab-notification').length).toBeGreaterThan(0);
+      expect(wrapper.find('nav').children().length).toEqual(1);
     });
   });
 });
