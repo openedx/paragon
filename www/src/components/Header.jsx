789ea29458fca6fa3dbbbdf0c2b10dde49fc87d4
”Peter <peter.kulko@raccoongang.com>
2022-05-29 19:44:37 +0300

    fix: fixing z-index Header

--- www/src/components/Header.jsx
+++ www/src/components/Header.jsx
@@ -138,7 +138,7 @@ const Header = ({ siteTitle, showMinimizedTitle }) => {
       enabled={isOpen}
       onClickOutside={close}
       onEscapeKey={close}
-      className="sticky-top"
+      className="pgn-doc-header sticky-top"
     >
       <div
         className="bg-white"
