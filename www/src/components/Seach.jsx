e50cb902838f31a3c73b95c072962c8861f2f564
Adam Stankiewicz <agstanki@gmail.com>
2022-06-03 11:31:55 -0400

    docs: fix process.env variable name for Algolia env var (#1352)

--- www/src/components/Seach.jsx
+++ www/src/components/Seach.jsx
@@ -4,7 +4,7 @@ import { DocSearch } from '@docsearch/react';
 import '@docsearch/css';
 
 const Search = () => {
-  const algoliaEnvVars = [process.env.GATSBY_ALGOLIA_API_ID, process.env.GATSBY_ALGOLIA_API_KEY, process.env.GATSBY_ALGOLIA_INDEX_NAME];
+  const algoliaEnvVars = [process.env.GATSBY_ALGOLIA_APP_ID, process.env.GATSBY_ALGOLIA_API_KEY, process.env.GATSBY_ALGOLIA_INDEX_NAME];
   if (algoliaEnvVars.some(envVar => !envVar)) {
     // some required environment variables for Algolia not set, so don't render `DocSearch`.
     return null;
