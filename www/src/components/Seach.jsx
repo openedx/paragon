77eddf03f2bb015fca9bdedabbc2c95f23ab94c3
Adam Stankiewicz <agstanki@gmail.com>
2022-06-03 10:05:07 -0400

    docs: add GATSBY_ prefix to algolia env vars

--- www/src/components/Seach.jsx
+++ www/src/components/Seach.jsx
@@ -4,16 +4,16 @@ import { DocSearch } from '@docsearch/react';
 import '@docsearch/css';
 
 const Search = () => {
-  const algoliaEnvVars = [process.env.ALGOLIA_API_ID, process.env.ALGOLIA_API_KEY, process.env.ALGOLIA_INDEX_NAME];
+  const algoliaEnvVars = [process.env.GATSBY_ALGOLIA_API_ID, process.env.GATSBY_ALGOLIA_API_KEY, process.env.GATSBY_ALGOLIA_INDEX_NAME];
   if (algoliaEnvVars.some(envVar => !envVar)) {
     // some required environment variables for Algolia not set, so don't render `DocSearch`.
     return null;
   }
   return (
     <DocSearch
-      appId={process.env.ALGOLIA_APP_ID}
-      indexName={process.env.ALGOLIA_INDEX_NAME}
-      apiKey={process.env.ALGOLIA_API_KEY}
+      appId={process.env.GATSBY_ALGOLIA_APP_ID}
+      indexName={process.env.GATSBY_ALGOLIA_INDEX_NAME}
+      apiKey={process.env.GATSBY_ALGOLIA_API_KEY}
     />
   );
 }
