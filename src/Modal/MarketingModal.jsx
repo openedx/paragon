a95624d0721dcfddb7a201543fb23cf44f07c931
Viktor Rusakov <52399399+viktorrusakov@users.noreply.github.com>
2022-06-10 15:36:41 +0300

    chore: remove airbnb-prop-types dependency and upgrade eslint in docs project (#1367)

--- src/Modal/MarketingModal.jsx
+++ src/Modal/MarketingModal.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { requiredWhenNot } from '../utils/propTypesUtils';
+import { requiredWhenNot } from '../utils/propTypes';
 import ModalDialog from './ModalDialog';
 
 const MarketingModal = ({
