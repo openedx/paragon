81cae2e716f5fe0b40f57216487fcd0db8d3d487
Adam Stankiewicz <agstanki@gmail.com>
2022-06-14 10:54:19 -0400

    fix: remove prop type warning and use only CSS for threshold dot in ProgressBar (#1380)

--- src/Popover/Popover.test.jsx
+++ src/Popover/Popover.test.jsx
@@ -6,17 +6,17 @@ import Popover from './index';
 describe('<Popover />', () => {
   describe('correct rendering', () => {
     it('renders with correct class for variant success', () => {
-      const wrapper = mount(<Popover variant="success" />);
+      const wrapper = mount(<Popover id="popover-id" variant="success" />);
       const popover = wrapper.find('.popover');
       expect(popover.hasClass('popover-success')).toEqual(true);
     });
     it('renders with correct class for variant warning', () => {
-      const wrapper = mount(<Popover variant="warning" />);
+      const wrapper = mount(<Popover id="popover-id" variant="warning" />);
       const popover = wrapper.find('.popover');
       expect(popover.hasClass('popover-warning')).toEqual(true);
     });
     it('renders with correct class for variant danger', () => {
-      const wrapper = mount(<Popover variant="danger" />);
+      const wrapper = mount(<Popover id="popover-id" variant="danger" />);
       const popover = wrapper.find('.popover');
       expect(popover.hasClass('popover-danger')).toEqual(true);
     });
