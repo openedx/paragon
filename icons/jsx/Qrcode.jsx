38f5a8fe9a7a1a80a39bd8c83e82e28dcad46c9a
Viktor Rusakov <52399399+viktorrusakov@users.noreply.github.com>
2022-07-15 15:56:17 +0300

    fix: update script for copying mui-icons and add additional docs (#1449)
    * feat: update script for copying mui-icons and add additional docs
    
    * refactor: add 'Pgn' to icon names that start with a number

--- icons/jsx/Qrcode.jsx
+++ icons/jsx/Qrcode.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-function SvgQrCode(props) {
+function SvgQrcode(props) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -14,4 +14,4 @@ function SvgQrCode(props) {
   );
 }
 
-export default SvgQrCode;
+export default SvgQrcode;
