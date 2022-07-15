2705e0a7a708e4242014421e3babb92766a539cb
Viktor Rusakov <52399399+viktorrusakov@users.noreply.github.com>
2022-07-15 15:56:17 +0300

    fix: update script for copying mui-icons and add additional docs (#1449)
    * feat: update script for copying mui-icons and add additional docs
    
    * refactor: add 'Pgn' to icon names that start with a number

---
+++ icons/es5/Addchart.js
@@ -0,0 +1,18 @@
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
+import * as React from "react";
+
+function SvgAddchart(props) {
+  return /*#__PURE__*/React.createElement("svg", _extends({
+    xmlns: "http://www.w3.org/2000/svg",
+    width: 24,
+    height: 24,
+    viewBox: "0 0 24 24"
+  }, props), /*#__PURE__*/React.createElement("path", {
+    d: "M19 19H5V5h9V3H3v18h18V10h-2z"
+  }), /*#__PURE__*/React.createElement("path", {
+    d: "M11 7h2v10h-2zm4 6h2v4h-2zm-8-3h2v7H7zm12-5V3h-2v2h-2v2h2v2h2V7h2V5z"
+  }));
+}
+
+export default SvgAddchart;
