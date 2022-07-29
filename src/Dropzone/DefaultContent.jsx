a462af19546fe9b8213537dfb80633b6bde8e674
Viktor Rusakov <52399399+viktorrusakov@users.noreply.github.com>
2022-07-29 13:30:09 +0300

    feat: add message about file restrictions to default state of `Dropzone` (#1470)

--- src/Dropzone/DefaultContent.jsx
+++ src/Dropzone/DefaultContent.jsx
@@ -1,21 +1,79 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import PropTypes from 'prop-types';
+import { FormattedMessage, useIntl } from 'react-intl';
+import { getTypesString, formatBytes } from './utils';
+import messages from './messages';
 import Icon from '../Icon';
 import { FileUpload } from '../../icons';
 
-const DefaultContent = () => (
-  <>
-    <div className="pgn__dropzone__upload-icon-wrapper">
-      <Icon src={FileUpload} className="pgn__dropzone__upload-icon" />
-    </div>
-    <p>
-      <FormattedMessage
-        id="pgn.Dropzone.DefaultContent.label"
-        description="A text that appears as a label for input of Dropzone component."
-        defaultMessage="Drag and drop your file here or click to upload."
-      />
-    </p>
-  </>
-);
+const DefaultContent = ({ accept, minSize, maxSize }) => {
+  const intl = useIntl();
+
+  const getFileRestrictionMessage = () => {
+    let fileTypePart;
+    let fileSizePart;
+
+    if (accept) {
+      const allTypes = getTypesString(accept);
+      const lastTypeSeparatorLocation = allTypes.lastIndexOf(',');
+      fileTypePart = intl.formatMessage(messages.fileTypeRestriction, {
+        count: lastTypeSeparatorLocation === -1 ? 1 : 2,
+        firstPart: lastTypeSeparatorLocation === -1 ? allTypes : allTypes.slice(0, lastTypeSeparatorLocation),
+        secondPart: lastTypeSeparatorLocation !== -1 && allTypes.slice(lastTypeSeparatorLocation + 1),
+      });
+    }
+
+    if (minSize && maxSize && Number.isFinite(maxSize)) {
+      fileSizePart = intl.formatMessage(messages.fileSizeBetween, {
+        sizeMin: formatBytes(minSize),
+        sizeMax: formatBytes(maxSize),
+      });
+    } else if (maxSize && Number.isFinite(maxSize)) {
+      fileSizePart = intl.formatMessage(messages.fileSizeMax, { sizeMax: formatBytes(maxSize) });
+    } else if (minSize) {
+      fileSizePart = intl.formatMessage(messages.fileSizeMin, { sizeMin: formatBytes(minSize) });
+    }
+
+    if (fileTypePart && fileSizePart) {
+      return `${fileTypePart} (${fileSizePart})`;
+    }
+    if (fileTypePart) {
+      return fileTypePart;
+    }
+    return fileSizePart;
+  };
+
+  return (
+    <>
+      <div className="pgn__dropzone__upload-icon-wrapper">
+        <Icon src={FileUpload} className="pgn__dropzone__upload-icon" />
+      </div>
+      <p>
+        <FormattedMessage
+          id="pgn.Dropzone.DefaultContent.label"
+          description="A text that appears as a label for input of Dropzone component."
+          defaultMessage="Drag and drop your file here or click to upload."
+        />
+      </p>
+      {[accept, minSize, maxSize].some(value => value) && (
+        <p className="pgn__dropzone__upload-restriction-message">
+          {getFileRestrictionMessage()}
+        </p>
+      )}
+    </>
+  );
+};
+
+DefaultContent.propTypes = {
+  accept: PropTypes.objectOf(PropTypes.arrayOf(PropTypes.string)),
+  maxSize: PropTypes.number,
+  minSize: PropTypes.number,
+};
+
+DefaultContent.defaultProps = {
+  accept: undefined,
+  maxSize: undefined,
+  minSize: undefined,
+};
 
 export default DefaultContent;
--- src/Dropzone/tests/DefaultContent.test.jsx
+++ src/Dropzone/tests/DefaultContent.test.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { IntlProvider } from 'react-intl';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 import DefaultContent from '../DefaultContent';
 
+// eslint-disable-next-line react/prop-types
+const DefaultContentWrapper = ({ children, ...props }) => (
+  <IntlProvider locale="en" messages={{}}>
+    <DefaultContent {...props} />
+  </IntlProvider>
+);
+
 describe('<Dropzone.DefaultContent />', () => {
   it('successfully renders', () => {
-    const tree = renderer.create(<IntlProvider locale="en" messages={{}}><DefaultContent /></IntlProvider>).toJSON();
+    const tree = renderer.create(<DefaultContentWrapper />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+  it('renders file type restriction message if receives accept prop', () => {
+    const { container } = render(<DefaultContentWrapper accept={{ 'image/*': ['.png', '.jpg'] }} />);
+    expect(container).toHaveTextContent('Upload PNG or JPG files');
+  });
+  it('renders min size restriction message if receives minSize prop', () => {
+    const { container } = render(<DefaultContentWrapper minSize={1048576} />);
+    expect(container).toHaveTextContent('Min 1MB');
+  });
+  it('renders max size restriction message if receives maxSize prop', () => {
+    const { container } = render(<DefaultContentWrapper maxSize={1048576} />);
+    expect(container).toHaveTextContent('Max 1MB');
+  });
+  it('renders type and size restriction message if receives all props', () => {
+    const { container } = render(
+      <DefaultContentWrapper
+        accept={{ 'image/*': ['.png', '.jpg', '.gif'] }}
+        minSize={1048576}
+        maxSize={20 * 1048576}
+      />,
+    );
+    expect(container).toHaveTextContent('Upload PNG, JPG or GIF files (Between 1MB and 20MB)');
   });
 });
