e2a728a881d945e16658a7d0900d5b54762e03e9
Peter <peter.kulko@raccoongang.com>
2022-08-03 16:05:08 +0300

    feat: installing storybook and chromatic tools

---
+++ src/Badge/Badge.stories.jsx
@@ -0,0 +1,16 @@
+import Badge from './index';
+import '../../www/public/static/edxorg-theme.css';
+
+export default {
+  title: 'Badge',
+  component: Badge,
+};
+
+const Template = (args) => <Badge {...args}>Success</Badge>;
+export const BadgeExample = Template.bind({});
+BadgeExample.args = {
+  as: 'span',
+  variant: 'primary',
+  pill: false,
+  bsPrefix: 'badge',
+};
