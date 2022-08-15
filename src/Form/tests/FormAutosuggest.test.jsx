ad5d98624d122c11e36c42abd66305181266df45
Peter <peter.kulko@raccoongang.com>
2022-08-16 00:31:32 +0300

    refactor: code refactoring

--- src/Form/tests/FormAutosuggest.test.jsx
+++ src/Form/tests/FormAutosuggest.test.jsx
@@ -41,7 +41,6 @@ describe('FormAutosuggest', () => {
     const newProps = { ...props, handleFocus: mockHandleFocus };
     const container = mount(<FormAutosuggest {...newProps} />);
     container.find('input').simulate('focus');
-
     expect(mockHandleFocus).toHaveBeenCalled();
   });
 
@@ -50,46 +49,43 @@ describe('FormAutosuggest', () => {
     const newProps = { ...props, handleBlur: mockHandleBlur };
     const container = mount(<FormAutosuggest {...newProps} />);
     container.find('input').simulate('blur');
-
     expect(mockHandleBlur).toHaveBeenCalled();
   });
 
   it('renders component with options', () => {
-    const newProps = { ...props, options: ['opt1', 'opt2'] };
+    const newProps = { ...props, options: ['option1', 'option2'] };
     const container = mount(<FormAutosuggest {...newProps} />);
-
     container.find('input').simulate('click');
     container.update();
-    const optionsList = container.find('.dropdown-container').find('button');
+    const optionsList = container.find('.pgn__form-autosuggest__container').find('button');
     expect(optionsList.length).toEqual(newProps.options.length);
   });
 
   it('selects option', () => {
     const newProps = { ...props, options: ['opt1', 'opt2'] };
     const container = mount(<FormAutosuggest {...newProps} />);
-
     container.find('input').simulate('click');
-    container.find('.dropdown-container').find('button').at(0).simulate('click');
+    container.find('.pgn__form-autosuggest__container').find('button').at(0).simulate('click');
     expect(container.find('input').instance().value).toEqual(newProps.options[0]);
   });
 
   it('toggles options list', () => {
-    const newProps = { ...props, options: ['opt1', 'opt2'] };
+    const newProps = { ...props, options: ['option1', 'option2'] };
     const container = mount(<FormAutosuggest {...newProps} />);
-
-    expect(container.find('.dropdown-container').find('button').length).toEqual(0);
+    expect(container.find('.pgn__form-autosuggest__container').find('button').length).toEqual(0);
     container.find('button.expand-more').simulate('click');
-    expect(container.find('.dropdown-container').find('button').length).toEqual(newProps.options.length);
+    expect(container.find('.pgn__form-autosuggest__container').find('button').length).toEqual(newProps.options.length);
     container.find('button.expand-less').simulate('click');
-    expect(container.find('.dropdown-container').find('button').length).toEqual(0);
+    expect(container.find('.pgn__form-autosuggest__container').find('button').length).toEqual(0);
   });
 
   it('shows options list depends on field value', () => {
-    const newProps = { ...props, options: ['opt1opt1opt1opt1opt1opt1opt1opt1opt1opt1opt1', 'opt2'] };
+    const newProps = { ...props, options: ['learn from more than 160 member universities', 'option2'] };
     const container = mount(<FormAutosuggest {...newProps} />);
 
     container.find('input').simulate('change', { target: { value: '1' } });
-    expect(container.find('.dropdown-container').find('button').length).toEqual(1);
+    expect(container.find('.pgn__form-autosuggest__container')
+      .find('button').length).toEqual(1);
   });
 
   it('closes options list on click outside', () => {
@@ -97,10 +93,61 @@ describe('FormAutosuggest', () => {
     const newProps = { ...props, options: ['opt1', 'opt2'] };
     const container = mount(<FormAutosuggest {...newProps} />);
     container.find('input').simulate('click');
-    expect(container.find('.dropdown-container').find('button').length).toEqual(2);
-
+    expect(container.find('.pgn__form-autosuggest__container').find('button').length).toEqual(2);
     fireEvent.click(document.body);
     container.update();
     expect(container.find('.dropdown-container').find('button').length).toEqual(0);
   });
 });
+//
+// describe('FormAutosuggesttest', () => {
+//   let props;
+//   let mockHandleChange;
+//   let mockHandleFocus;
+//   let mockHandleClick;
+//   let mockHandleBlur;
+//
+//   beforeEach(() => {
+//     mockHandleChange = jest.fn();
+//     mockHandleFocus = jest.fn();
+//     mockHandleClick = jest.fn();
+//     mockHandleBlur = jest.fn();
+//     props = {
+//       handleBlur: mockHandleBlur,
+//       handleChange: mockHandleChange,
+//       handleFocus: mockHandleFocus,
+//       handleClick: mockHandleClick,
+//       helpText: 'helpText text',
+//       options: null,
+//       trailingElement: null,
+//       type: 'text',
+//       children: null,
+//       className: '',
+//       floatingLabel: 'floatingLabel text',
+//       name: 'title',
+//       value: '',
+//     };
+//   });
+//   it('handles element focus', () => {
+//     const container = mount(<FormAutosuggest {...props} />);
+//     container.find('input').at(0).simulate('focus');
+//
+//     expect(mockHandleFocus).toHaveBeenCalled();
+//   });
+//
+//   it('handles element blur', () => {
+//     const container = mount(<FormAutosuggest {...props} />);
+//     container.find('input').at(0).simulate('focus');
+//     container.find('input').at(0).simulate('blur');
+//
+//     expect(mockHandleBlur).toHaveBeenCalled();
+//   });
+//
+//   it('handles element click', () => {
+//     const newProps = { ...props, options: ['opt1', 'opt2'] };
+//     const container = mount(<FormAutosuggest {...newProps} />);
+//     container.find('input').at(0).simulate('click');
+//
+//     expect(mockHandleClick).toHaveBeenCalled();
+//   });
+// });
