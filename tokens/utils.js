c2bf50180fe9aa58b066734507145f11e1839d82
vzadorozhnii <vladyslav.zadorozhnii@raccoongang.com>
2022-09-12 13:25:33 +0300

    fix: replace variables script update

--- tokens/utils.js
+++ tokens/utils.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 
-function getFilesWithExtension(location, extension, files = []) {
+function getFilesWithExtension(location, extension, files = [], excludeDirectories = []) {
   const content = fs.statSync(location);
   if (content.isDirectory()) {
     const contentPaths = fs.readdirSync(location);
     contentPaths.forEach(contentPath => {
-      getFilesWithExtension(path.join(location, contentPath), extension, files);
+      if (!excludeDirectories.includes(contentPath)) {
+        getFilesWithExtension(path.join(location, contentPath), extension, files, excludeDirectories);
+      }
     });
   } else if (location.endsWith(extension)) {
     files.push(location);
@@ -33,17 +35,30 @@ function cssVariableWrapper(variable) {
 
 function replaceVariables(filePath, direction = 'css-to-scss') {
   const targetFile = fs.readFileSync(filePath, 'utf-8');
-  const mapFile = fs.readFileSync(path.resolve(__dirname, './build/scss-to-css-map.json'), 'utf-8');
-  const variables = JSON.parse(mapFile);
+  const coreMapFile = fs.readFileSync(path.resolve(__dirname, './build/scss-to-css-core.json'), 'utf-8');
+  const coreVariables = JSON.parse(coreMapFile);
+
   let result = targetFile;
-  Object.keys(variables).forEach(variable => {
-    const cssVariable = cssVariableWrapper(variables[variable]);
+  Object.keys(coreVariables).forEach(variable => {
+    const cssVariable = cssVariableWrapper(coreVariables[variable]);
     if (direction === 'scss-to-css') {
       result = result.replaceAll(variable, cssVariable);
     } else {
       result = result.replaceAll(cssVariable, variable);
     }
   });
+  if (!filePath.endsWith('_variables.scss')) {
+    const componentsMapFile = fs.readFileSync(path.resolve(__dirname, './build/scss-to-css-components.json'), 'utf-8');
+    const componentsVariables = JSON.parse(componentsMapFile);
+    Object.keys(componentsVariables).forEach(variable => {
+      const cssVariable = cssVariableWrapper(componentsVariables[variable]);
+      if (direction === 'scss-to-css') {
+        result = result.replaceAll(variable, cssVariable);
+      } else {
+        result = result.replaceAll(cssVariable, variable);
+      }
+    });
+  }
   fs.writeFileSync(filePath, result);
 }
 
