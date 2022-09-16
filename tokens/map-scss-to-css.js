198282a1a6486b71cf50970cd15a6842e86a3d55
Peter Kulko <93188219+PKulkoRaccoonGang@users.noreply.github.com>
2022-09-16 15:13:07 +0300

    refactor: refactoring css vars

--- tokens/map-scss-to-css.js
+++ tokens/map-scss-to-css.js
@@ -2,26 +2,16 @@ const fs = require('fs');
 const path = require('path');
 const { getSCSStoCSSMap, getFilesWithExtension } = require('./utils');
 
-const CORE_BASE_PATH = path.resolve(__dirname, './source');
-const COMPONENTS_BASE_PATH = path.resolve(__dirname, './source/components');
+const BASE_PATH = path.resolve(__dirname, './source');
 const BASE_PREFIX = '--pgn';
 
-const coreContent = {};
-const componentsContent = {};
-const coreTokenPaths = getFilesWithExtension(CORE_BASE_PATH, '.json', [], ['components']);
-const componentsTokenPaths = getFilesWithExtension(COMPONENTS_BASE_PATH, '.json');
+const result = {};
+const tokenPaths = getFilesWithExtension(BASE_PATH, '.json');
 
-coreTokenPaths.forEach(tokenFile => {
+tokenPaths.forEach(tokenFile => {
   const content = fs.readFileSync(tokenFile, 'utf-8');
   const parsed = JSON.parse(content);
-  getSCSStoCSSMap(BASE_PREFIX, parsed, coreContent);
+  getSCSStoCSSMap(BASE_PREFIX, parsed, result);
 });
 
-componentsTokenPaths.forEach(tokenFile => {
-  const content = fs.readFileSync(tokenFile, 'utf-8');
-  const parsed = JSON.parse(content);
-  getSCSStoCSSMap(BASE_PREFIX, parsed, componentsContent);
-});
-
-fs.writeFileSync(path.resolve(__dirname, './build/scss-to-css-core.json'), JSON.stringify(coreContent));
-fs.writeFileSync(path.resolve(__dirname, './build/scss-to-css-components.json'), JSON.stringify(componentsContent));
+fs.writeFileSync(path.resolve(__dirname, './build/scss-to-css-map.json'), JSON.stringify(result));
