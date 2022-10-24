737947bd729232bbf30d2c88c40d7ca4cc066e96
Peter Kulko <93188219+PKulkoRaccoonGang@users.noreply.github.com>
2022-10-24 17:26:45 +0300

    feat: added translations English strings in FormAutosuggest component

---
+++ src/Form/messages.js
@@ -0,0 +1,16 @@
+import { defineMessages } from 'react-intl';
+
+const messages = defineMessages({
+  iconButtonOpened: {
+    id: 'pgn.FormAutosuggest.iconButtonOpened',
+    defaultMessage: 'Open the options menu',
+    description: 'A message shown in case when the autosuggest menu is closed.',
+  },
+  iconButtonClosed: {
+    id: 'pgn.FormAutosuggest.iconButtonClosed',
+    defaultMessage: 'Close the options menu',
+    description: 'A message shown in case when the autosuggest menu is opened.',
+  },
+});
+
+export default messages;
