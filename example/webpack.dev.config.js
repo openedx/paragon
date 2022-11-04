9245d2dfaf194c176bd46c2afc63feff61b713b4
Viktor Rusakov <52399399+viktorrusakov@users.noreply.github.com>
2022-11-04 13:42:19 +0200

    chore: remove TypeScript dependencies from example app and update frontend-build version (#1735)

--- example/webpack.dev.config.js
+++
@@ -1,8 +0,0 @@
-const { getBaseConfig } = require('@edx/frontend-build');
-
-const config = getBaseConfig('webpack-dev');
-
-config.module.rules[0].test = /\.(js|jsx|ts|tsx)$/;
-config.resolve = { ...config.resolve, extensions: ['.js', '.jsx', '.ts', '.tsx', '...'] };
-
-module.exports = config;
