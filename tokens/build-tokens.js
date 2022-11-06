95c6a1b75d234cbc446fb131dd7398344da2a264
Viktor Rusakov <vrusakov66@gmail.com>
2022-11-06 18:22:26 +0200

    feat: add sass functions design tokens transform

--- tokens/build-tokens.js
+++ tokens/build-tokens.js
@@ -1,23 +1,52 @@
 const StyleDictionary = require('style-dictionary');
 const path = require('path');
+const chroma = require('chroma-js');
 
 const { formattedVariables, fileHeader } = StyleDictionary.formatHelpers;
 
 const PGN_PREFIX = 'pgn';
 const BASE_BUILD_PATH = path.resolve(__dirname, 'build');
 
+const colorTransform = (token) => {
+  const { value, modify = [], original } = token;
+
+  if (original.value.endsWith('%')) {
+    return original.value;
+  }
+
+  let color = chroma(value);
+
+  if (modify.length > 0) {
+    // iterate over the modify array (see tokens/color.json)
+    // and apply each modification in order
+    modify.forEach(({type, amount, otherColor}) => {
+      // modifier type must match a method name in chromajs
+      // https://gka.github.io/chroma.js/
+      // chroma methods can be chained, so each time we override the color variable
+      // we can still call other chroma methods, similar to
+      // chroma(value).brighten(1).darken(1).hex();
+      if (type === 'mix') {
+        color = color[type](otherColor, amount, 'rgb');
+      } else {
+        color = color[type](amount);
+      }
+    });
+  }
+
+  return color.hex('rgba').toUpperCase();
+}
+
 /**
  * Transforms color values to be in uppercase format to be compatible with our stylelint rules.
  */
 StyleDictionary.registerTransform({
-  name: 'color/uppercase',
+  name: 'color/sass-functions',
+  transitive: true,
   type: 'value',
   matcher(token) {
-    return token.attributes.category === 'color';
+    return token.attributes.category === 'color' || token.value.toString().startsWith('#');
   },
-  transformer(token) {
-    return token.original.value.toUpperCase();
-  },
+  transformer: colorTransform,
 });
 
 /**
@@ -50,7 +79,7 @@ const paragonStyleDictionary = StyleDictionary.extend({
           outputReferences: true,
         },
       }],
-      transforms: StyleDictionary.transformGroup.scss.concat('color/uppercase'),
+      transforms: StyleDictionary.transformGroup.scss.concat('color/sass-functions'),
       options: {
         showFileHeader: false,
       },
@@ -74,7 +103,7 @@ const paragonStyleDictionary = StyleDictionary.extend({
           },
         },
       ],
-      transforms: StyleDictionary.transformGroup.css.concat('color/uppercase'),
+      transforms: StyleDictionary.transformGroup.css.concat('color/sass-functions'),
       options: {
         showFileHeader: false,
       },
