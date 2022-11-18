17548759052530c4a2dabf1bf4c107a645a9f57c
Viktor Rusakov <52399399+viktorrusakov@users.noreply.github.com>
2022-11-18 17:21:05 +0200

    feat: alpha release of design tokens (#1770)

--- src/DataTable/CollapsibleButtonGroup.jsx
+++ src/DataTable/CollapsibleButtonGroup.jsx
@@ -79,7 +79,7 @@ function CollapsibleButtonGroup({
             placement="bottom-end"
             isOpen={isOverflowMenuOpen}
           >
-            <div className="pgn__datatable__overflow-actions-menu">
+            <div className="pgn__data-table__overflow-actions-menu">
               <Stack gap={2}>
                 {dropdownActions.map(cloneAction)}
               </Stack>
@@ -87,7 +87,7 @@ function CollapsibleButtonGroup({
           </ModalPopup>
         </>
       )}
-      <div className="pgn__datatable__visible-actions">
+      <div className="pgn__data-table__visible-actions">
         {visibleActions.map(cloneAction)}
       </div>
     </div>
