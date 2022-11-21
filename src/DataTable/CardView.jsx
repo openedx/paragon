5ffc3c706146779ad6530671b3a289755c341df6
Viktor Rusakov <52399399+viktorrusakov@users.noreply.github.com>
2022-11-21 19:01:37 +0200

    feat: add support for selectable CardView in DataTable (#1698)

--- src/DataTable/CardView.jsx
+++ src/DataTable/CardView.jsx
@@ -1,22 +1,52 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import DataTableContext from './DataTableContext';
 import { useRows } from './hooks';
+import { selectColumn } from './utils/getVisibleColumns';
 import { CardGrid } from '..';
 
+function CardItem({
+  row,
+  prepareRow,
+  isSelectable,
+  SelectionComponent,
+  CardComponent,
+  selectionPlacement,
+}) {
+  prepareRow(row);
+  const { isSelected } = row;
+
+  if (isSelectable && SelectionComponent) {
+    return (
+      <div className={classNames(
+        'pgn__data-table__selectable-card',
+        {
+          'is-selected': isSelected,
+          'selection-right': selectionPlacement === 'right',
+          'selection-left': selectionPlacement !== 'right',
+        },
+      )}
+      >
+        <CardComponent {...row} />
+        <SelectionComponent row={row} />
+      </div>
+    );
+  }
+
+  return <CardComponent {...row} />;
+}
+
 function CardView({
-  columnSizes, CardComponent, className,
+  columnSizes, CardComponent, className, selectionPlacement,
 }) {
   const {
     getTableProps, prepareRow, displayRows,
   } = useRows();
-
-  const renderCards = () => displayRows.map((row) => {
-    prepareRow(row);
-    return (
-      <CardComponent {...row} key={row.id} />
-    );
-  });
+  const { isSelectable, manualSelectColumn } = useContext(DataTableContext);
+  // use the same component for card selection that is used for row selection
+  // otherwise view switching might break if row selection uses component that supports backend filtering / sorting
+  const selectionComponent = manualSelectColumn?.Cell || selectColumn.Cell;
 
   if (!getTableProps) {
     return null;
@@ -27,10 +57,36 @@ function CardView({
       className={classNames('pgn__data-table-card-view', className)}
       columnSizes={columnSizes}
     >
-      {renderCards()}
+      {displayRows.map((row) => (
+        <CardItem
+          key={row.id}
+          CardComponent={CardComponent}
+          SelectionComponent={selectionComponent}
+          isSelectable={isSelectable}
+          row={row}
+          prepareRow={prepareRow}
+          selectionPlacement={selectionPlacement}
+        />
+      ))}
     </CardGrid>
   );
 }
+
+CardItem.defaultProps = {
+  SelectionComponent: undefined,
+};
+
+CardItem.propTypes = {
+  row: PropTypes.shape({
+    getToggleRowSelectedProps: PropTypes.func,
+    isSelected: PropTypes.bool,
+  }).isRequired,
+  prepareRow: PropTypes.func.isRequired,
+  isSelectable: PropTypes.bool.isRequired,
+  CardComponent: PropTypes.func.isRequired,
+  SelectionComponent: PropTypes.func,
+  selectionPlacement: PropTypes.oneOf(['right', 'left']).isRequired,
+};
 
 CardView.defaultProps = {
   columnSizes: {
@@ -39,6 +95,7 @@ CardView.defaultProps = {
     xl: 4,
   },
   className: '',
+  selectionPlacement: 'right',
 };
 
 CardView.propTypes = {
@@ -58,6 +115,8 @@ CardView.propTypes = {
   /** Your card component must be individualized to your table.
    * It will be called with props from the "row" of data it will display */
   CardComponent: PropTypes.func.isRequired,
+  /** If the Cards are selectable this prop determines from which side of the Card to show selection component. */
+  selectionPlacement: PropTypes.oneOf(['left', 'right']),
 };
 
 export default CardView;
---
+++ src/DataTable/tests/CardView.test.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { selectColumn } from '../utils/getVisibleColumns';
+import CardView from '../CardView';
+import DataTableContext from '../DataTableContext';
+
+const instance = {
+  isSelectable: false,
+  rows: [
+    {
+      id: '1',
+      name: 'Name 1',
+      isSelected: false,
+      getToggleRowSelectedProps: () => ({ indeterminate: false, title: 'Toggle Row Selected' }),
+    },
+    {
+      id: '2',
+      name: 'Name 2',
+      isSelected: true,
+      getToggleRowSelectedProps: () => ({
+        indeterminate: false,
+        title: 'Toggle Row Selected',
+        checked: true,
+        onChange: () => {},
+      }),
+    },
+  ],
+  getTableProps: () => {},
+  prepareRow: (row) => row,
+  visibleColumns: [{
+    Header: 'Name',
+    accessor: 'name',
+  }],
+};
+
+const selectableInstance = {
+  ...instance,
+  isSelectable: true,
+  visibleColumns: [
+    selectColumn,
+    {
+      Header: 'Name',
+      accessor: 'name',
+    },
+  ],
+};
+
+// eslint-disable-next-line react/prop-types
+function Card({ name }) {
+  return <div>{name}</div>;
+}
+
+// eslint-disable-next-line react/prop-types
+function CardViewWrapper({ value = instance }) {
+  return (
+    <DataTableContext.Provider value={value}>
+      <CardView CardComponent={Card} />
+    </DataTableContext.Provider>
+  );
+}
+
+describe('<CardView />', () => {
+  it('renders nothing if Table is empty', async () => {
+    const { container } = render(<CardViewWrapper value={{ ...instance, getTableProps: null }} />);
+    expect(container).toBeEmptyDOMElement();
+  });
+
+  it('renders cards without selection component if DataTable is not selectable', async () => {
+    render(<CardViewWrapper />);
+
+    expect(screen.queryByText('Name 1')).toBeInTheDocument();
+    expect(screen.queryByText('Name 2')).toBeInTheDocument();
+
+    expect(screen.queryByTitle('Toggle Row Selected')).not.toBeInTheDocument();
+  });
+
+  it('renders cards with selection component if DataTable is selectable', async () => {
+    render(<CardViewWrapper value={selectableInstance} />);
+
+    expect(screen.queryByText('Name 1')).toBeInTheDocument();
+    expect(screen.queryByText('Name 2')).toBeInTheDocument();
+
+    const selectionComponents = screen.queryAllByTitle('Toggle Row Selected');
+    expect(selectionComponents.length).toEqual(2);
+    expect(selectionComponents[0]).not.toBeChecked();
+    expect(selectionComponents[1]).toBeChecked();
+  });
+});
