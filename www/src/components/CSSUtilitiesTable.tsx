bb2ed462a1f57f6baae7d7bcf4baa0b18ab42f8d
Viktor Rusakov <vrusakov66@gmail.com>
2022-12-01 15:29:42 +0200

    feat: add ability to generate CSS utility classes through design tokens

---
+++ www/src/components/CSSUtilitiesTable.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// @ts-ignore
+import { DataTable } from '~paragon-react'; // eslint-disable-line
+
+export interface ICSSUtilitiesTable {
+  selectors: Array<{
+    selector: string,
+    declarations: Array<string>,
+  }>,
+}
+
+function CSSUtilitiesTable({ selectors }: ICSSUtilitiesTable) {
+  return (
+    <DataTable
+      data={selectors.map(({ selector, declarations }) => ({
+        selector: <code style={{ fontSize: '14px' }}>.{selector}</code>,
+        declarations: (
+          <div>
+            {declarations.map(declaration => (
+              <code style={{ fontSize: '14px' }} key={declaration} className="mb-0 text-muted">
+                {declaration}
+              </code>
+            ))}
+          </div>
+        ),
+      }))}
+      itemCount={selectors.length}
+      columns={[
+        {
+          Header: 'Utility Class Name',
+          accessor: 'selector',
+        },
+        {
+          Header: 'Styles',
+          accessor: 'declarations',
+        },
+      ]}
+    >
+      <DataTable.Table />
+    </DataTable>
+  );
+}
+
+CSSUtilitiesTable.propTypes = {
+  selectors: PropTypes.arrayOf(
+    PropTypes.shape({
+      selector: PropTypes.string,
+      declarations: PropTypes.arrayOf(PropTypes.string),
+    }),
+  ),
+};
+
+CSSUtilitiesTable.defaultProps = {
+  selectors: [],
+};
+
+export default CSSUtilitiesTable;
