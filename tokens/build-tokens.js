f2c6bc937f6f10d158411232e2138441ef35f090
Viktor Rusakov <vrusakov66@gmail.com>
2022-12-01 15:29:42 +0200

    feat: add ability to generate CSS utility classes through design tokens

--- tokens/build-tokens.js
+++ tokens/build-tokens.js
@@ -1,9 +1,10 @@
 const StyleDictionary = require('style-dictionary');
 const path = require('path');
 const chroma = require('chroma-js');
-const { colorYiq, darken, lighten } = require('./color-helpers');
+const { colorYiq, darken, lighten } = require('./sass-helpers');
+const cssUtilities = require('./css-utilities');
 
-const { formattedVariables, fileHeader } = StyleDictionary.formatHelpers;
+const { formattedVariables, fileHeader, sortByReference } = StyleDictionary.formatHelpers;
 
 const PGN_PREFIX = 'pgn';
 const BASE_BUILD_PATH = path.resolve(__dirname, 'build');
@@ -39,16 +40,33 @@ const colorTransform = (token) => {
 };
 
 /**
- * Transforms color values to be in uppercase format to be compatible with our stylelint rules.
+ * Transformer that applies SASS color functions to tokens.
  */
 StyleDictionary.registerTransform({
-  name: 'color/sass-functions',
+  name: 'color/sass-color-functions',
   transitive: true,
   type: 'value',
   matcher(token) {
     return token.attributes.category === 'color' || token.value?.toString().startsWith('#');
   },
   transformer: colorTransform,
+});
+
+/**
+ * Transforms that implements str-replace from SASS.
+ */
+StyleDictionary.registerTransform({
+  name: 'str-replace',
+  transitive: true,
+  type: 'value',
+  matcher(token) {
+    return token.modify && token.modify[0].type === 'str-replace';
+  },
+  transformer(token) {
+    const { value, modify } = token;
+    const { toReplace, replaceWith } = modify[0];
+    return value.replaceAll(toReplace, replaceWith);
+  },
 });
 
 /**
@@ -67,8 +85,69 @@ StyleDictionary.registerFormat({
   },
 });
 
+/**
+ * Custom formatter that extends default css/variables format to allow specifying
+ * 'outputReferences' per token (by default you are only able to specify it globally for all tokens)
+ */
+StyleDictionary.registerFormat({
+  name: `css/custom-variables`,
+  formatter: function({dictionary, options}) {
+    const variables = dictionary.allTokens.sort(sortByReference(dictionary)).map(token => {
+      let value = token.value;
+      const outputReferencesForToken = token.original.outputReferences === false ? false : options.outputReferences;
+
+      if (dictionary.usesReference(token.original.value) && outputReferencesForToken) {
+        const refs = dictionary.getReferences(token.original.value);
+        refs.forEach(ref => {
+          value = value.replace(ref.value, `var(--${ref.name})`);
+        });
+      }
+
+      return `  --${token.name}: ${value};`
+    }).join(`\n`)
+
+    return `:root {\n${variables}\n}\n`
+  }
+});
+
+/**
+ * Formatter to generate CSS utility classes.
+ * Looks in ./src/utilities/ to get utility classes configuration, filters tokens by 'filters' object attributes
+ * (see https://amzn.github.io/style-dictionary/#/tokens?id=category-type-item for possible keys in the object,
+ * each key should have a list of valid values) and generates CSS classes with using functions defined in
+ * 'utilityFunctionsToApply' list, those functions must be located in css-utilities.js module and return string.
+ */
+StyleDictionary.registerFormat({
+  name: 'css/utility-classes',
+  formatter({ dictionary }) {
+    const utilities = dictionary.properties.utilities;
+
+    if (!utilities) {
+      return '';
+    }
+
+    let utilityClasses = '';
+
+    utilities.forEach(({ filters,  utilityFunctionsToApply }) => {
+      let tokens = dictionary.allTokens;
+
+      Object.entries(filters).forEach(([attributeName, allowedValues]) => {
+        tokens = tokens.filter((token) => allowedValues.includes(token.attributes[attributeName]));
+      })
+
+      for (const token of tokens) {
+        for (const funcName of utilityFunctionsToApply) {
+          utilityClasses += cssUtilities[funcName](token);
+        }
+      }
+    })
+
+    return utilityClasses;
+  },
+});
+
 const paragonStyleDictionary = StyleDictionary.extend({
-  source: ['./source/**/*.json'],
+  source: ['./src/**/*.json'],
   platforms: {
     scss: {
       transformGroup: 'scss',
@@ -82,7 +161,7 @@ const paragonStyleDictionary = StyleDictionary.extend({
           themeable: true,
         },
       }],
-      transforms: StyleDictionary.transformGroup.scss.concat('color/sass-functions'),
+      transforms: StyleDictionary.transformGroup.scss.concat('color/sass-color-functions', 'str-replace'),
       options: {
         showFileHeader: false,
       },
@@ -92,21 +171,35 @@ const paragonStyleDictionary = StyleDictionary.extend({
       prefix: PGN_PREFIX,
       files: [
         {
-          format: 'css/variables',
+          format: 'css/custom-variables',
           destination: `${BASE_BUILD_PATH}/variables.css`,
           options: {
             outputReferences: true,
           },
         },
         {
-          format: 'css/variables',
-          destination: path.resolve(__dirname, '../scss/core/tokens.css'),
+          format: 'css/custom-variables',
+          destination: path.resolve(__dirname, '../scss/core/css/variables.css'),
+          options: {
+            outputReferences: true,
+          },
+        },
+        {
+          format: 'css/utility-classes',
+          destination: path.resolve(__dirname, '../scss/core/css/utility-classes.css'),
+          options: {
+            outputReferences: true,
+          },
+        },
+        {
+          format: 'css/utility-classes',
+          destination: `${BASE_BUILD_PATH}/utility-classes.css`,
           options: {
             outputReferences: true,
           },
         },
       ],
-      transforms: StyleDictionary.transformGroup.css.concat('color/sass-functions'),
+      transforms: StyleDictionary.transformGroup.css.concat('color/sass-color-functions', 'str-replace'),
       options: {
         showFileHeader: false,
       },
