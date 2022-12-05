e5a76076bbe168961d7ad890826e2296ebf66657
Peter Kulko <93188219+PKulkoRaccoonGang@users.noreply.github.com>
2022-12-05 11:59:56 +0200

    docs: added background-color for color HEX values on CSS utility classes page

--- www/src/components/CSSUtilitiesTable.tsx
+++ www/src/components/CSSUtilitiesTable.tsx
@@ -1,6 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+// @ts-ignore
 import { DataTable } from '~paragon-react';
+
+export function convertHexToRgb(hexColor: string, startIndex: number, endIndex: number) {
+  const RADIX_VALUE = 16;
+  return parseInt(hexColor.substring(startIndex, endIndex), RADIX_VALUE);
+}
+
+/**
+ * Decides whether to return light color variant or dark one based on contrast value of the input color
+ */
+export function getInverseColorContrast(hexColor: string) {
+  const SHORT_HEX_CODE_LENGTH = 3;
+  const CONTRAST_THRESHOLD = 128;
+  const BLACK_COLOR = '#000';
+  const WHITE_COLOR = '#FFF';
+  let hexColorValue = hexColor;
+
+  if (hexColorValue.includes('#')) {
+    hexColorValue = hexColor.slice(1);
+  }
+
+  // conversion of a three-digit color HEX code to a six-digit one is necessary for transform to RGB
+  if (hexColorValue.length === SHORT_HEX_CODE_LENGTH) {
+    hexColorValue = hexColor.split('').map((hex) => hex + hex).join('');
+  }
+
+  const r = convertHexToRgb(hexColorValue, 0, 2);
+  const g = convertHexToRgb(hexColorValue, 2, 4);
+  const b = convertHexToRgb(hexColorValue, 4, 8);
+
+  const yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+
+  return (yiq >= CONTRAST_THRESHOLD) ? BLACK_COLOR : WHITE_COLOR;
+}
 
 export interface ICSSUtilitiesTable {
   selectors: Array<{
@@ -16,11 +50,30 @@ function CSSUtilitiesTable({ selectors }: ICSSUtilitiesTable) {
         selector: <code style={{ fontSize: '14px' }}>.{selector}</code>,
         declarations: (
           <div>
-            {declarations.map(declaration => (
-              <code style={{ fontSize: '14px' }} key={declaration} className="mb-0 text-muted">
-                {declaration}
-              </code>
-            ))}
+            {declarations.map(declaration => {
+              const transformDeclaration = declaration.split(' ');
+              const nameProperty = transformDeclaration[0];
+              const colorValue = transformDeclaration[1];
+              const importantRule = transformDeclaration[2];
+
+              return (
+                <code
+                  style={{ fontSize: '14px' }}
+                  key={declaration}
+                  className="mb-0 text-muted"
+                >
+                  {(nameProperty.includes('color:') && (colorValue.includes('#'))) ? (
+                    <>
+                      {`${nameProperty} `}
+                      <mark style={{ backgroundColor: colorValue, color: getInverseColorContrast(colorValue) }}>
+                        {colorValue}
+                      </mark>
+                      {` ${importantRule}`}
+                    </>
+                  ) : declaration}
+                </code>
+              );
+            })}
           </div>
         ),
       }))}
