937b00401a290be344a383f40bfc76dafd9afa65
Viktor Rusakov <vrusakov66@gmail.com>
2022-12-07 10:12:16 +0200

    chore: remove unused import

--- src/Overlay/index.jsx
+++ src/Overlay/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import BaseOverlay from 'react-bootstrap/Overlay';
 import BaseOverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Fade from 'react-bootstrap/Fade';
 import PropTypes from 'prop-types';
 
 const PLACEMENT_VARIANTS = [
@@ -84,7 +83,7 @@ Overlay.propTypes = {
    *
    * Controls `onToggle`.
    */
-  target: PropTypes.oneOfType([PropTypes.elementType, PropTypes.instanceOf(Element), PropTypes.func]),
+  target: PropTypes.oneOfType([PropTypes.elementType, PropTypes.func]),
   /**
    * Animate the entering and exiting of the Overlay. `true` will use the `<Fade>` transition,
    * or a custom react-transition-group `<Transition>` component can be provided.
