808f7ee0e6ffe3bb98a8351e2b417908bd49f74a
Viktor Rusakov <vrusakov66@gmail.com>
2022-12-07 18:42:04 +0200

    feat: remove deprecated components
    BREAKING CHANGE: all of the deprecated components were removed from Paragon, these include `CheckBoxGroup`, `Checkbox`, `Fieldset`, `Input`, `InputSelect`, `InputText`, `ListBox`, `Modal`, `RadioButtonGroup`, `StatusAlert`, `Table`, `TextArea`, `ValidationFormGroup`, `Button.Deprecated`, `Tabs.Deprecated`, `Dropdown.Deprecated`.

--- www/src/pages/foundations/elevation.jsx
+++ www/src/pages/foundations/elevation.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import {
-  Button, Form, Container, Input, Toast, Icon, IconButtonWithTooltip,
-} from '~paragon-react';
-import { Close, WbSunny, DoDisturb } from '~paragon-icons';
 import SEO from '../../components/SEO';
 import Layout from '../../components/PageLayout';
+import {
+  Button, Form, Container,
+  Toast, Icon, IconButtonWithTooltip,
+} from '~paragon-react';
+import { Close, WbSunny, DoDisturb } from '~paragon-icons';
 
 const boxShadowSides = ['down', 'up', 'right', 'left', 'centered'];
 const boxShadowLevels = [1, 2, 3, 4, 5];
@@ -47,7 +48,7 @@ function BoxShadowNode() {
 
   return (
     <div className="pgn-doc__box-shadow-cells">
-      { boxShadowCells }
+      {boxShadowCells}
       <Toast
         className="pgn-doc__box-shadow--toast"
         onClose={() => setShowToast(false)}
@@ -93,15 +94,15 @@ function BoxShadowToolkit({
   return (
     <section className="pgn-doc__box-shadow-toolkit--controls-box">
       {controlsProps.map(({ key, name }) => (
-        <Form.Label
-          className="d-block"
-          key={key}
-          htmlFor={`toolkit-control-${key}`}
-        >
-          {name}
-          <Input
+        <Form.Group key={key}>
+          <Form.Label
+            className="d-block"
+            htmlFor={`toolkit-control-${key}`}
+          >
+            {name}
+          </Form.Label>
+          <Form.Control
             id={`toolkit-control-${key}-${id}`}
-            key={key}
             min={key === 'x' || key === 'y' ? '-100' : '0'}
             max="100"
             type={key === 'color' ? 'color' : 'range'}
@@ -109,7 +110,7 @@ function BoxShadowToolkit({
             onChange={(e) => updateBoxShadowModel(key, e.target.value)}
             disabled={isDisabled}
           />
-        </Form.Label>
+        </Form.Group>
       ))}
       <div className="pgn-doc__box-shadow-toolkit--controls-box--disable-btn-wrapper">
         <Form.Checkbox
