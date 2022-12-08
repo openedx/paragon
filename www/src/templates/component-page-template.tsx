52760b93650fbff6710b7474618188467b0c08f0
Viktor Rusakov <vrusakov66@gmail.com>
2022-12-08 17:58:06 +0200

    docs: display computed CSS variables on components' pages

--- www/src/templates/component-page-template.tsx
+++ www/src/templates/component-page-template.tsx
@@ -13,6 +13,7 @@ import GenericPropsTable from '../components/PropsTable';
 import Layout from '../components/PageLayout';
 import SEO from '../components/SEO';
 import LinkedHeading from '../components/LinkedHeading';
+import ComponentVariablesTable from '../components/ComponentVariablesTable';
 
 export interface IPageTemplate {
   data: {
@@ -80,8 +81,8 @@ export default function PageTemplate({
     };
   }, [components]);
 
-  const scssVariablesTitle = 'Theme Variables (SCSS)';
-  const scssVariablesUrl = 'theme-variables-scss';
+  const scssVariablesTitle = 'Theme Variables';
+  const scssVariablesUrl = 'theme-variables';
 
   const getTocData = () => {
     const tableOfContents = JSON.parse(JSON.stringify(mdx.tableOfContents));
@@ -127,7 +128,7 @@ export default function PageTemplate({
                 <span className="pgn-doc__anchor">#</span>
               </a>
             </h2>
-            <CodeBlock className="language-scss">{scssVariables}</CodeBlock>
+            <ComponentVariablesTable rawStylesheet={scssVariables} />
           </div>
         )}
         {typeof sortedComponentNames !== 'string'
