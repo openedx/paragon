329b12d0453859d5ae7395ba4fb7247a938631bd
Peter Kulko <93188219+PKulkoRaccoonGang@users.noreply.github.com>
2023-01-06 15:15:34 +0200

    refactor: replace old tables with `DataTable` on documentation site (#1859)

---
+++ www/src/components/typography-page/Body.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import MeasuredItem from '../MeasuredItem';
+import measuredTypeProps from './measuredTypeProps';
+// @ts-ignore
+import { DataTable } from '~paragon-react';
+
+import { DesktopMeasuredCell, ClassNameCell } from '../TableCells';
+
+const bodyClassesAndDescriptions = [
+  { className: 'lead', text: 'Large Body' },
+  { className: '', text: 'Body' },
+  { className: 'small', text: 'Small Body' },
+  { className: 'x-small', text: 'Extra Small Body' },
+  { className: 'micro', text: 'Micro Body' },
+];
+
+export default function Body() {
+  return (
+    <>
+      <h2 className="mb-2">Body</h2>
+      <div className="mb-4">
+        <DataTable
+          itemCount={5}
+          data={bodyClassesAndDescriptions}
+          columns={[
+            {
+              Header: 'Desktop & Mobile',
+              Cell: DesktopMeasuredCell,
+            },
+            {
+              Header: 'CSS Class',
+              accessor: 'className',
+              Cell: ClassNameCell,
+            },
+          ]}
+        >
+          <DataTable.Table />
+        </DataTable>
+      </div>
+    </>
+  );
+}
