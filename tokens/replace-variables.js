cf0c20e9b4ce08a55e80641b23730b48daada351
Peter Kulko <93188219+PKulkoRaccoonGang@users.noreply.github.com>
2023-01-06 15:40:56 +0200

    [BD-46] refactor: improve design tokens architecture (#1874)
    * refactor: update design tokens naming and add missing ones
    
    * feat: expose replace vars script to CLI
    
    Co-authored-by: Viktor Rusakov <vrusakov66@gmail.com>

--- tokens/replace-variables.js
+++ tokens/replace-variables.js
@@ -1,11 +1,13 @@
-const fs = require('fs');
-const path = require('path');
+#!/usr/bin/env node
 const { program, Option } = require('commander');
 const { transformInPath } = require('./utils');
+const mapSCSStoCSS = require('./map-scss-to-css');
 
 program
+  .version('0.0.1')
   .description('CLI to replace SCSS variables usages or definitions to CSS variables and vice versa in .scss files.')
   .requiredOption('-p, --filePath <filePath>', 'Path to the file or directory where to replace variables.')
+  .addOption(new Option('-s, --source <sourcePath>', 'Type of replacement: usage or definition. If set to "definition" the command will only update SCSS variables definitions with CSS variables, if set to "usage" - all occurrences of SCSS variables will we replaced'))
   .addOption(new Option('-t, --replacementType <replacementType>', 'Type of replacement: usage or definition. If set to "definition" the command will only update SCSS variables definitions with CSS variables, if set to "usage" - all occurrences of SCSS variables will we replaced')
     .choices(['usage', 'definition'])
     .default('definition'))
@@ -13,14 +15,16 @@ program
     .choices(['scss-to-css', 'css-to-scss'])
     .default('scss-to-css'))
   .action(async (options) => {
-    const { direction, filePath, replacementType } = options;
+    const {
+      direction,
+      filePath,
+      replacementType,
+      source,
+    } = options;
+    const variablesMap = mapSCSStoCSS(source);
     if (replacementType === 'usage') {
-      const mapFile = fs.readFileSync(path.resolve(__dirname, `./variables-maps/${direction}.json`), 'utf-8');
-      const variablesMap = JSON.parse(mapFile);
-      await transformInPath(filePath, variablesMap, 'usage', ['Table'], direction);
+      await transformInPath(filePath, variablesMap, 'usage', [], direction);
     } else {
-      const mapFile = fs.readFileSync(path.resolve(__dirname, './variables-maps/scss-to-css.json'), 'utf-8');
-      const variablesMap = JSON.parse(mapFile);
       await transformInPath(filePath, variablesMap);
     }
   });
