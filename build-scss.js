3ee9288fa78f4de34264cb68386744dd1b62ce32
PKulkoRaccoonGang <peter.kulko@raccoongang.com>
2023-03-09 02:11:57 +0200

    refactor: added postCSS plugins

--- build-scss.js
+++ build-scss.js
@@ -1,12 +1,20 @@
-const sass = require('sass');
 const fs = require('fs');
+const sass = require('sass');
+const postCSS = require('postcss');
+const postCSSCustomMedia = require('postcss-custom-media');
+const postCSSImport = require('postcss-import');
+const postCSSMinify = require('postcss-minify');
 const { pathToFileURL } = require('url');
 
-const compileStyleSheets = (path, output) => {
-    return sass.compile(path, {
-        style: output,
-        sourceMap: true,
-        sourceMapIncludeSources: true,
+/**
+ * The function that compiles SCSS files
+ * and writes CSS output in the `dist` directory.
+ *
+ * @param {string} name - name to the stylesheet
+ * @param {string} path - path to the stylesheet
+ */
+const compileAndWriteStyleSheets = (name, path) => {
+    const compiledStyleSheet =  sass.compile(path, {
         importers: [{
             // An importer that redirects relative URLs starting with '~' to 'node_modules'.
             findFileUrl(url) {
@@ -15,54 +23,37 @@ const compileStyleSheets = (path, output) => {
             }
         }]
     });
+
+    convertAndWriteCSSWithPostCSS(name, path, compiledStyleSheet);
 }
 
-fs.writeFileSync(
-    './dist/core.css',
-    compileStyleSheets('./scss/core/core.scss').css
-);
+/**
+ * Converted media queries with CSS custom variables,
+ * minified the output CSS file and created a CSS source map.
+ *
+ * @param {string} name - name to the stylesheet
+ * @param {string} path - path to the stylesheet
+ * @param {Object} source - compiled stylesheet object
+ */
+const convertAndWriteCSSWithPostCSS = (name, path, source) => {
+    postCSS([postCSSCustomMedia(), postCSSImport()])
+        .process(source.css, { from: path })
+        .then(outputStyleSheet => fs.writeFileSync(`./dist/${name}.css`, outputStyleSheet.css));
 
-fs.writeFileSync(
-    './dist/core.min.css',
-    compileStyleSheets('./scss/core/core.scss', 'compressed').css
-);
+    postCSS([postCSSCustomMedia(), postCSSImport(), postCSSMinify()])
+        .process(source.css, { from: path })
+        .then(outputStyleSheet => fs.writeFileSync(`./dist/${name}.min.css`, outputStyleSheet.css));
 
-fs.appendFileSync(
-    './dist/core.css',
-        compileStyleSheets(`./css/core/custom-media-breakpoints.css`).css
-);
-
-fs.writeFileSync(
-    './dist/core.css.map',
-    JSON.stringify(compileStyleSheets('./scss/core/core.scss').sourceMap)
-);
+    postCSS([postCSSCustomMedia(), postCSSImport()])
+        .process(source.css, { from: path, map: { inline: false } })
+        .then(outputStyleSheet => fs.writeFileSync(`./dist/${name}.css.map`, JSON.stringify(outputStyleSheet.map)));
+}
 
 const compileThemeStyleSheets = (themeVariant) => {
-    fs.writeFileSync(
-        `./dist/${themeVariant}.css`,
-        compileStyleSheets(`./css/${themeVariant}/variables.css`).css
-    );
-
-    fs.writeFileSync(
-        `./dist/${themeVariant}.min.css`,
-        compileStyleSheets(`./css/${themeVariant}/variables.css`, 'compressed').css
-    );
-
-    fs.appendFileSync(
-        `./dist/${themeVariant}.css`,
-        `\n${compileStyleSheets(`./css/${themeVariant}/utility-classes.css`).css}`
-    );
-
-    fs.writeFileSync(
-        `./css/${themeVariant}/${themeVariant}.css`,
-        compileStyleSheets(`./dist/${themeVariant}.css`).css
-    );
-
-    fs.writeFileSync(
-        `./dist/${themeVariant}.css.map`,
-        JSON.stringify(compileStyleSheets(`./dist/${themeVariant}.min.css`).sourceMap)
-    );
+    compileAndWriteStyleSheets(themeVariant, `./css/${themeVariant}/${themeVariant}.css`);
 };
+
+compileAndWriteStyleSheets('core', './scss/core/core.scss');
 
 const THEME_VARIANTS = ['light'];
 THEME_VARIANTS.forEach(themeVariant => compileThemeStyleSheets(themeVariant));
