9dc31bebf8d4dc827c0bae85286b32155a894d2d
PKulkoRaccoonGang <peter.kulko@raccoongang.com>
2023-04-19 19:29:55 +0300

    refactor: refactoring after review

--- www/build-themes.js
+++ www/build-themes.js
@@ -9,10 +9,6 @@ const importer = function importer(url) {
   if (url.startsWith('~paragon-style')) {
     file = path.resolve(__dirname, '../scss', url.substr('~paragon-style/'.length));
   } else if (url[0] === '~') {
-    // we need to correctly resolve urls when reaching Paragon's core scss
-    // since stylesheets there also use tilde imports which
-    // webpack resolves to Paragon's node_modules (not node_modules related to docs site),
-    // so we do the same
     file = path.resolve(__dirname, '../node_modules', url.substr(1));
   }
 
