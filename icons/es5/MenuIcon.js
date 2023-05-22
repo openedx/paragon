62f90fbd303917fd56ec15e4a58ed56d67b55e0a
Viktor Rusakov <vrusakov66@gmail.com>
2023-05-22 16:13:05 +0300

    chore: delete old icons

--- icons/es5/MenuIcon.js
+++ icons/es5/MenuIcon.js
@@ -2,11 +2,11 @@ function _extends() { _extends = Object.assign ? Object.assign.bind() : function
 import * as React from "react";
 function SvgMenuIcon(props) {
   return /*#__PURE__*/React.createElement("svg", _extends({
+    xmlns: "http://www.w3.org/2000/svg",
     width: 24,
     height: 24,
     viewBox: "0 0 24 24",
-    fill: "none",
-    xmlns: "http://www.w3.org/2000/svg"
+    fill: "none"
   }, props), /*#__PURE__*/React.createElement("path", {
     d: "M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z",
     fill: "currentColor"
--- icons/jsx/CheckBoxIcon.jsx
+++ icons/jsx/CheckBoxIcon.jsx
@@ -2,11 +2,11 @@ import * as React from "react";
 function SvgCheckBoxIcon(props) {
   return (
     <svg
+      xmlns="http://www.w3.org/2000/svg"
       width={24}
       height={24}
       viewBox="0 0 24 24"
       fill="none"
-      xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
       <path
