fdafefa0f3a67573e45b3f6b2bc0e8588e84ef29
Viktor Rusakov <vrusakov66@gmail.com>
2023-05-28 10:47:59 +0300

    chore: fix lint

--- src/Button/deprecated/index.jsx
+++ src/Button/deprecated/index.jsx
@@ -113,7 +113,7 @@ export const buttonPropTypes = {
   /** Specifies variant to use. */
   variant: PropTypes.oneOf([
     'primary', 'secondary', 'success', 'danger', 'warning', 'info', 'dark', 'light', 'link', 'outline-primary',
-    'outline-secondary', 'outline-success', 'outline-danger', 'outline-warning', 'outline-info', 'outline-dark', 'outline-light'
+    'outline-secondary', 'outline-success', 'outline-danger', 'outline-warning', 'outline-info', 'outline-dark', 'outline-light',
   ]),
 };
 
