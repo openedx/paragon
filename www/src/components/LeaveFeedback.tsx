624c1968969d9f77327cb050d10d254e2f955731
PKulkoRaccoonGang <peter.kulko@raccoongang.com>
2023-06-06 18:11:53 +0300

    refactor: after review

--- www/src/components/LeaveFeedback.tsx
+++ www/src/components/LeaveFeedback.tsx
@@ -4,8 +4,8 @@ import { Nav } from '~paragon-react';
 import { useLocation } from '@reach/router';
 
 function LeaveFeedback(props) {
-  const CURRENT_URL = useLocation();
-  const FEEDBACK_URL = `https://github.com/openedx/paragon/issues/new?title=%5Bparagon-openedx.netlify.app%5D%20Feedback%20(on%20${CURRENT_URL.pathname})&amp;labels=docs&template=feedback_template.md`;
+  const location = useLocation();
+  const FEEDBACK_URL = `https://github.com/openedx/paragon/issues/new?title=%5Bparagon-openedx.netlify.app%5D%20Feedback%20(on%20${location.pathname})&amp;labels=docs&template=feedback_template.md`;
 
   const handleLinkFeedbackClick = () => {
     global.analytics.track('openedx.paragon.docs.leave_feedback.clicked');
