8874137dc7da89bb3bed432ce03af64a7230d76d
Peter Kulko <93188219+PKulkoRaccoonGang@users.noreply.github.com>
2023-06-08 14:53:43 +0300

    docs: fixed issue feedback error (#2359)
    * docs: fixed issue feedback error
    * refactor: after review

--- www/src/components/LeaveFeedback.tsx
+++ www/src/components/LeaveFeedback.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Nav } from '~paragon-react';
+import { useLocation } from '@reach/router';
 
 function LeaveFeedback(props) {
-  const CURRENT_URL = typeof window !== 'undefined' ? window.location.pathname : '';
-  const FEEDBACK_URL = `https://github.com/openedx/paragon/issues/new?title=%5Bparagon-openedx.netlify.app%5D%20Feedback%20(on%20${CURRENT_URL})&amp;labels=docs&template=feedback_template.md`;
+  const location = useLocation();
+  const FEEDBACK_URL = `https://github.com/openedx/paragon/issues/new?title=%5Bparagon-openedx.netlify.app%5D%20Feedback%20(on%20${location.pathname})&amp;labels=docs&template=feedback_template.md`;
 
   const handleLinkFeedbackClick = () => {
     global.analytics.track('openedx.paragon.docs.leave_feedback.clicked');
