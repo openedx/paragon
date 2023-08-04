c1ecb7158b5c1b589cb162cd13f4c8eb1afd2515
monteri <36768631+monteri@users.noreply.github.com>
2023-08-04 15:16:52 +0300

    feat: create chip carousel (#2378)
    ---------
    
    Co-authored-by: monteri <lansevermore>

---
+++ src/ChipCarousel/messages.js
@@ -0,0 +1,16 @@
+import { defineMessages } from 'react-intl';
+
+const messages = defineMessages({
+  scrollToPrevious: {
+    id: 'pgn.ChipCarousel.scrollToPrevious',
+    defaultMessage: 'Scroll to previous',
+    description: 'Accessibility text describing the action for navigating a scrollable carousel to the previous element',
+  },
+  scrollToNext: {
+    id: 'pgn.ChipCarousel.scrollToNext',
+    defaultMessage: 'Scroll to next',
+    description: 'Accessibility text describing the action for navigating a scrollable carousel to the next element',
+  },
+});
+
+export default messages;
