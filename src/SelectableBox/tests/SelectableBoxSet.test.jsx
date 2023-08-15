a0353dd0ecada0fcdcabe2c269422bff4b70b5c2
Maxwell Frank <92897870+MaxFrank13@users.noreply.github.com>
2023-08-15 09:21:58 -0400

    fix: selectable box set props forwarding (#2461)
    BREAKING CHANGE: aria-label is required when not using aria-labelledby
    in the SelectableBoxSet component. An associated label for the component is a WCAG requirement.
    
    Co-authored-by: Maxwell Frank <mfrank@2u.com>

--- src/SelectableBox/tests/SelectableBoxSet.test.jsx
+++ src/SelectableBox/tests/SelectableBoxSet.test.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
 import { Form } from '../..';
 import SelectableBox from '..';
@@ -11,9 +13,11 @@ const checkboxText = (text) => `SelectableCheckbox${text}`;
 const radioType = 'radio';
 const radioText = (text) => `SelectableRadio${text}`;
 
+const ariaLabel = 'test-default-label';
+
 function SelectableCheckboxSet(props) {
   return (
-    <SelectableBox.Set name={radioType} type={checkboxType} {...props}>
+    <SelectableBox.Set name={radioType} type={checkboxType} ariaLabel={ariaLabel} {...props}>
       <SelectableBox value={1} type={checkboxType}>{checkboxText(1)}</SelectableBox>
       <SelectableBox value={2} type={checkboxType}>{checkboxText(2)}</SelectableBox>
       <SelectableBox value={3} type={checkboxType}>{checkboxText(3)}</SelectableBox>
@@ -23,7 +27,7 @@ function SelectableCheckboxSet(props) {
 
 function SelectableRadioSet(props) {
   return (
-    <SelectableBox.Set name={radioType} type={radioType} {...props}>
+    <SelectableBox.Set name={radioType} type={radioType} ariaLabel={ariaLabel} {...props}>
       <SelectableBox value={1} type={radioType}>{radioText(1)}</SelectableBox>
       <SelectableBox value={2} type={radioType}>{radioText(2)}</SelectableBox>
       <SelectableBox value={3} type={radioType}>{radioText(3)}</SelectableBox>
@@ -36,6 +40,10 @@ describe('<SelectableBox.Set />', () => {
     it('renders without props', () => {
       const tree = renderer.create((<SelectableRadioSet name="testName" />)).toJSON();
       expect(tree).toMatchSnapshot();
+    });
+    it('forwards props', () => {
+      render((<SelectableRadioSet name="testName" data-testid="test-radio-set-name" />));
+      expect(screen.getByTestId('test-radio-set-name')).toBeInTheDocument();
     });
     it('correct render when type prop is changed', () => {
       const setWrapper = mount(<SelectableRadioSet name="set" />);
@@ -83,6 +91,22 @@ describe('<SelectableBox.Set />', () => {
       const wrapper = mount(<SelectableRadioSet columns={columns} />);
       const selectableBoxSet = wrapper.find(Form.RadioSet);
       expect(selectableBoxSet.hasClass(`pgn__selectable_box-set--${columns}`)).toBe(true);
+    });
+    it('renders with an aria-label attribute', () => {
+      render((<SelectableRadioSet name="testName" ariaLabel="test-radio-set-label" />));
+      expect(screen.getByLabelText('test-radio-set-label')).toBeInTheDocument();
+    });
+    it('renders with an aria-labelledby attribute', () => {
+      render((
+        <>
+          <h2 id="test-radio-set-label">Radio Set Label text</h2>
+          <SelectableRadioSet
+            name="testName"
+            ariaLabelledby="test-radio-set-label"
+          />
+        </>
+      ));
+      expect(screen.getByLabelText('Radio Set Label text')).toBeInTheDocument();
     });
   });
 });
