02c3161978b5a6d7065b5eabb50a196432272d04
adamstankiewicz <agstanki@gmail.com>
2023-09-01 12:39:25 +0000

    Deploying to gh-pages from @ openedx/paragon@d07827d9a0ff8f01395a39f571daed0b8ad6f6db 🚀

---
+++ component---src-pages-404-tsx-484e3640e9bc0d090383.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkparagon_pattern_library_documentation=self.webpackChunkparagon_pattern_library_documentation||[]).push([[218],{67096:function(t,e,n){n.r(e);var a=n(27378),r=n(46197),u=n(42980);e.default=function(){return a.createElement(r.Z,null,a.createElement(u.Z,{title:"404: Not found"}),a.createElement("h1",null,"404: Not Found"),a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-484e3640e9bc0d090383.js.map
