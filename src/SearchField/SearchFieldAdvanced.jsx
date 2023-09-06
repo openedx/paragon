9a7ce31f911e4b6aea1b01b85cf44685bcfaeb2f
Kyrylo Hudym-Levkovych <kyr.hudym@kyrs-MacBook-Pro.local>
2023-09-06 15:15:16 +0300

    refactor: refactor after review

--- src/SearchField/SearchFieldAdvanced.jsx
+++ src/SearchField/SearchFieldAdvanced.jsx
@@ -100,7 +100,7 @@ function SearchFieldAdvanced(props) {
         role="search"
         onSubmit={handleSubmit}
         onReset={handleClear}
-        className="pgn__searchfield-form d-flex align-items-center w-100"
+        className="pgn__searchfield-form"
         aria-label={formAriaLabel}
       >
         <SearchFieldContext.Provider
