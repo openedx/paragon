c3ca11436b912831eb84c5f253fcc7a7b25df4fe
monteri <lansevermore>
2023-09-07 17:23:19 +0300

    feat: help command for Paragon CLI

--- bin/paragon-scripts.js
+++ bin/paragon-scripts.js
@@ -1,10 +1,49 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
 const chalk = require('chalk');
 const themeCommand = require('../lib/install-theme');
+const helpCommand = require('../lib/help');
 
-// command: executor function
 const COMMANDS = {
-  'install-theme': themeCommand,
+  // 'command-name': {
+  //   executor: executorFunc,
+  //
+  //   ********** Block for help command start **********
+  //   description: 'Command description',
+  //   parameters: [
+  //     {
+  //       name: 'paramName',
+  //       description: 'paramDescription',
+  //       defaultValue: 'paramDefaultValue',
+  //       required: true/false,
+  //     },
+  //     ...
+  //   ],
+  //   options: [
+  //     {
+  //       name: '--optionName',
+  //       description: 'optionDescription',
+  //     },
+  //     ...
+  //   ],
+  //   ********** Block for help command end **********
+  // },
+  'install-theme': {
+    executor: themeCommand,
+    description: 'Installs the specific @edx/brand package.',
+    parameters: [
+      {
+        name: 'theme',
+        description: 'The @edx/brand package to install.',
+        defaultValue: '@edx/brand-openedx@latest',
+        required: false,
+      },
+    ],
+  },
+  help: {
+    executor: helpCommand,
+    description: 'Displays help for available commands.',
+  },
 };
 
 (async () => {
@@ -12,15 +51,18 @@ const COMMANDS = {
   const executor = COMMANDS[command];
 
   if (!executor) {
-    // eslint-disable-next-line no-console
-    console.log(chalk.red.bold('Unknown command. Usage: paragon <command>'));
+    console.log(chalk.red.bold('Unknown command. Usage: paragon <command>.'));
+    return;
+  }
+
+  if (command === 'help') {
+    helpCommand(COMMANDS);
     return;
   }
 
   try {
-    await executor();
+    await executor.executor();
   } catch (error) {
-    // eslint-disable-next-line no-console
     console.error(chalk.red.bold('An error occurred:', error.message));
     process.exit(1);
   }
