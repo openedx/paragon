a78b4816db0674ecdc1b17466ee935d038bc507b
SundasNoreen <sundas.noreen@arbisoft.com>
2023-09-27 16:51:53 +0500

    feat: added new icon in es5 and jsx

---
+++ icons/jsx/CheckCircleLightOutline.jsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+
+const CheckCircleLightOutline = (props) => (
+  <svg
+    width="57"
+    height="57"
+    viewBox="0 0 57 57"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <circle
+      cx="28.5"
+      cy="28.5"
+      r="26.5"
+      stroke="#00262B"
+      stroke-width="3"
+    />
+    <path
+      d="M12.658 29.9736L22.2369 39.5525L44.3422 17.4473"
+      stroke="#00262B"
+      stroke-width="3"
+    />
+  </svg>
+
+);
+
+export default CheckCircleLightOutline;
