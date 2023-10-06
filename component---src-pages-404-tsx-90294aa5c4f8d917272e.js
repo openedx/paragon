57d64ebb2bc6a4640896831726d7adbc259d7cf3
viktorrusakov <52399399+viktorrusakov@users.noreply.github.com>
2023-10-06 08:00:21 +0000

    Deploying to gh-pages from @ openedx/paragon@39d967606479505c5c35f307ce8dfad01215febc 🚀

---
+++ component---src-pages-404-tsx-90294aa5c4f8d917272e.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkparagon_pattern_library_documentation=self.webpackChunkparagon_pattern_library_documentation||[]).push([[218],{67096:function(t,e,n){n.r(e);var a=n(27378),u=n(46197),r=n(42980);e.default=function(t){let{pageContext:e}=t;return a.createElement(u.Z,{githubEditPath:e.githubEditPath},a.createElement(r.Z,{title:"404: Not found"}),a.createElement("h1",null,"404: Not Found"),a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-90294aa5c4f8d917272e.js.map
