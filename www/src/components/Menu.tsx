3eb4aa35dffe41c48ed05ffc6d5f193d444f340f
PKulkoRaccoonGang <peter.kulko@raccoongang.com>
2023-11-10 13:50:31 +0200

    Merge remote-tracking branch 'origin/master' into HEAD

--- www/src/components/Menu.tsx
+++ www/src/components/Menu.tsx
@@ -158,6 +158,10 @@ const handlePlaygroundClick = () => sendUserAnalyticsEvent(PLAYGROUND_EVENTS.LIN
 
 const handleContrastCheckerClick = () => sendUserAnalyticsEvent(CONTRAST_CHECKER_CLICKED_EVENT);
 
+const handleContrastCheckerClick = () => {
+  global.analytics.track('openedx.paragon.docs.menu.tools.visit_contrast_checker.clicked');
+};
+
 MenuComponentListCategory.propTypes = {
   children: PropTypes.node.isRequired,
   title: PropTypes.string.isRequired,
@@ -272,6 +276,17 @@ function Menu() {
               >
                 Component Generator
               </Link>
+            </li>
+            <li>
+              <Hyperlink
+                destination="https://webaim.org/resources/contrastchecker"
+                target="_blank"
+                externalLinkAlternativeText="Contrast checker page"
+                externalLinkTitle="Contrast checker"
+                onClick={handleContrastCheckerClick}
+              >
+                Contrast Checker
+              </Hyperlink>
             </li>
             <li>
               <Hyperlink
