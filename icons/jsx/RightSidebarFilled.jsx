f5ac240c9897e6070347ec1297d60e82ba1a26ec
sundasnoreen12 <sundasnoreen12@gmail.com>
2023-12-21 12:20:28 +0500

    feat: added new svg for sidebar filled and outlined

---
+++ icons/jsx/RightSidebarFilled.jsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+const SvgRightSidebarFilled = (props) => (
+  <svg
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M2 22V2h20v20H2ZM14 4H4v16h10V4Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+export default SvgRightSidebarFilled;
