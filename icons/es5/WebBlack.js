bcf563fee1d3c2932d9041ae37f599b98a5eb91c
sundasnoreen12 <sundasnoreen12@gmail.com>
2023-12-21 12:02:44 +0500

    fix: added web black svg in svg folder

--- icons/es5/WebBlack.js
+++ icons/es5/WebBlack.js
@@ -1,61 +1,28 @@
-import * as React from 'react';
-
-function _extends() { _extends = Object.assign ? Object.assign.bind() : function (target) { for (let i = 1; i < arguments.length; i++) { const source = arguments[i]; for (const key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
-
-const SvgWebBlack = props => /*#__PURE__*/React.createElement(
-  'svg',
-  _extends(
-    {
-      width: '24',
-      height: '24',
-      viewBox: '0 0 24 24',
-      fill: 'none',
-      xmlns: 'http://www.w3.org/2000/svg',
-    },
-    props,
-  ),
-  /*#__PURE__*/React.createElement(
-    'g',
-    { id: 'web_black_24dp', clipPath: 'url(#clip0_164_539)' },
-    /*#__PURE__*/React.createElement(
-      'g',
-      { id: 'Group 621' },
-      /*#__PURE__*/React.createElement('rect', {
-        id: 'Rectangle 320',
-        x: '3',
-        y: '3',
-        width: '18',
-        height: '18',
-        stroke: '#00262B',
-        strokeWidth: '2',
-      }),
-      /*#__PURE__*/React.createElement('rect', {
-        id: 'Rectangle 322',
-        x: '14',
-        y: '3.25',
-        width: '7',
-        height: '17.5',
-        fill: '#00262B',
-      }),
-      /*#__PURE__*/React.createElement('rect', {
-        id: 'Rectangle 321',
-        x: '14',
-        y: '3.25',
-        width: '2',
-        height: '17.5',
-        fill: '#00262B',
-      }),
-    ),
-  ),
-  /*#__PURE__*/React.createElement(
-    'defs',
-    null,
-    /*#__PURE__*/React.createElement(
-      'clipPath',
-      { id: 'clip0_164_539' },
-      /*#__PURE__*/React.createElement('rect', { width: '24', height: '24', fill: 'white' }),
-    ),
-  ),
-);
-
+function _extends() { _extends = Object.assign ? Object.assign.bind() : function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+import * as React from "react";
+const SvgWebBlack = props => /*#__PURE__*/React.createElement("svg", _extends({
+  width: 24,
+  height: 24,
+  viewBox: "0 0 24 24",
+  fill: "none",
+  xmlns: "http://www.w3.org/2000/svg"
+}, props), /*#__PURE__*/React.createElement("g", {
+  clipPath: "url(#a)"
+}, /*#__PURE__*/React.createElement("path", {
+  stroke: "#00262B",
+  strokeWidth: 2,
+  d: "M3 3h18v18H3z",
+  fill: "currentColor"
+}), /*#__PURE__*/React.createElement("path", {
+  fill: "currentColor",
+  d: "M14 3.25h7v17.5h-7z"
+}), /*#__PURE__*/React.createElement("path", {
+  fill: "currentColor",
+  d: "M14 3.25h2v17.5h-2z"
+})), /*#__PURE__*/React.createElement("defs", null, /*#__PURE__*/React.createElement("clipPath", {
+  id: "a"
+}, /*#__PURE__*/React.createElement("path", {
+  fill: "currentColor",
+  d: "M0 0h24v24H0z"
+}))));
 export default SvgWebBlack;
--- icons/jsx/WebBlack.jsx
+++ icons/jsx/WebBlack.jsx
@@ -1,28 +1,28 @@
-import * as React from 'react';
-
-const WebBlack = (props) => (
-  <svg 
-    width="24" 
-    height="24" 
-    viewBox="0 0 24 24" 
-    fill="none" 
-    xmlns="http://www.w3.org/2000/svg" 
+import * as React from "react";
+const SvgWebBlack = (props) => (
+  <svg
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <g id="web_black_24dp" clipPath="url(#clip0_164_539)">
-      <g id="Group 621">
-        <rect id="Rectangle 320" x="3" y="3" width="18" height="18" stroke="#00262B" strokeWidth="2" />
-        <rect id="Rectangle 322" x="14" y="3.25" width="7" height="17.5" fill="#00262B" />
-        <rect id="Rectangle 321" x="14" y="3.25" width="2" height="17.5" fill="#00262B" />
-      </g>
+    <g clipPath="url(#a)">
+      <path
+        stroke="#00262B"
+        strokeWidth={2}
+        d="M3 3h18v18H3z"
+        fill="currentColor"
+      />
+      <path fill="currentColor" d="M14 3.25h7v17.5h-7z" />
+      <path fill="currentColor" d="M14 3.25h2v17.5h-2z" />
     </g>
     <defs>
-      <clipPath id="clip0_164_539">
-        <rect width="24" height="24" fill="white" />
+      <clipPath id="a">
+        <path fill="currentColor" d="M0 0h24v24H0z" />
       </clipPath>
     </defs>
   </svg>
-
 );
-
-export default WebBlack;
+export default SvgWebBlack;
