0515f97a0b81d1131400cad639db51b2557613f6
PKulkoRaccoonGang <peter.kulko@raccoongang.com>
2023-12-26 22:01:19 +0200

    refactor: code refactoring

--- www/netlify/functions/trackGenerateComponent.js
+++ www/netlify/functions/trackGenerateComponent.js
@@ -6,19 +6,19 @@ const analytics = new Analytics(process.env.SEGMENT_KEY);
 
 exports.handler = async function eventHandler(event) {
     // Only allow POST
-    if (event.httpMethod !== 'POST') {
-        return { statusCode: 405, body: 'Method Not Allowed' };
-    }
-    const { componentName } = JSON.parse(event.body);
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+  const { componentName } = JSON.parse(event.body);
     // dispatch event to Segment
-    analytics.track({
+  analytics.track({
         anonymousId: uuidv4(),
-        event: COMPONENT_GENERATED_EVENT,
+    event: COMPONENT_GENERATED_EVENT,
         properties: { componentName },
     });
 
     return {
-        statusCode: 200,
-        body: JSON.stringify({ success: true }),
-    };
+    statusCode: 200,
+    body: JSON.stringify({ success: true }),
+  };
 };
