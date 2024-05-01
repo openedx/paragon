044fc921fdcbee8bda7cb77a9e7245d515da5f88
Brian Smith <bsmith@axim.org>
2024-05-01 19:27:00 -0400

    chore(deps): update `bootstrap-icons` package and build icons

--- icons/es5/BsTiktok.js
+++ icons/es5/BsTiktok.js
@@ -9,7 +9,7 @@ var SvgBsTiktok = function SvgBsTiktok(props) {
     className: "bi bi-tiktok",
     viewBox: "0 0 16 16"
   }, props), /*#__PURE__*/React.createElement("path", {
-    d: "M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3V0Z",
+    d: "M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z",
     fill: "currentColor"
   }));
 };
