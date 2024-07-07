662f00dae4e275c490a65456bc6f224c4cb3c8c8
Braden MacDonald <braden@opencraft.com>
2024-07-07 04:20:33 -0700

    Add TypeScript types for <Overlay>, <OverlayTrigger>, <Tooltip>, <IconButton> (#3100)
    * feat: add typings for <Overlay> and <OverlayTrigger>
    
    * feat: add typings for <Tooltip>
    
    * feat: add typings for <IconButton>
    
    * chore: typing cleanups for <Chip> code
    
    * feat: slightly more detailed types for <Chip/ChipIcon> - per review

--- src/ChipCarousel/index.tsx
+++ src/ChipCarousel/index.tsx
@@ -4,9 +4,7 @@ import { useIntl } from 'react-intl';
 import classNames from 'classnames';
 // @ts-ignore
 import { OverflowScroll, OverflowScrollContext } from '../OverflowScroll';
-// @ts-ignore
 import IconButton from '../IconButton';
-// @ts-ignore
 import Icon from '../Icon';
 // @ts-ignore
 import { ArrowForward, ArrowBack } from '../../icons';
