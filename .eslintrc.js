4b065a6ac7a1e1d1e68c17501e56afd4e84c35ee
Peter Kulko <peter.kulko@raccoongang.com>
2024-11-13 09:43:19 +0200

    chore: reordered design tokens structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -70,5 +70,19 @@ module.exports = {
         project: './tsconfig.json',
       },
     },
+    {
+      files: ['**/*.json'],
+      plugins: ['jsonc'],
+      extends: ['plugin:jsonc/recommended-with-json'],
+      rules: {
+        'jsonc/indent': ['error', 2],
+        'jsonc/quote-props': ['error', 'always'],
+        'jsonc/quotes': ['error', 'double'],
+        'jsonc/no-octal': 'error',
+        'jsonc/no-dupe-keys': 'error',
+        'jsonc/valid-json-number': 'error',
+        'jsonc/no-bigint-literals': 'error',
+      },
+    },
   ],
 };
