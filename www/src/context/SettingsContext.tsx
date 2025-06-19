2d00fe16f4e0f61e26419ab0b9e486420555f43a
Adam Stankiewicz <agstanki@gmail.com>
2025-06-19 13:33:55 -0400

    fix: remove build-themes command

--- www/src/context/SettingsContext.tsx
+++ www/src/context/SettingsContext.tsx
@@ -1,10 +1,8 @@
 import React, { createContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Helmet } from 'react-helmet';
 import { IntlProvider } from 'react-intl';
 import { messages } from '~paragon-react';
 
-import { THEMES, DEFAULT_THEME } from '../../theme-config';
 import { useSettings, useCustomThemes, useDirection, useSettingsUI } from '../hooks';
 
 export interface IDefaultValue {
@@ -68,22 +66,6 @@ function SettingsContextProvider({ children }) {
 
   return (
     <SettingsContext.Provider value={contextValue}>
-      <Helmet>
-        {/* Open edX theme is the base and default theme which should always be included and active in the <head>.
-            Other themes generally inherit from it and override its values, so if they are included after the base
-            one they wil get applied to the site. This is done to avoid flickering when changing themes,
-            if you simply change href of the stylesheet there is a small window of time when the previous
-            theme gets unapplied and new one loaded which leaves whose site without styles.
-         */}
-        {THEMES.map(({ stylesheet, id }) => id !== DEFAULT_THEME && (
-          <link
-            key={id}
-            href={`/static/${stylesheet}.css`}
-            rel={`stylesheet${id === DEFAULT_THEME ? '' : ' alternate'}`}
-            type="text/css"
-          />
-        ))}
-      </Helmet>
       <IntlProvider messages={messages[settings.language || 'en']} locale={settings.language?.split('-')[0] || 'en'}>
         {children}
       </IntlProvider>
