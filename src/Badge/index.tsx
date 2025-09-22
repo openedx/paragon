ac2b4990803cf75df17dc12de0c9886a7f1ef1a0
diana-villalvazo-wgu <diana.villalvazo@wgu.edu>
2025-09-22 14:41:58 -0600

    refactor: transform components into Typescript

---
+++ src/Badge/index.tsx
@@ -0,0 +1,32 @@
+import React, { ForwardedRef } from 'react';
+import BaseBadge from 'react-bootstrap/Badge';
+
+const STYLE_VARIANTS = [
+  'primary',
+  'secondary',
+  'success',
+  'danger',
+  'warning',
+  'info',
+  'light',
+  'dark',
+];
+
+interface BadgeProps extends React.HTMLAttributes<HTMLElement> {
+  /** Specifies element type for this component */
+  as?: React.ElementType;
+  /** Visual style of the badge */
+  variant?: typeof STYLE_VARIANTS[number];
+  /** Add the `pill` modifier to make badges more rounded with some additional horizontal padding */
+  pill?: boolean;
+  /** Overrides underlying component base CSS class name */
+  bsPrefix?: string;
+}
+
+const Badge = React.forwardRef(({
+  as = 'span', variant = 'primary', pill = false, bsPrefix = 'badge', ...props
+}: BadgeProps, ref: ForwardedRef<HTMLElement>) => (
+  <BaseBadge as={as} variant={variant} pill={pill} bsPrefix={bsPrefix} {...props} ref={ref} />
+));
+
+export default Badge;
