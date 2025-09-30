35a9a893627bfa7a0829031957c411b96611e850
diana-villalvazo-wgu <diana.villalvazo@wgu.edu>
2025-09-30 14:56:19 -0600

    refactor: transform components into Typescript

--- src/Tabs/Tab.tsx
+++ src/Tabs/Tab.tsx
@@ -1,33 +1,25 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import BaseTab from 'react-bootstrap/Tab';
 
-function Tab(props) {
-  return <BaseTab {...props} />;
-}
-
-Tab.propTypes = {
+interface TabProps {
   /** Specifies the `Tab` navigation title. */
-  title: PropTypes.node.isRequired,
+  title: React.ReactNode;
   /** Specifies notification bubble content. It appears on the top right of the `Tab`. */
-  notification: PropTypes.node,
+  notification?: React.ReactNode;
   /** Specifies whether `Tab` is disabled. */
-  disabled: PropTypes.bool,
+  disabled?: boolean;
   /**
    * A unique identifier for the Component, the `eventKey` makes it distinguishable
    * from others in a set. Similar to React's `key` prop, in that it only needs to be
    * unique amongst the Components siblings, not globally.
    */
-  eventKey: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  eventKey?: string | number;
   /** Specifies class name to append to the base element. */
-  tabClassName: PropTypes.string,
-};
+  tabClassName?: string;
+}
 
-Tab.defaultProps = {
-  notification: undefined,
-  disabled: undefined,
-  eventKey: undefined,
-  tabClassName: undefined,
-};
+function Tab(props: TabProps) {
+  return <BaseTab {...props} />;
+}
 
 export default Tab;
