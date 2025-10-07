411e19e7593925105a2c74010074282ac01f7144
Diana Villalvazo <diana.villalvazo@wgu.edu>
2025-10-07 10:23:29 -0600

    refactor: Remove propTypes + defaultProps from <ChipCarousel>, <Container>, <FormGroup> and <HyperLink> (#3794)
    * refactor: remove propTypes and defaultProps
    
    * refactor: remove propTypes and defaultProps Container
    
    * refactor: adding props extended to be available on docs
    
    * fix: container forward type
    
    * refactor: adding size props to be available on docs
    
    * docs: add links to type definition
    
    * docs: address comments to improve docs
    
    * fix: adding back react import to avoid docs fail
    
    * fix: small fix on as type

--- src/Hyperlink/index.tsx
+++ src/Hyperlink/index.tsx
@@ -1,5 +1,6 @@
-import React, { forwardRef } from 'react';
-import PropTypes from 'prop-types';
+import React, {
+  forwardRef, ForwardedRef, ElementType, ReactNode,
+} from 'react';
 import classNames from 'classnames';
 import {
   type BsPrefixRefForwardingComponent as ComponentWithAsProp,
@@ -8,14 +9,14 @@ import {
 import { defineMessages, useIntl } from 'react-intl';
 import { Launch } from '../../icons';
 import Icon from '../Icon';
-// @ts-ignore
-import { customPropTypeRequirement } from '../utils/propTypes/utils';
 
 export interface HyperlinkProps extends BsPrefixProps, Omit<React.ComponentPropsWithRef<'a'>, 'href' | 'target'> {
-  /** specifies the URL */
+  /** specifies the component element type to render for the hyperlink. */
+  as?: ElementType;
+  /** specifies the URL; required if `as` prop is a standard anchor tag */
   destination?: string;
   /** Content of the hyperlink */
-  children: React.ReactNode;
+  children: ReactNode;
   /** Custom class names for the hyperlink */
   className?: string;
   /** Alt text for the icon indicating that this link opens in a new tab, if target="_blank". e.g. _("in a new tab") */
@@ -49,7 +50,7 @@ const messages = defineMessages({
   },
 });
 
-const Hyperlink = forwardRef<HTMLAnchorElement, HyperlinkProps>(({
+const Hyperlink = forwardRef(({
   as: Component = 'a',
   className,
   destination,
@@ -62,7 +63,7 @@ const Hyperlink = forwardRef<HTMLAnchorElement, HyperlinkProps>(({
   isInline = false,
   showLaunchIcon = true,
   ...attrs
-}, ref) => {
+}: HyperlinkProps, ref: ForwardedRef<HTMLAnchorElement>) => {
   const intl = useIntl();
   let externalLinkIcon;
 
@@ -126,55 +127,6 @@ const Hyperlink = forwardRef<HTMLAnchorElement, HyperlinkProps>(({
     </Component>
   );
 });
-
-Hyperlink.propTypes = {
-  /** specifies the component element type to render for the hyperlink */
-  // @ts-ignore
-  as: PropTypes.elementType,
-  /** specifies the URL; required iff `as` prop is a standard anchor tag */
-  destination: customPropTypeRequirement(
-    PropTypes.string,
-    ({ as }: { as: React.ElementType }) => as && as === 'a',
-    // "[`destination` is required when]..."
-    'the `as` prop is a standard anchor element (i.e., "a")',
-  ),
-  /** Content of the hyperlink */
-  // @ts-ignore
-  children: PropTypes.node.isRequired,
-  /** Custom class names for the hyperlink */
-  className: PropTypes.string,
-  /** specifies where the link should open. The default behavior is `_self`, which means that the URL will be
-   * loaded into the same browsing context as the current one.
-   * If the target is `_blank` (opening a new window) `rel='noopener'` will be added to the anchor tag to prevent
-   * any potential [reverse tabnabbing attack](https://www.owasp.org/index.php/Reverse_Tabnabbing).
-   */
-  target: PropTypes.oneOf(['_blank', '_self']),
-  /** specifies the callback function when the link is clicked */
-  onClick: PropTypes.func,
-  /** Alt text for the icon indicating that this link opens in a new tab, if target="_blank". e.g. _("in a new tab") */
-  externalLinkAlternativeText: PropTypes.string,
-  /** Tooltip text for the "opens in new tab" icon, if target="_blank". e.g. _("Opens in a new tab"). */
-  externalLinkTitle: PropTypes.string,
-  /** type of hyperlink */
-  variant: PropTypes.oneOf(['default', 'muted', 'brand']),
-  /** Display the link with an underline. By default, it is only underlined on hover. */
-  isInline: PropTypes.bool,
-  /** specify if we need to show launch Icon. By default, it will be visible. */
-  showLaunchIcon: PropTypes.bool,
-};
-
-Hyperlink.defaultProps = {
-  as: 'a',
-  className: undefined,
-  destination: undefined,
-  externalLinkAlternativeText: undefined,
-  externalLinkTitle: undefined,
-  isInline: false,
-  onClick: undefined,
-  showLaunchIcon: true,
-  target: '_self',
-  variant: 'default',
-};
 
 Hyperlink.displayName = 'Hyperlink';
 
