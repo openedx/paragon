35a3a1e9a137a48231c00f8140013a897651c875
Diana Villalvazo <diana.villalvazo@wgu.edu>
2025-10-15 16:36:22 -0600

    refactor: transform 5 more components to typescript (#3820)
    * refactor: transform components into Typescript
    
    * fix: adding back react import to avoid docs fail
    
    * fix: addressing feedback comments for card context

---
+++ src/Stack/index.tsx
@@ -0,0 +1,44 @@
+import React, { forwardRef, ForwardedRef } from 'react';
+import classNames from 'classnames';
+
+interface StackProps {
+  /** Specifies the content of the `Stack`. */
+  children: React.ReactNode;
+  /** Specifies direction of the children blocks (column/row). */
+  direction?: 'horizontal' | 'vertical';
+  /**
+   * Specifies inner space between children blocks.
+   *
+   * Valid values are based on `the spacing classes`:
+   * `0, 0.5, ... 6`.
+   */
+  gap?: number;
+  /** Specifies the order of the children. */
+  reversed?: boolean;
+  /** Specifies an additional `className` to add to the base element. */
+  className?: string;
+}
+
+const Stack = forwardRef(({
+  direction = 'vertical',
+  gap = 0,
+  reversed = false,
+  children,
+  className,
+  ...rest
+}: StackProps, ref: ForwardedRef<HTMLDivElement>) => (
+  <div
+    ref={ref}
+    className={classNames(
+      direction === 'horizontal' ? 'pgn__hstack' : 'pgn__vstack',
+      gap ? `pgn__stack-gap--${gap}` : '',
+      reversed ? 'pgn__stack-reversed' : '',
+      className,
+    )}
+    {...rest}
+  >
+    {children}
+  </div>
+));
+
+export default Stack;
